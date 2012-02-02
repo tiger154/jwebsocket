b312fa9ea44d7aa45db17d2c0ac81a994b5c8f0d
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-02-02 21:42:59 +0000

    Adding JMX plugin

---
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketJMXPlugIn/JMXPlugIn/src/main/java/org/jwebsocket/plugins/jmx/JMXPlugInsExporter.java
@@ -0,0 +1,180 @@
+// ---------------------------------------------------------------------------
+// jWebSocket - JMXPlugIn v1.0
+// Copyright(c) 2010-2012 Innotrade GmbH, Herzogenrath, Germany, jWebSocket.org
+// ---------------------------------------------------------------------------
+// THIS CODE IS FOR RESEARCH, EVALUATION AND TEST PURPOSES ONLY!
+// THIS CODE MAY BE SUBJECT TO CHANGES WITHOUT ANY NOTIFICATION!
+// THIS CODE IS NOT YET SECURE AND MAY NOT BE USED FOR PRODUCTION ENVIRONMENTS!
+// ---------------------------------------------------------------------------
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3 of the License, or (at your
+// option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+// more details.
+// You should have received a copy of the GNU Lesser General Public License along
+// with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+// ---------------------------------------------------------------------------
+package org.jwebsocket.plugins.jmx;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.util.List;
+import java.util.Map;
+import javax.management.MBeanServer;
+import javolution.util.FastList;
+import javolution.util.FastMap;
+import org.apache.log4j.Logger;
+import org.jwebsocket.api.WebSocketServer;
+import org.jwebsocket.config.JWebSocketConfig;
+import org.jwebsocket.factory.JWebSocketFactory;
+import org.jwebsocket.factory.JWebSocketJarClassLoader;
+import org.jwebsocket.plugins.jmx.configDefinition.ConstuctorParameterDefinition;
+import org.jwebsocket.plugins.jmx.configDefinition.JMXDefinition;
+import org.jwebsocket.plugins.jmx.configDefinition.JMXPluginDefinition;
+import org.jwebsocket.plugins.jmx.mbeanSpring.MBeanEnabledExporter;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+/**
+ *
+ * @author Lisdey Pérez Hernández(lisdey89, UCI)
+ */
+public class JMXPlugInsExporter {
+
+	private Logger mLog;
+	private String mConfigFilePath;
+	private MBeanServer mServer;
+
+	public JMXPlugInsExporter(String aPath, MBeanServer aMBeanServer, Logger aLog) {
+		this.mConfigFilePath = aPath;
+		this.mServer = aMBeanServer;
+		this.mLog = aLog;
+	}
+
+	private List listConfigFilesNames() {
+		List lAllConfigFiles = new FastList();
+		try {
+			File lpathOfConfig = new File(mConfigFilePath);
+			if (false == lpathOfConfig.exists() || false == lpathOfConfig.isDirectory()) {
+				throw new IllegalArgumentException("The config file path is incorrect.");
+			}
+			for (String lfileName : lpathOfConfig.list()) {
+				if (lfileName.toLowerCase().endsWith("beanconfig.xml")) {
+					lAllConfigFiles.add(lfileName);
+				}
+			}
+		} catch (Exception ex) {
+			mLog.error("JMXPlugInExporter on actionPerformed: " + ex.getMessage());
+		}
+		return lAllConfigFiles;
+	}
+
+	public void createMBeansToExport() throws Exception {
+		List<String> lConfigFiles = listConfigFilesNames();
+		MBeanEnabledExporter lExporter = new MBeanEnabledExporter();
+		lExporter.setServer(mServer);
+		Map lBean = new FastMap();
+		for (String lConfigFile : lConfigFiles) {
+			Resource lBeanResource = new FileSystemResource(mConfigFilePath + lConfigFile);
+			XmlBeanFactory lBeanFactory = new XmlBeanFactory(lBeanResource);
+			if (lConfigFile.toLowerCase().endsWith("beanconfig.xml")) {
+				String lBeanName = lConfigFile.substring(0, lConfigFile.length() - 14);
+				registerMBean(lExporter, lBeanFactory, lBeanName, lBean);
+			}
+		}
+		try {
+			if (!lBean.isEmpty()) {
+				lExporter.setBeans(lBean);
+				lExporter.afterPropertiesSet();
+			}
+		} catch (Exception ex) {
+			mLog.error("JMXPlugInExporter on actionPerformed: " + ex.getMessage());
+		}
+	}
+
+	private Object getNewClassInstance(String aClassName, String aJarName, Object[] aValues) throws Exception {
+		JWebSocketJarClassLoader lClassLoader = new JWebSocketJarClassLoader();
+		String lJarFilePath = JWebSocketConfig.getLibsFolder(aJarName);
+		Class lClass = null;
+		Object lObj = null;
+
+		//lJarFilePath may be null if .jar is included in server bundle
+		try {
+			if (lJarFilePath != null) {
+				lClassLoader.addFile(lJarFilePath);
+				lClass = lClassLoader.loadClass(aClassName);
+			}
+
+			// if class found try to create an instance
+			if (lClass != null) {
+				if (aValues == null) {
+					lObj = lClass.newInstance();
+				} else {
+					Class[] lTypes = new Class[aValues.length];
+					for (int i = 0; i < aValues.length; i++) {
+						lTypes[i] = aValues[i].getClass();
+					}
+
+					Constructor lClassConstructor = lClass.getConstructor(lTypes);
+
+					if (lClassConstructor != null) {
+						lClassConstructor.setAccessible(true);
+						lObj = lClassConstructor.newInstance(aValues);
+					}
+				}
+			}
+
+		} catch (Exception ex) {
+			throw new Exception(ex.getClass().getSimpleName() + ": " + ex.getMessage());
+		}
+
+		return lObj;
+	}
+
+	private void registerMBean(MBeanEnabledExporter aExporter, XmlBeanFactory aBeanFactory, String aBeanName, Map aBeanMap) throws Exception {
+		JMXDefinition lDefinition = null;
+		try {
+			lDefinition = (JMXDefinition) aBeanFactory.getBean(aBeanName);
+
+			if (lDefinition instanceof JMXPluginDefinition) {
+				JMXPluginDefinition lPluginDefinition = (JMXPluginDefinition) lDefinition;
+				//get the object of the server
+				WebSocketServer lServer = JWebSocketFactory.getServer(lPluginDefinition.getServerId());
+				//get the plugin by id
+				Object lMbeanObject = lServer.getPlugInById(lPluginDefinition.getPluginId());
+
+				aBeanMap.put("jWebSocketServer:name=" + aBeanName, lMbeanObject);
+			} else {
+				Object lClass = null;
+				if (lDefinition.getConstructors().length > 0) {
+					ConstuctorParameterDefinition[] lParameters = lDefinition.getConstructors()[0].getParameters();
+
+					if (lParameters.length == 0 || lParameters == null) {
+						lClass = getNewClassInstance(lDefinition.getClassName(), lDefinition.getJarName(), null);
+					} else {
+						Object[] lValues = new Object[lParameters.length];
+						for (int i = 0; i < lParameters.length; i++) {
+							lValues[i] = lParameters[i].getValue();
+						}
+						lClass = getNewClassInstance(lDefinition.getClassName(), lDefinition.getJarName(), lValues);
+					}
+				}
+				if (lClass != null) {
+					aBeanMap.put("jWebSocketServer:name=" + aBeanName, lClass);
+				} else {
+					throw new Exception("The class" + aBeanName + "can't be instanciated");
+				}
+			}
+
+			if (!aBeanName.equals("")) {
+				aExporter.setDefinition("jWebSocketServer:name=" + aBeanName, lDefinition);
+			}
+		} catch (Exception e) {
+			mLog.error("JMXPlugInExporter on actionPerformed: " + e.getMessage());
+		}
+	}
+}
---
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketJMXPlugIn/JMXPlugIn/src/main/java/org/jwebsocket/plugins/jmx/mbeanSpring/ModelMBeanUtil.java
@@ -0,0 +1,82 @@
+// ---------------------------------------------------------------------------
+// jWebSocket - JMXPlugIn v1.0
+// Copyright(c) 2010-2012 Innotrade GmbH, Herzogenrath, Germany, jWebSocket.org
+// ---------------------------------------------------------------------------
+// THIS CODE IS FOR RESEARCH, EVALUATION AND TEST PURPOSES ONLY!
+// THIS CODE MAY BE SUBJECT TO CHANGES WITHOUT ANY NOTIFICATION!
+// THIS CODE IS NOT YET SECURE AND MAY NOT BE USED FOR PRODUCTION ENVIRONMENTS!
+// ---------------------------------------------------------------------------
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3 of the License, or (at your
+// option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+// more details.
+// You should have received a copy of the GNU Lesser General Public License along
+// with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+// ---------------------------------------------------------------------------
+
+package org.jwebsocket.plugins.jmx.mbeanSpring;
+
+import java.lang.reflect.Method;
+import javax.management.Descriptor;
+import javax.management.MBeanException;
+import javax.management.MBeanNotificationInfo;
+import javax.management.modelmbean.ModelMBeanAttributeInfo;
+import javax.management.modelmbean.ModelMBeanInfo;
+
+/**
+ *
+ * @author Lisdey Pérez Hernández(lisdey89, UCI)
+ */
+public class ModelMBeanUtil {
+
+    protected static String toMethodName(String aPrefix, String aName) {
+        char lHead = Character.toUpperCase(aName.charAt(0));
+        String lTail = aName.substring(1);
+        return aPrefix + lHead + lTail;
+    }
+
+    protected static String matchType(MBeanNotificationInfo aInfo, String aMatch) {
+        String[] lTypes = aInfo.getNotifTypes();
+        for (String lType : lTypes) {
+            if (lType.endsWith(aMatch)) {
+                return lType;
+            }
+        }
+        return lTypes[0];
+    }
+
+    public static Method findGetMethod(ModelMBeanInfo aModelMBeanInfo, Object aManagedBean, String aAttribute)
+            throws MBeanException {
+        try {
+            ModelMBeanAttributeInfo lInfo = aModelMBeanInfo.getAttribute(aAttribute);
+            Descriptor lDescriptor = lInfo.getDescriptor();
+            String lMethodName = (String) (lDescriptor.getFieldValue("getMethod"));
+            if (lMethodName == null) {
+                lMethodName = toMethodName("get", aAttribute);
+            }
+            return aManagedBean.getClass().getMethod(lMethodName, new Class[]{});
+        } catch (NoSuchMethodException e) {
+            throw new MBeanException(e);
+        }
+    }
+
+    public static Method findSetMethod(ModelMBeanInfo aModelMBeanInfo, Object aManagedBean, String aAttribute)
+            throws MBeanException {
+        try {
+            ModelMBeanAttributeInfo lInfo = aModelMBeanInfo.getAttribute(aAttribute);
+            Descriptor lDescriptor = lInfo.getDescriptor();
+            String lMethodName = (String) (lDescriptor.getFieldValue("setMethod"));
+            if (lMethodName == null) {
+                lMethodName = toMethodName("set", aAttribute);
+            }
+            Class[] lType = {Class.forName(lInfo.getType())};
+            return aManagedBean.getClass().getMethod(lMethodName, lType);
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            throw new MBeanException(e);
+        }
+    }
+}
