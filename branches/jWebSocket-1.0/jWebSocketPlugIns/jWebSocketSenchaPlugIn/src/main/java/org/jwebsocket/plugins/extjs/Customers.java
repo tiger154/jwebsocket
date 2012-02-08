af3361b72d09881f592bf179a98b9604d6fc6c54
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-02-08 16:59:47 +0000

    Just adjusting format

--- branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketSenchaPlugIn/src/main/java/org/jwebsocket/plugins/extjs/Customers.java
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketSenchaPlugIn/src/main/java/org/jwebsocket/plugins/extjs/Customers.java
@@ -18,133 +18,128 @@ package org.jwebsocket.plugins.extjs;
 import java.util.LinkedList;
 import java.util.List;
 
-
-
 /**
  *
  * @author Osvaldo Aguilar Lauzurique, Alexander Rojas Hernandez
  */
 public class Customers {
 
-    private LinkedList<CustomerDef> customers;
-    private Integer count;
+	private LinkedList<CustomerDef> customers;
+	private Integer count;
 
-    public Customers() {
-        count = 0;
-        customers = new LinkedList<CustomerDef>();
-        try{
-            
-            add(new CustomerDef(count, "Alexander", "arojash@uci.cu", 24));
-            add(new CustomerDef(count, "Alexander", "a.schulze@jwebsocket.org", 40));
-            add(new CustomerDef(count, "Anuradha", "galianuradha@gmail.com", 25));
-            add(new CustomerDef(count, "Armando", "alsimon@uci.cu", 26));
-            add(new CustomerDef(count, "Carlos", "carlosfeyt@hab.uci.cu", 25));
-            add(new CustomerDef(count, "Carlos", "ckcespedes@uci.cu", 25));
-            add(new CustomerDef(count, "Claudia", "c.gillrath@innotrade.de", 30));
-            add(new CustomerDef(count, "Daimi", "dmederos@hab.uci.cu", 24));
-            add(new CustomerDef(count, "Dariel", "dnoa@uci.cu", 25));
-            add(new CustomerDef(count, "Eduardo", "ebourzach@uci.cu", 26));
+	public Customers() {
+		count = 0;
+		customers = new LinkedList<CustomerDef>();
+		try {
 
-            add(new CustomerDef(count, "Johannes", "johannes.schoenborn@gmail.com", 25));
-            add(new CustomerDef(count, "Johannes", "j.smutny@gmail.com", 25));
-            add(new CustomerDef(count, "Lester", "lzaila@hab.uci.cu", 25));
-            add(new CustomerDef(count, "Lisdey", "lperez@hab.uci.cu", 23));
-            add(new CustomerDef(count, "Merly", "mlopez@hab.uci.cu", 24));
-            add(new CustomerDef(count, "Marcos ", "magonzalez@hab.uci.cu", 25));
-            add(new CustomerDef(count, "Marta ", "mrodriguez@hab.uci.cu", 25));
-            add(new CustomerDef(count, "Mayra", "memaranon@hab.uci.cu", 25));
-            add(new CustomerDef(count, "Orlando", "omiranda@uci.cu", 25));
-            add(new CustomerDef(count, "Osvaldo", "oaguilar@uci.cu", 25));
+			add(new CustomerDef(count, "Alexander", "arojash@uci.cu", 24));
+			add(new CustomerDef(count, "Alexander", "a.schulze@jwebsocket.org", 40));
+			add(new CustomerDef(count, "Anuradha", "galianuradha@gmail.com", 25));
+			add(new CustomerDef(count, "Armando", "alsimon@uci.cu", 26));
+			add(new CustomerDef(count, "Carlos", "carlosfeyt@hab.uci.cu", 25));
+			add(new CustomerDef(count, "Carlos", "ckcespedes@uci.cu", 25));
+			add(new CustomerDef(count, "Claudia", "c.gillrath@innotrade.de", 30));
+			add(new CustomerDef(count, "Daimi", "dmederos@hab.uci.cu", 24));
+			add(new CustomerDef(count, "Dariel", "dnoa@uci.cu", 25));
+			add(new CustomerDef(count, "Eduardo", "ebourzach@uci.cu", 26));
 
-            add(new CustomerDef(count, "Prashant", "prashantkhanal@gmail.com", 25));
-            add(new CustomerDef(count, "Puran", "mailtopuran@gmail.com", 25));
-            add(new CustomerDef(count, "Quentin", "quentin.ambard@gmail.com", 25));
-            add(new CustomerDef(count, "Rebecca", "r.schulze@jwebsocket.org", 23));
-            add(new CustomerDef(count, "Rolando", "rbetancourt@hab.uci.cu", 24));
-            add(new CustomerDef(count, "Rolando", "rsantamaria@hab.uci.cu", 26));
-            add(new CustomerDef(count, "Roylandi", "rgpujol@hab.uci.cu", 25));
-            add(new CustomerDef(count, "Unni", "unnivm@gmail.com", 25));
-            add(new CustomerDef(count, "Victor", "vbarzana@uci.cu", 25));
-            add(new CustomerDef(count, "Yamila", "yvigil@hab.uci.cu", 27));
+			add(new CustomerDef(count, "Johannes", "johannes.schoenborn@gmail.com", 25));
+			add(new CustomerDef(count, "Johannes", "j.smutny@gmail.com", 25));
+			add(new CustomerDef(count, "Lester", "lzaila@hab.uci.cu", 25));
+			add(new CustomerDef(count, "Lisdey", "lperez@hab.uci.cu", 23));
+			add(new CustomerDef(count, "Merly", "mlopez@hab.uci.cu", 24));
+			add(new CustomerDef(count, "Marcos ", "magonzalez@hab.uci.cu", 25));
+			add(new CustomerDef(count, "Marta ", "mrodriguez@hab.uci.cu", 25));
+			add(new CustomerDef(count, "Mayra", "memaranon@hab.uci.cu", 25));
+			add(new CustomerDef(count, "Orlando", "omiranda@uci.cu", 25));
+			add(new CustomerDef(count, "Osvaldo", "oaguilar@uci.cu", 25));
 
-            add(new CustomerDef(count, "Yasmany", "ynbosh@hab.uci.cu", 24));
+			add(new CustomerDef(count, "Prashant", "prashantkhanal@gmail.com", 25));
+			add(new CustomerDef(count, "Puran", "mailtopuran@gmail.com", 25));
+			add(new CustomerDef(count, "Quentin", "quentin.ambard@gmail.com", 25));
+			add(new CustomerDef(count, "Rebecca", "r.schulze@jwebsocket.org", 23));
+			add(new CustomerDef(count, "Rolando", "rbetancourt@hab.uci.cu", 24));
+			add(new CustomerDef(count, "Rolando", "rsantamaria@hab.uci.cu", 26));
+			add(new CustomerDef(count, "Roylandi", "rgpujol@hab.uci.cu", 25));
+			add(new CustomerDef(count, "Unni", "unnivm@gmail.com", 25));
+			add(new CustomerDef(count, "Victor", "vbarzana@uci.cu", 25));
+			add(new CustomerDef(count, "Yamila", "yvigil@hab.uci.cu", 27));
 
-
+			add(new CustomerDef(count, "Yasmany", "ynbosh@hab.uci.cu", 24));
 
 
-        }catch(Exception ex){
-        
-        }
-        
-    }
-
-    public void add(CustomerDef customer) throws Exception {
-        for (CustomerDef u : customers) {
-            if (u.getEmail().equals(customer.getEmail())) {
-                throw new Exception("customers duplicated");
-            }
-        }
-        customers.add(customer);
-        count++;
-    }
-
-    public LinkedList<CustomerDef> getCustomers() {
-        return customers;
-    }
-
-    public List<CustomerDef> getSubList(int start, int limit) {
 
 
-        if (limit >  customers.size())
-        {
-            limit -= limit - customers.size();
-        }
-        
-        return customers.subList(start, limit);
-         
-    }
+		} catch (Exception ex) {
+		}
 
-    public Integer getSize(){
-        return customers.size();
-    }
+	}
+
+	public void add(CustomerDef customer) throws Exception {
+		for (CustomerDef u : customers) {
+			if (u.getEmail().equals(customer.getEmail())) {
+				throw new Exception("customers duplicated");
+			}
+		}
+		customers.add(customer);
+		count++;
+	}
+
+	public LinkedList<CustomerDef> getCustomers() {
+		return customers;
+	}
+
+	public List<CustomerDef> getSubList(int start, int limit) {
 
 
-    public CustomerDef getCustomer(Integer id) {
-        CustomerDef customer = null;
-        for (CustomerDef u : customers) {
-            if (u.getId().equals(id)) {
-                return u;
-            }
-        }
-        return customer;
-    }
+		if (limit > customers.size()) {
+			limit -= limit - customers.size();
+		}
 
-    public void setCount(Integer count) {
-        this.count = count;
-    }
+		return customers.subList(start, limit);
 
-    public Integer getCount() {
-        return count;
-    }
+	}
 
-    public boolean findCustomer(Integer id) {
-        for (CustomerDef u : customers) {
-            if (u.getId().equals(id)) {
-                return true;
-            }
-        }
+	public Integer getSize() {
+		return customers.size();
+	}
 
-        return false;
-    }
+	public CustomerDef getCustomer(Integer id) {
+		CustomerDef customer = null;
+		for (CustomerDef u : customers) {
+			if (u.getId().equals(id)) {
+				return u;
+			}
+		}
+		return customer;
+	}
 
-    public boolean deleteCustomer(Integer id) {
-        for (CustomerDef u : customers) {
-            if (u.getId().equals(id)) {
-                customers.remove(u);
-                return true;
-            }
-        }
+	public void setCount(Integer count) {
+		this.count = count;
+	}
 
-        return false;
-    }
+	public Integer getCount() {
+		return count;
+	}
+
+	public boolean findCustomer(Integer id) {
+		for (CustomerDef u : customers) {
+			if (u.getId().equals(id)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean deleteCustomer(Integer id) {
+		for (CustomerDef u : customers) {
+			if (u.getId().equals(id)) {
+				customers.remove(u);
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
