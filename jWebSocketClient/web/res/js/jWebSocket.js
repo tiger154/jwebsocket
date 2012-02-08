720a27d98210125862008fd70a21e279acf39b7a
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-02-08 09:42:33 +0000

    Updated to latest version number

--- jWebSocketClient/web/res/js/jWebSocket.js
+++ jWebSocketClient/web/res/js/jWebSocket.js
@@ -35,9 +35,9 @@ if( window.MozWebSocket ) {
 //:d:en:including various utility methods.
 var jws = {
 
-	//:const:*:VERSION:String:1.0b5 (nightly build 20105)
+	//:const:*:VERSION:String:1.0b6 (nightly build 20123)
 	//:d:en:Version of the jWebSocket JavaScript Client
-	VERSION: "1.0b5 (nightly build 20105)",
+	VERSION: "1.0b6 (nightly build 20123)",
 
 	//:const:*:NS_BASE:String:org.jwebsocket
 	//:d:en:Base namespace
