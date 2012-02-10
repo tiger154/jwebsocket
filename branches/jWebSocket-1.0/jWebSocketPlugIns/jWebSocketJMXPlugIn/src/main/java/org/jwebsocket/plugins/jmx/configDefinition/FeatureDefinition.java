7e0b0498e645a274b8365adb01fa319894c8c48f
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-02-10 16:01:54 +0000

    Refactored JMX plug-in to match our common standards.

---
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketJMXPlugIn/src/main/java/org/jwebsocket/plugins/jmx/configDefinition/FeatureDefinition.java
@@ -0,0 +1,67 @@
+// ---------------------------------------------------------------------------
+// jWebSocket - JMXPlugIn v1.0
+// Copyright(c) 2010-2012 Innotrade GmbH, Herzogenrath, Germany, jWebSocket.org
+// ---------------------------------------------------------------------------
+// THIS CODE IS FOR RESEARCH, EVALUATION AND TEST PURPOSES ONLY!
+// THIS CODE MAY BE SUBJECT TO CHANGES WITHOUT ANY NOTIFICATION!
+// THIS CODE IS NOT YET SECURE AND MAY NOT BE USED FOR PRODUCTION ENVIRONMENTS!
+// ---------------------------------------------------------------------------
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3 of the License, or (at your
+// option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+// more details.
+// You should have received a copy of the GNU Lesser General Public License along
+// with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+// ---------------------------------------------------------------------------
+
+package org.jwebsocket.plugins.jmx.configDefinition;
+
+/**
+ *
+ * @author Lisdey Pérez Hernández(lisdey89, UCI)
+ */
+public abstract class FeatureDefinition {
+
+    private String mName;
+    private String mDescription;
+
+    public FeatureDefinition() {
+    }
+
+    public FeatureDefinition(String aName, String aDescription) {
+        this.mDescription = aDescription;
+        if (!aName.equals("")) {
+            this.mName = aName;
+        } else {
+            throw new IllegalArgumentException("Name must not be empty");
+        }
+    }
+
+    public String getName() {
+        if (this.mName != null) {
+            return this.mName;
+        } else {
+            throw new IllegalArgumentException("Name must not be null");
+        }
+    }
+
+    public void setName(String aName) {
+        if (!aName.equals("")) {
+            this.mName = aName;
+        } else {
+            throw new IllegalArgumentException("Name must not be empty");
+        }
+    }
+
+    public String getDescription() {
+        return mDescription == null ? "" : mDescription;
+    }
+
+    public void setDescription(String aDescription) {
+        this.mDescription = aDescription;
+    }
+}
---
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketJMXPlugIn/src/main/java/org/jwebsocket/plugins/jmx/configDefinition/JMXPluginDefinition.java
@@ -0,0 +1,83 @@
+// ---------------------------------------------------------------------------
+// jWebSocket - JMXPlugIn v1.0
+// Copyright(c) 2010-2012 Innotrade GmbH, Herzogenrath, Germany, jWebSocket.org
+// ---------------------------------------------------------------------------
+// THIS CODE IS FOR RESEARCH, EVALUATION AND TEST PURPOSES ONLY!
+// THIS CODE MAY BE SUBJECT TO CHANGES WITHOUT ANY NOTIFICATION!
+// THIS CODE IS NOT YET SECURE AND MAY NOT BE USED FOR PRODUCTION ENVIRONMENTS!
+// ---------------------------------------------------------------------------
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 3 of the License, or (at your
+// option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+// more details.
+// You should have received a copy of the GNU Lesser General Public License along
+// with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+// ---------------------------------------------------------------------------
+
+package org.jwebsocket.plugins.jmx.configDefinition;
+
+/**
+ *
+ * @author Lisdey Pérez Hernández(lisdey89, UCI)
+ */
+public class JMXPluginDefinition extends JMXDefinition {
+
+    private String mPluginId;
+    private String mServerId;
+
+    public JMXPluginDefinition() {
+    }
+
+    public JMXPluginDefinition(String aPluginId, String aServerId, String aClassName, String aJarName, AttributeDefinition[] aAttributes, 
+            OperationDefinition[] aOperations, ConstructorDefinition[] aConstructors, NotificationDefinition[] aNotifications) {
+        super(aClassName, aJarName, aAttributes, aOperations, aConstructors, aNotifications);
+        
+        if (!aPluginId.equals("")) {
+            this.mPluginId = aPluginId;
+        } else {
+            throw new IllegalArgumentException("The plugin id must not be empty");
+        }
+
+        if (!aServerId.equals("")) {
+            this.mServerId = aServerId;
+        } else {
+            throw new IllegalArgumentException("The server id must not be empty.");
+        }
+    }
+    
+    public String getPluginId() {
+        if (this.mPluginId != null) {
+            return mPluginId;
+        } else {
+            throw new IllegalArgumentException("The plugin id must not be null.");
+        }
+    }
+
+    public void setPluginId(String aPluginId) {
+        if (!aPluginId.equals("")) {
+            this.mPluginId = aPluginId;
+        } else {
+            throw new IllegalArgumentException("The plugin id must not be empty.");
+        }
+    }
+
+    public String getServerId() {
+        if (this.mServerId != null) {
+            return mServerId;
+        } else {
+            throw new IllegalArgumentException("The server id must not be null.");
+        }
+    }
+
+    public void setServerId(String aServerId) {
+        if (!aServerId.equals("")) {
+            this.mServerId = aServerId;
+        } else {
+            throw new IllegalArgumentException("The server id must not be empty.");
+        }
+    }
+}
