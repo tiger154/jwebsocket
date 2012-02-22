a9ba7eeb63a8193bfda0f3e759841b3d1471fdc6
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-02-22 10:38:34 +0000

    Submitting missing files for new spring based logging and reporting, sorry for any potential confusion.

---
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketLoggingPlugIn/src/main/java/org/jwebsocket/plugins/logging/Settings.java
@@ -0,0 +1,40 @@
+//	---------------------------------------------------------------------------
+//	jWebSocket - jWebSocket Settings for Logging Plug-in
+//  Copyright (c) 2012 Innotrade GmbH, jWebSocket.org
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.jwebsocket.plugins.logging;
+
+/**
+ *
+ * @author aschulze
+ */
+public class Settings {
+
+	private ILogger mTarget = null;
+
+	/**
+	 * @return the mTarget
+	 */
+	public ILogger getTarget() {
+		return mTarget;
+	}
+
+	/**
+	 * @param mTarget the mTarget to set
+	 */
+	public void setTarget(ILogger mTarget) {
+		this.mTarget = mTarget;
+	}
+
+}
---
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketReportingPlugIn/src/main/java/org/jwebsocket/plugins/reporting/Settings.java
@@ -0,0 +1,95 @@
+//	---------------------------------------------------------------------------
+//	jWebSocket - jWebSocket Settings for Reporting Plug-in
+//  Copyright (c) 2012 Innotrade GmbH, jWebSocket.org
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.jwebsocket.plugins.reporting;
+
+import org.apache.commons.io.FilenameUtils;
+import org.jwebsocket.util.Tools;
+
+/**
+ *
+ * @author aschulze
+ */
+public class Settings {
+
+	private String mReportFolder = null;
+	private String mOutputFolder = null;
+	private String mOutputURL = null;
+	private String mReportNamePattern = null;
+
+	/**
+	 * @return the mReportFolder
+	 */
+	public String getReportFolder() {
+		return mReportFolder;
+	}
+
+	/**
+	 * @param mReportFolder the mReportFolder to set
+	 */
+	public void setReportFolder(String aReportFolder) {
+		mReportFolder = aReportFolder;
+		mReportFolder = FilenameUtils.separatorsToUnix(Tools.expandEnvVars(mReportFolder));
+		if (!mReportFolder.endsWith("/")) {
+			mReportFolder += "/";
+		}
+	}
+
+	/**
+	 * @return the mOutputFolder
+	 */
+	public String getOutputFolder() {
+		return mOutputFolder;
+	}
+
+	/**
+	 * @param mOutputFolder the mOutputFolder to set
+	 */
+	public void setOutputFolder(String aOutputFolder) {
+		mOutputFolder = aOutputFolder;
+		mOutputFolder = FilenameUtils.separatorsToUnix(Tools.expandEnvVars(mOutputFolder));
+		if (!mOutputFolder.endsWith("/")) {
+			mOutputFolder += "/";
+		}
+	}
+
+	/**
+	 * @return the mOutputURL
+	 */
+	public String getOutputURL() {
+		return mOutputURL;
+	}
+
+	/**
+	 * @param mOutputURL the mOutputURL to set
+	 */
+	public void setOutputURL(String mOutputURL) {
+		this.mOutputURL = mOutputURL;
+	}
+
+	/**
+	 * @return the mReportNamePattern
+	 */
+	public String getReportNamePattern() {
+		return mReportNamePattern;
+	}
+
+	/**
+	 * @param mReportNamePattern the mReportNamePattern to set
+	 */
+	public void setReportNamePattern(String mReportNamePattern) {
+		this.mReportNamePattern = mReportNamePattern;
+	}
+}
