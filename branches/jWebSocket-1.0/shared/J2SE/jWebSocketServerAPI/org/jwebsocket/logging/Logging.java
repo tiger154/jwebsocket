a4b73df75e0e2d002eaa5e143725f3ab5d4bccfe
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-03-08 19:10:33 +0000

    Preparing rollout 1.0 beta 6, build 20312, fixing log4j logging in web app environments.

--- branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/logging/Logging.java
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/logging/Logging.java
@@ -15,8 +15,10 @@
 //	---------------------------------------------------------------------------
 package org.jwebsocket.logging;
 
+import java.io.File;
 import java.io.IOException;
 import javolution.util.FastMap;
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.*;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.jwebsocket.config.JWebSocketConfig;
@@ -24,8 +26,8 @@ import org.jwebsocket.config.JWebSocketServerConstants;
 import org.jwebsocket.config.xml.LoggingConfig;
 
 /**
- * Provides the common used jWebSocket logging support based on
- * Apache's log4j.
+ * Provides the common used jWebSocket logging support based on Apache's log4j.
+ *
  * @author Alexander Schulze (aschulze, Innotrade GmbH, jWebSocket.org)
  */
 public class Logging {
@@ -56,8 +58,8 @@ public class Logging {
 	 */
 	private static String mPattern = "%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p - %C{1}: %m%n";
 	/**
-	 * buffer size if write cache for logs is activated (recommended)
-	 * buffer size = 0 means no write cache.
+	 * buffer size if write cache for logs is activated (recommended) buffer
+	 * size = 0 means no write cache.
 	 */
 	private static int mBuffersize = 8096; // 8K is log4j default
 	private static int mLogTarget = CONSOLE; // ROLLING_FILE;
@@ -72,43 +74,37 @@ public class Logging {
 		// try to obtain JWEBSOCKET_HOME environment variable
 		String lFileSep = System.getProperty("file.separator");
 		/*
-		String lWebSocketHome = System.getenv(JWebSocketServerConstants.JWEBSOCKET_HOME);
-		String lFileSep = System.getProperty("file.separator");
-		String lWebSocketLogs = null;
-		if (lWebSocketHome != null) {
-			// append trailing slash if needed
-			if (!lWebSocketHome.endsWith(lFileSep)) {
-				lWebSocketHome += lFileSep;
-			}
-			// logs are located in %JWEBSOCKET_HOME%/logs
-			lWebSocketLogs = lWebSocketHome + "logs" + lFileSep + aFileName;
-		}
-		*/
-		
+		 * String lWebSocketHome =
+		 * System.getenv(JWebSocketServerConstants.JWEBSOCKET_HOME); String
+		 * lFileSep = System.getProperty("file.separator"); String
+		 * lWebSocketLogs = null; if (lWebSocketHome != null) { // append
+		 * trailing slash if needed if (!lWebSocketHome.endsWith(lFileSep)) {
+		 * lWebSocketHome += lFileSep; } // logs are located in
+		 * %JWEBSOCKET_HOME%/logs lWebSocketLogs = lWebSocketHome + "logs" +
+		 * lFileSep + aFileName; }
+		 */
+
 		String lWebSocketLogs = JWebSocketConfig.getJWebSocketHome()
 				+ "logs" + lFileSep + aFileName;
 
 		/*
-		if (lWebSocketLogs == null) {
-			// try to obtain CATALINA_HOME environment variable
-			String lWebSocketHome = System.getenv("CATALINA_HOME");
-			if (lWebSocketHome != null) {
-				// append trailing slash if needed
-				if (!lWebSocketHome.endsWith(lFileSep)) {
-					lWebSocketHome += lFileSep;
-				}
-				// logs are located in %CATALINA_HOME%/logs
-				lWebSocketLogs = lWebSocketHome + "logs" + lFileSep + aFileName;
-			}
-		}
-		*/
+		 * if (lWebSocketLogs == null) { // try to obtain CATALINA_HOME
+		 * environment variable String lWebSocketHome =
+		 * System.getenv("CATALINA_HOME"); if (lWebSocketHome != null) { //
+		 * append trailing slash if needed if
+		 * (!lWebSocketHome.endsWith(lFileSep)) { lWebSocketHome += lFileSep; }
+		 * // logs are located in %CATALINA_HOME%/logs lWebSocketLogs =
+		 * lWebSocketHome + "logs" + lFileSep + aFileName; } }
+		 */
 		return lWebSocketLogs;
 	}
 
 	/**
 	 * Initializes the Apache log4j system to produce the desired logging
 	 * output.
-	 * @param aLogLevel one of the values TRACE, DEBUG, INFO, WARN, ERROR or FATAL.
+	 *
+	 * @param aLogLevel one of the values TRACE, DEBUG, INFO, WARN, ERROR or
+	 * FATAL.
 	 *
 	 */
 	private static void checkLogAppender() {
@@ -123,7 +119,9 @@ public class Logging {
 				if (ROLLING_FILE == mLogTarget && logsPath != null) {
 					try {
 						RollingFileAppender lRFA = new RollingFileAppender(mLayout,
-								logsPath, true /* append, don't truncate */);
+								logsPath, true /*
+								 * append, don't truncate
+								 */);
 						lRFA.setBufferedIO(mBuffersize > 0);
 						lRFA.setImmediateFlush(true);
 						if (mBuffersize > 0) {
@@ -137,7 +135,9 @@ public class Logging {
 				} else if (SINGLE_FILE == mLogTarget && logsPath != null) {
 					try {
 						FileAppender lFA = new FileAppender(mLayout, logsPath,
-								true /* append, don't truncate */);
+								true /*
+								 * append, don't truncate
+								 */);
 						lFA.setBufferedIO(mBuffersize > 0);
 						lFA.setImmediateFlush(true);
 						if (mBuffersize > 0) {
@@ -160,6 +160,17 @@ public class Logging {
 		} else {
 			if (!mSettingsLoaded) {
 				String lLog4JConfigFile = JWebSocketConfig.expandEnvAndJWebSocketVars(mConfigFile);
+				/*
+				String lConfigXml = null;
+				try {
+					lConfigXml = FileUtils.readFileToString(new File(lLog4JConfigFile));
+				} catch (IOException ex) {
+					System.out.println("Logs: " + lLog4JConfigFile + "\nEXCEPTION!");
+				}
+				System.out.println("=============================");
+				System.out.println("Logs: " + lLog4JConfigFile + "\n" + lConfigXml);
+				System.out.println("=============================");
+				*/
 				if (mReloadDelay >= MIN_RELOAD_DELAY) {
 					DOMConfigurator.configureAndWatch(lLog4JConfigFile, mReloadDelay);
 				} else {
@@ -171,9 +182,9 @@ public class Logging {
 	}
 
 	/**
-	 * Initializes the jWebSocket logging system with the given log level.
-	 * All subsequently instantiated class specific loggers will use this
-	 * setting.
+	 * Initializes the jWebSocket logging system with the given log level. All
+	 * subsequently instantiated class specific loggers will use this setting.
+	 *
 	 * @param aLogLevel
 	 */
 	public static void initLogs(String aLogLevel, String aLogTarget,
@@ -259,8 +270,8 @@ public class Logging {
 	}
 
 	/**
-	 * closes the log file. Take care that no further lines are appended
-	 * to the logs after it has been closed!
+	 * closes the log file. Take care that no further lines are appended to the
+	 * logs after it has been closed!
 	 */
 	public static void exitLogs() {
 		if (mAppender != null) {
@@ -272,8 +283,9 @@ public class Logging {
 	}
 
 	/**
-	 * Returns a logger for a certain class by using the jWebSocket settings
-	 * for logging and ignoring inherited log4j settings.
+	 * Returns a logger for a certain class by using the jWebSocket settings for
+	 * logging and ignoring inherited log4j settings.
+	 *
 	 * @param aClass
 	 * @return Logger the new logger for the given class.
 	 */
