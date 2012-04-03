3eb1f75cefce89a32d4c3dabad83e9553e102058
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-04-03 05:58:39 +0000

    Problem with the monitoring PlugIn fixed

--- jWebSocketPlugIns/jWebSocketMonitoringPlugIn/src/main/java/org/jwebsocket/plugins/monitoring/ServerRequestListener.java
+++ jWebSocketPlugIns/jWebSocketMonitoringPlugIn/src/main/java/org/jwebsocket/plugins/monitoring/ServerRequestListener.java
@@ -23,7 +23,6 @@ import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.apache.log4j.Logger;
-import org.jwebsocket.api.WebSocketConnector;
 import org.jwebsocket.api.WebSocketPacket;
 import org.jwebsocket.api.WebSocketServerListener;
 import org.jwebsocket.kit.WebSocketServerEvent;
@@ -38,82 +37,19 @@ public class ServerRequestListener implements WebSocketServerListener {
 	Mongo mConnection;
 	DBCollection mChartingCollection;
 	Logger mLog = Logging.getLogger(ServerRequestListener.class);
-	
-//	DBCollection mConnUsersCollection;
-//	private static boolean mIsRunning = false;
-//	private static int mConnectedUsers = 0;
-//	private static int mTimeCounter = 0;
-//	private static boolean mCountConnectedUsersRunning = false;
-//	private static Thread mThreadConnectedUsers;
+
 
 	public ServerRequestListener() {
 
 		try {
 			mConnection = new Mongo();
 			mChartingCollection = mConnection.getDB("db_charting").getCollection("exchanges_server");
-//			mConnUsersCollection = mConnection.getDB("db_users").getCollection("connected_users");
-
-			//initializing the thread to count each 1 second the online users and save it into mongoDB
-//			mThreadConnectedUsers = new Thread(new CountConnectedUsersTask());
-
 		} catch (UnknownHostException ex) {
 			mLog.error(ex.getMessage());
 
 		}
 	}
-/*
-	class CountConnectedUsersTask implements Runnable {
-
-		@Override
-		public void run() {
-			while (mCountConnectedUsersRunning) {
-				//mongoDB logic
-				/*
-				DBObject lRecord = mConnUsersCollection.findOne(
-				new BasicDBObject().append("seconds", 0));
-				
-				if (null == lRecord) {
-				mConnUsersCollection.insert(new BasicDBObject().append("seconds", 0));
-				}
-				mConnUsersCollection.update(lRecord, new BasicDBObject()
-				.append("$set", new BasicDBObject().append("s" + mTimeCounter, mConnectedUsers)));
-				
-				/* a mechanism should be implemented in order to insert into mongoDB
-				For example: 
-				Seconds [{0:5, 1:7, 2:5, 3:9, 4:150, 5:90, 6:95 ... 59:11}];
-				Minutes 0 1 2 3 4 5 6 ... 59
-				Hours   0 1 2 3 4 5 6 ... 23
-				Days    1 2 3 4 5 6 7 ... 31
-				Months  
-				* 
-			}
-			try {
-				Thread.sleep(1000);
-				mTimeCounter++;
-				if (mTimeCounter >= 60) {
-					mTimeCounter = 0;
-				}
-			} catch (InterruptedException ex) {
-			}
-		}
-	}
-*/
-
-	@Override
-	public void processClosed(WebSocketServerEvent wsse) {
-//		mConnectedUsers--;
-	}
-
-	@Override
-	public void processOpened(WebSocketServerEvent wsse) {
-//		mConnectedUsers++;
-//		if (!mIsRunning) {
-//			mThreadConnectedUsers.start();
-//			mIsRunning = true;
-//		}
-//		
-	}
-
+	
 	@Override
 	public void processPacket(WebSocketServerEvent wsse, WebSocketPacket wsp) {
 		SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
@@ -126,18 +62,15 @@ public class ServerRequestListener implements WebSocketServerListener {
 			lRecord = mChartingCollection.findOne(new BasicDBObject().append("date", lToday));
 		}
 		mChartingCollection.update(lRecord, new BasicDBObject().append("$inc", new BasicDBObject().append("h" + String.valueOf(new Date().getHours()), 1)));
+	}
 
+	@Override
+	public void processOpened(WebSocketServerEvent aEvent) {
+		
+	}
 
+	@Override
+	public void processClosed(WebSocketServerEvent aEvent) {
+		
 	}
-	/*
-	public void destroy() {
-	try {
-	mCountConnectedUsersRunning = false;
-	mThreadConnectedUsers.join(1000);
-	mThreadConnectedUsers.stop();
-	} catch (Exception e) {
-	mLog.error(e.getMessage());
-	}
-	}
-	 */
 }
