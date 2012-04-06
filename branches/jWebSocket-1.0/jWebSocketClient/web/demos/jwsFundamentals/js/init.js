c3f81c3ecd764b8bf77be09ad8a947b648641439
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-04-06 09:23:06 +0000

    Fundamental's demo is completely finished, running and ready to be released!

--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsFundamentals/js/init.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsFundamentals/js/init.js
@@ -2,57 +2,35 @@
  * @author vbarzana
  */
 
-function init(){
+function init(  ) {
 	w                   = {};
 	mLog                = {};
 	mLog.isDebugEnabled = true;
-    
-	var lMessage = "This demo is being modified, it's not ready to use yet, we are \n\
-		creating a new set of demos for the jWebSocket 1.0 version, we apologize\n\
-		for the problems it can cause to you, please, refer to the old demos area\n\
-		in the navigation menu.\n\
-		<img src='../../res/img/under_construction.png'></img>";
-	
-	// dialog(aMessage, aTitle, aIsModal, aCloseFunction, aButtons, aIconType, aWidth)
-	dialog(lMessage, "Demo under construction", true, null, null, "", 400);
-	
-	//Each demo will configure its own callbacks to be passed to the login widget
-	var lCallbacks = {
-		OnOpen: function(aEvent){
-		},
-		OnClose: function(aEvent){
-		},
-		OnMessage: function(aEvent){
-		},
-		OnWelcome: function(aEvent){
-		},
-		OnGoodBye: function(aEvent){
-		}
-	};
     
 	// Options
 	// @maxLogLines: maximum number of lines that will be logged
 	// @linesToDelete: quantity of lines that will be deleted from 
 	// the log window each time the log exceeds the maxLogLines
-	$("#log_box").log({
+	$( "#log_box" ).log( {
 		maxLogLines: 200, 
 		linesToDelete: 20
-	});
+	} );
 	
-	$("#demo_box").auth(lCallbacks);
+	//starting the widget fundamentals
+	$("#demo_box").fundamentals();
 	
 	//configuring tooltip as we wish
-	$("[title]").tooltip({
+	$( "[title]" ).tooltip( {
 		position: "bottom center", 
-		onShow: function() {
-			var lTip = this.getTip();
-			var lTop = ("<div class='top'></div>");
-			var lMiddle = $("<div class='middle'></div>").text(lTip.text());
-			var lBottom = ("<div class='bottom'></div>");
-			lTip.html("").append(lTop).append(lMiddle).append(lBottom);
+		onShow: function(  ) {
+			var lTip = this.getTip(  );
+			var lTop = ( "<div class='top'></div>" );
+			var lMiddle = $( "<div class='middle'></div>" ).text( lTip.text(  ) );
+			var lBottom = ( "<div class='bottom'></div>" );
+			lTip.html( "" ).append( lTop ).append( lMiddle ).append( lBottom );
 		}
-	});
+	} );
 }
-$(document).ready(function(){
-	init();
-});
+$( document ).ready( function(  ) {
+	init(  );
+} );
--- branches/jWebSocket-1.0/jWebSocketClient/web/res/js/widgets/wAuth.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/res/js/widgets/wAuth.js
@@ -22,7 +22,7 @@ $.widget("jws.auth",{
 	_init:function(){
 		w.auth   = this;
 		
-		// WEBSOCKET CLIENT
+		// Stores the jWebSocketJSONClient
 		mWSC = null;
 		
 		w.auth.eLogoffArea		= w.auth.element.find("#logoff_area");
@@ -47,54 +47,131 @@ $.widget("jws.auth",{
 		
 		w.auth.registerEvents();
 	},
+	
 	checkWebSocketSupport: function(){
-		if( jws.browserSupportsWebSockets() ) {
-			mWSC = new jws.jWebSocketJSONClient();
+		if( jws.browserSupportsWebSockets( ) ) {
+			mWSC = new jws.jWebSocketJSONClient( );
+			// Setting the type of WebSocket
+			w.auth.eWebSocketType.text("WebSocket: " + 
+				(jws.browserSupportsNativeWebSockets ? "(native)" : "(flashbridge)" ));
 		} else {
-			//disable all buttons
-			//        $( "#login_button" ).attr( "disabled", "disabled" );
 			var lMsg = jws.MSG_WS_NOT_SUPPORTED;
 			alert( lMsg );
-			log( lMsg );
+			if( mLog.isDebugEnabled ) {
+				log( lMsg );
+			}
 		}
-	},    
-	registerEvents: function(){
-		//adding click functions
-		w.auth.eLoginButton.click(w.auth.logon);
-		w.auth.eLogoffButton.click(w.auth.disconnect);
-		
-		w.auth.eConnectButton.click(w.auth.connect);
-		w.auth.eDisConnectButton.click(w.auth.disconnect);
 	},
-	logon: function(){
-		var lURL = (w.auth.options.lURL)?w.auth.options.lURL:jws.getDefaultServerURL();
-        
-		var lUsername = w.auth.eUsername.val();
-		var lPassword = w.auth.ePassword.val();
-		if(lUsername == "" || lPassword == ""){
-			dialog("Incorrect Data", "User or password are not correct, please check");
-			return;
-		}
-		// optionally reset the password to force
-		// re-typing after a disconnect or logout.
-		// w.auth.ePassword.value = "";
-        
-		if(mLog.isDebugEnabled)
-			log( "Connecting to " + lURL + " and logging in as '" + lUsername + "'..." );
-        
-		var lRes = mWSC.logon( lURL, lUsername, lPassword, {
-
-			// OnOpen callback
-			OnOpen: function( aEvent, aToken ) {
-				if(w.auth.options.OnOpen){
-					w.auth.options.OnOpen(aEvent);
+	
+	registerEvents: function() {
+		//adding click functions
+		w.auth.eLoginButton.click( function() {
+			// If there is not a connect button
+			if( !w.auth.eConnectButton.attr("id") ) {
+				// we open the connection and then login
+				w.auth.logon();
+			} else {
+				// must be connected, otherwise it won't work
+				w.auth.login();
+			}
+		});
+		w.auth.eLogoffButton.click( 
+			function(){
+				// If there is not a connect button
+				if( !w.auth.eConnectButton.attr("id") ) {
+					// logout and close the connection
+					w.auth.disconnect();
+				} else {
+					// just logout
+					w.auth.logoff();
 				}
-				if(mLog.isDebugEnabled)
+			}
+			);
+		
+		w.auth.eConnectButton.click( w.auth.connect );
+		w.auth.eDisConnectButton.click( w.auth.disconnect );
+		
+		w.auth.eUsername.keypress( w.auth.eUsernameKeypress );
+		w.auth.ePassword.keypress( w.auth.ePasswordKeypress );
+	},
+	
+	// Logs in, only if there is connection with the server, otherwise it won't work
+	login: function() {
+		if( mWSC ) {
+			if( mLog.isDebugEnabled ) {
+				log( "Logging in..." );
+			}
+			
+			try {
+				// you can choose the username entered by the user or 
+				// jWebSocket defalut jws.GUEST_USER_LOGINNAME
+				var lUsername = w.auth.eUsername.val( );
+				// jWebSocket defalut jws.GUEST_USER_PASSWORD
+				var lPassword = w.auth.ePassword.val( );
+		
+				var lRes = mWSC.login(
+					lUsername,
+					lPassword
+					);
+				
+				if( lRes.code == 0 ) {
+					if( mLog.isDebugEnabled ) {
+						log( "Asychronously waiting for response..." );
+					}
+					
+				} else {
+					if( mLog.isDebugEnabled ) {
+						log( lRes.msg );
+					}
+				}
+			} catch( ex ) {
+				console.log( ex.message );
+				if( mLog.isDebugEnabled ) {
+					log( "Exception: " + ex.message );
+				}
+			}
+		}
+	},
+	
+	getCallbacks: function(){
+		return {
+			// use JSON sub protocol
+			subProtocol:  (w.auth.options.subProtocol)?w.auth.options.subProtocol:jws.WS_SUBPROT_JSON,
+			// connection timeout in ms
+			openTimeout: (w.auth.options.timeout)?w.auth.options.timeout:3000,
+				
+			OnOpen: function( aEvent, aToken ) {
+				if( w.auth.options.OnOpen ){
+					w.auth.options.OnOpen( aEvent );
+				}
+				if( mLog.isDebugEnabled ) {
 					log( "<font style='color:#888'>jWebSocket connection established.</font>" );
-				//update statusbar client status
-				w.auth.eLogonArea.hide();
-				w.auth.eLogoffArea.fadeIn(200);
-				w.auth.eClientStatus.hide().removeClass("offline").addClass("online").text("connected").show();
+				}
+				
+				w.auth.eConnectButton.hide( );
+				w.auth.eDisConnectButton.show( );
+				
+				mIsConnected = true;
+				// Setting the status connected
+				w.auth.eClientStatus.attr( "class", "online" ).text( "connected" );
+			},
+		
+			// OnOpenTimeout callback
+			OnOpenTimeout: function( aEvent ) {
+				if(mLog.isDebugEnabled ) {
+					log( "Opening timeout exceeded!" );
+				}
+					
+				if( w.auth.options.OnOpenTimeout ){
+					w.auth.options.OnOpenTimeout( aEvent );
+				}
+					
+			},
+			// OnReconnecting callback
+			OnReconnecting: function( aEvent ) {
+				if(mLog.isDebugEnabled ) {
+					log( "Re-establishing jWebSocket connection..." );
+				}
 			},
 					
 			OnWelcome: function( aToken )  {
@@ -107,11 +184,11 @@ $.widget("jws.auth",{
 			},
 
 			OnGoodBye: function( aEvent ) {
-				if (w.auth.options.OnGoodBye) {
-					w.auth.options.OnGoodBye(aEvent);
+				if( w.auth.options.OnGoodBye ) {
+					w.auth.options.OnGoodBye( aEvent );
 				}
-				if (mLog.isDebugEnabled) {
-					log("<font style='color:red'>jWebSocket GoodBye received.</font>");
+				if( mLog.isDebugEnabled ) {
+					log( "<font style='color:red'>jWebSocket GoodBye received.</font>" );
 				}
 			},
 
@@ -122,17 +199,42 @@ $.widget("jws.auth",{
 					lDate = jws.tools.ISO2Date( aToken.date_val );
 				}
 				
-				log( "<font style='color:#888'>jWebSocket '" + aToken.type + "' token received, full message: '" + aEvent.data + "' " + lDate + "</font>" );
+				if( mLog.isDebugEnabled ) {
+					log( "<font style='color:#888'>jWebSocket '" + aToken.type 
+						+ "' token received, full message: '" + aEvent.data + "' " 
+						+ lDate + "</font>" );
+				}
 				
-				if("login" == aToken.reqType){
-					w.auth.eLogonArea.hide();
-					w.auth.eLogoffArea.fadeIn(300);
-									
-					w.auth.eUserInfoName.text(aToken.username);
-					w.auth.mUsername = aToken.username;
-					w.auth.eClientId.text("Client-ID: " + aToken.sourceId);
-					w.auth.eClientStatus.hide().removeClass("offline").removeClass("online").addClass("authenticated").text("authenticated").show();
-					w.auth.eWebSocketType.text("WebSocket: " + (jws.browserSupportsNativeWebSockets ? "(native)" : "(flashbridge)" ));
+				if( "login" == aToken.reqType ) {
+					if( aToken.code != -1 ) {
+						if( mLog.isDebugEnabled ) {
+							log( "<font style='color:green'>Successfully authenticated as: " 
+								+ aToken.username + "</font>");
+						}
+						w.auth.eLogonArea.hide();
+						w.auth.eLogoffArea.fadeIn(300);
+
+						w.auth.eUserInfoName.text(aToken.username);
+						w.auth.mUsername = aToken.username;
+						w.auth.eClientId.text("Client-ID: " + aToken.sourceId);
+						w.auth.eClientStatus.attr( "class", "authenticated").text("authenticated");
+					} else {
+						if( mLog.isDebugEnabled ) {
+							log( "<font style='color:red'><b>Error trying to login, \n\
+								invalid credentials, please, check your user or password</font>");
+						}
+					}
+				} else if( "logout" == aToken.reqType ) {
+					w.auth.eLogoffArea.hide( );
+					w.auth.eLogonArea.fadeIn( 200 );
+					
+					w.auth.eConnectButton.hide( );
+					w.auth.eDisConnectButton.show( );
+					
+					w.auth.mUsername = null;
+					w.auth.eUserInfoName.text( "" );
+					w.auth.eClientStatus.attr( "class", "online" ).text( "online" );
+					
 				}
 				if(w.auth.options.OnMessage) {
 					w.auth.options.OnMessage(aEvent, aToken);
@@ -140,136 +242,208 @@ $.widget("jws.auth",{
 			},
 			// OnClose callback
 			OnClose: function( aEvent ) {
-				if(mLog.isDebugEnabled)
+				if( mLog.isDebugEnabled ) {
 					log( "<font style='color:#888'>jWebSocket connection closed.</font>" );
-				
-				w.auth.eLogoffArea.hide();
-				w.auth.eLogonArea.fadeIn(200);
-				
+				}
+				w.auth.eLogoffArea.hide( );
+				w.auth.eLogonArea.fadeIn( 200 );
+			
+				w.auth.eDisConnectButton.hide( );
+				w.auth.eConnectButton.show( );
+			
 				w.auth.mUsername = null;
-				w.auth.eUserInfoName.text("");
-				w.auth.eClientId.text("Client-ID: -");
+				w.auth.eUserInfoName.text( "" );
+				w.auth.eClientId.text( "Client-ID: -" );
 				w.auth.eWebSocketType.text( "WebSocket: - " );
-				w.auth.eClientStatus.removeClass("online").removeClass("online").addClass("offline").text("disconnected");
-				w.auth.eUsername.focus();
+				w.auth.eClientStatus.attr( "class", "offline" ).text( "disconnected" );
+				w.auth.eUsername.focus( );
 				
-				if(w.auth.options.OnClose){
-					w.auth.options.OnClose(aEvent);
+				if( w.auth.options.OnClose ){
+					w.auth.options.OnClose( aEvent );
 				}
 			}
-		});
-		if(mLog.isDebugEnabled)
+		};
+	},
+	
+	// If there is not connection with the server, opens a connection and then 
+	// tries to log the user in the system
+	logon: function() {
+		var lURL = ( w.auth.options.lURL )? w.auth.options.lURL: jws.getDefaultServerURL( );
+        
+		var lUsername = w.auth.eUsername.val( );
+		var lPassword = w.auth.ePassword.val( );
+		
+		if( lUsername == "" || lPassword == "" ){
+			if( mLog.isDebugEnabled ) {
+				log("<font style='color:red'>User or password can not be empty,\n\
+						please check your login information.</font>")
+			}
+			return;
+		}
+		
+		if( mLog.isDebugEnabled ) {
+			log( "Connecting to " + lURL + " and logging in as '" + lUsername + "'..." );
+		}
+		
+		var lRes = mWSC.logon( lURL, lUsername, lPassword, w.auth.getCallbacks() );
+		
+		if( mLog.isDebugEnabled ) {
 			log( mWSC.resultToString( lRes ) );
-	}, 
-	logoff: function(){
+		}
+	},
+	
+	logoff: function( ){
 		if( mWSC ) {
-			if(mLog.isDebugEnabled)
-				log( "Logging off " + ( w.auth.mUsername != null ? "'" + w.auth.mUsername + "'" : "" ) + " and disconnecting..." );
+			if( mLog.isDebugEnabled ) {
+				log( "Logging off " + ( w.auth.mUsername != null ? "'" + 
+					w.auth.mUsername + "'" : "" ) + " and disconnecting..." );
+			}
 			// the timeout below  is optional,
 			// if you use it you'll get a good-bye message.
-			var lRes = mWSC.logout({
+			var lRes = mWSC.logout( {
 				timeout: 3000
-			});
+			} );
 			
-			if(mLog.isDebugEnabled)
+			if( mLog.isDebugEnabled ) {
 				log( mWSC.resultToString( lRes ) );
+			}
 		}
-        
-	}, 
-	connect: function() {
-		var lURL = (w.auth.options.lURL)?w.auth.options.lURL:jws.getDefaultServerURL();
+	},
+	
+	connect: function( ) {
+		var lURL = ( w.auth.options.lURL )?w.auth.options.lURL:jws.getDefaultServerURL( );
+		if( mLog.isDebugEnabled ) {
+			log( "Connecting to " + lURL + " ..." );
+		}
 		
-		log( "Connecting to " + lURL + " ..." );
-		
-		if( mWSC.isConnected()) {
-			log( "Already connected." );
+		if( mWSC.isConnected( ) ) {
+			if( mLog.isDebugEnabled ) {
+				log( "Already connected." );
+			}
 			return;
 		}
 
 		try {
-			mWSC.open( lURL, {
-
-				subProtocol: jws.WS_SUBPROT_JSON,
-
-				OnOpen: function( aEvent ) {
-					if(w.auth.options.OnOpen){
-						w.auth.options.OnOpen(aEvent);
-					}
-					
-					w.auth.eConnectButton.hide();
-					w.auth.eDisConnectButton.show();
-					
-					w.auth.eClientStatus.hide().removeClass("offline").removeClass("authenticated").addClass("online").text("online").show();
-					log( "jWebSocket connection established." );
-				},
-
-				OnWelcome: function( aEvent )  {
-					if( mWSC.isLoggedIn() ) {
-						w.auth.eUserInfoName.text(aToken.username);
-						w.auth.mUsername = aToken.username;
-						w.auth.eClientStatus.hide().removeClass("offline").removeClass("online").addClass("authenticated").text("authenticated").show();
-					}
-					log( "<font style=\"color:red\">jWebSocket Welcome received.</font>" );
-					w.auth.eClientId.text("Client-ID: " + ( mWSC.getId()));
-					w.auth.eWebSocketType.text("WebSocket: " + (jws.browserSupportsNativeWebSockets ? "(native)" : "(flashbridge)" ));
-					
-					if(w.auth.options.OnWelcome){
-						w.auth.options.OnWelcome(aEvent);
-					}
-				},
-
-				OnGoodBye: function( aEvent )  {
-					if(w.auth.options.OnGoodBye){
-						w.auth.options.OnGoodBye(aEvent);
-					}
-					log( "<font style=\"color:red\">jWebSocket GoodBye received.</font>" );
-				},
-
-				OnMessage: function( aEvent, aToken ) {
-					log( "jWebSocket message received: '" + aEvent.data + "'" );
-					
-					if(w.auth.options.OnMessage){
-						w.auth.options.OnMessage(aEvent);
-					}
-				},
-
-				OnClose: function( aEvent ) {
-					if(w.auth.options.OnClose){
-						w.auth.options.OnClose(aEvent);
-					}
-					w.auth.eDisConnectButton.hide();
-					w.auth.eConnectButton.show();
-					
-					log( "jWebSocket connection closed." );
-					
-					w.auth.eUserInfoName.text("");
-					w.auth.mUsername = null;
-					w.auth.eClientId.text("Client-ID: -");
-					w.auth.eWebSocketType.text( "WebSocket: - " );
-					w.auth.eClientStatus.removeClass("online").removeClass("online").addClass("offline").text("disconnected");
-					w.auth.eUsername.focus();
-				}
-			});
+			mWSC.open( lURL, w.auth.getCallbacks() );
 		} catch( ex ) {
 			console.log( ex );
-			log( "Exception: " + ex.message );
+			if( mLog.isDebugEnabled ) {
+				log( "Exception: " + ex.message );
+			}
 		}
-	}, 
+	},
+	
 	disconnect: function() {
 		if( mWSC ) {
-			log( "Disconnecting..." );
+			if( mLog.isDebugEnabled ) {
+				log( "Disconnecting..." );
+			}
 			try {
 				var lRes = mWSC.close({
 					timeout: 3000
 				});
 				
-				if( lRes.code == 0 ) {
-				} else {
-					log( lRes.msg );
+				if( lRes.code != 0 ) {
+					if( mLog.isDebugEnabled ) {
+						log( lRes.msg );
+					}
 				}
 			} catch( ex ) {
-				log( "Exception: " + ex.message );
+				console.log( ex );
+				if( mLog.isDebugEnabled ) {
+					log( "Exception: " + ex.message );
+				}
 			}
+		}
+	},
+	
+	auth: function() {
+		if( mWSC ) {
+			if( mLog.isDebugEnabled ) {
+				log( "Authenticating..." );
+			}
+			try {
+				var lRes = mWSC.systemLogon(
+					jws.GUEST_USER_LOGINNAME, 
+					jws.GUEST_USER_PASSWORD
+					);
+				if( lRes.code == 0 ) {
+					if( mLog.isDebugEnabled ) {
+						log( "Asychronously waiting for response..." );
+					}
+				} else {
+					if( mLog.isDebugEnabled ) {
+						log( lRes.msg );
+					}
+				}
+			} catch( ex ) {
+				console.log( ex.message );
+				if( mLog.isDebugEnabled ) {
+					log( "Exception: " + ex.message );
+				}
+			}
+		}
+	},
+
+	deauth: function() {
+		if( mWSC ) {
+			if( mLog.isDebugEnabled ) {
+				log( "Deauthenticating..." );
+			}
+			try {
+				var lRes = mWSC.systemLogoff();
+				if( lRes.code == 0 ) {
+					if( mLog.isDebugEnabled ) {
+						log( "Asychronously waiting for response..." );
+					}
+				} else {
+					if( mLog.isDebugEnabled ) {
+						log( lRes.msg );
+					}
+				}
+			} catch( ex ) {
+				console.log( ex.message );
+				if( mLog.isDebugEnabled ) {
+					log( "Exception: " + ex.message );
+				}
+			}
+		}
+	},
+
+	getAuth: function() {
+		if( mWSC ) {
+			if( mLog.isDebugEnabled ) {
+				log( "Getting authorities..." );
+			}
+			try {
+				var lRes = mWSC.systemGetAuthorities();
+				if( lRes.code == 0 ) {
+					if( mLog.isDebugEnabled ) {
+						log( "Asychronously waiting for response..." );
+					}
+				} else {
+					if( mLog.isDebugEnabled ) {
+						log( lRes.msg );
+					}
+				}
+			} catch( ex ) {
+				if( mLog.isDebugEnabled ) {
+					log( "Exception: " + ex.message );
+				}
+			}
+		}
+	},
+	
+	// EVENTS FUNCTIONS
+	eUsernameKeypress: function( aEvent ) {
+		if( aEvent.keyCode == 13 || aEvent.keyChar == 13 ) {
+			w.auth.ePassword.focus();
+		}
+	},
+	
+	ePasswordKeypress: function( aEvent ) {
+		if( aEvent.keyCode == 13 || aEvent.keyChar == 13 ) {
+			w.auth.logon();
 		}
 	}
 });
