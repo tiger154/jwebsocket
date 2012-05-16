abbb3c91cd8dd7bcf3f3b34a4ae5d7efc104694a
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-05-16 14:16:03 +0000

    jQueryPlugIn already fixed!

--- jWebSocketPlugIns/jWebSocketJQueryPlugIn/src/main/java/org/jwebsocket/plugins/jquery/JQueryPlugIn.java
+++ jWebSocketPlugIns/jWebSocketJQueryPlugIn/src/main/java/org/jwebsocket/plugins/jquery/JQueryPlugIn.java
@@ -14,20 +14,34 @@
 //	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
 //	---------------------------------------------------------------------------
 /**
+ * Sends the server time each second to a set of registered clients. The logic
+ * of this plugIn is implemented in the StreamingPlugIn in the following class:
+ * <tt>org.jwebsocket.plugins.streaming.StreamingPlugIn</tt>, this plugIn is not
+ * used anymore by jWebSocket, will be removed very soon.
  *
  * @author Victor and Carlos
  */
 package org.jwebsocket.plugins.jquery;
 
-import java.util.Timer;
+import java.util.Collection;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javolution.util.FastList;
 import org.jwebsocket.api.PluginConfiguration;
+import org.jwebsocket.api.WebSocketConnector;
 import org.jwebsocket.api.WebSocketEngine;
+import org.jwebsocket.kit.CloseReason;
+import org.jwebsocket.kit.PlugInResponse;
 import org.jwebsocket.plugins.TokenPlugIn;
+import org.jwebsocket.token.Token;
+import org.jwebsocket.token.TokenFactory;
 
 public class JQueryPlugIn extends TokenPlugIn {
-	// a timer to execute a task each some time
 
-	private Timer mTimer = new Timer("jWebSocket jQuery Plugin Timer");
+	private static Collection<WebSocketConnector> mClients = new FastList<WebSocketConnector>().shared();
+	private static Thread mServerTimeThread;
+	private static boolean mIsServerTimeRunning = false;
 
 	public JQueryPlugIn(PluginConfiguration aConfiguration) {
 		super(aConfiguration);
@@ -36,8 +50,79 @@ public class JQueryPlugIn extends TokenPlugIn {
 	}
 
 	@Override
-	public void engineStarted(WebSocketEngine aEngine) {
-		super.engineStarted(aEngine);
-		mTimer.schedule(new UpdateTimeTask(getServer(), getNamespace()), 1000);
+	public void engineStopped(WebSocketEngine aEngine) {
+		try {
+			mIsServerTimeRunning = false;
+			mServerTimeThread.join(100);
+			mServerTimeThread.stop();
+		} catch (InterruptedException ex) {
+			Logger.getLogger(JQueryPlugIn.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	@Override
+	public void connectorStopped(WebSocketConnector aConnector, CloseReason aCloseReason) {
+		if (!mClients.isEmpty()) {
+			mClients.remove(aConnector);
+			try {
+				mIsServerTimeRunning = false;
+				mServerTimeThread.join();
+				mServerTimeThread.stop();
+			} catch (InterruptedException ex) {
+				Logger.getLogger(JQueryPlugIn.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+	}
+
+	@Override
+	public void processToken(PlugInResponse aResponse, WebSocketConnector aConnector, Token aToken) {
+		if (getNamespace().equals(aToken.getNS())) {
+			if ("register".equals(aToken.getType())) {
+				if (mClients.isEmpty()) {
+					mIsServerTimeRunning = true;
+					mServerTimeThread = new Thread(new getServerTime(), "jWebSocket Monitoring Plug-in jQuery time");
+					mServerTimeThread.start();
+				}
+				mClients.add(aConnector);
+			} else if ("unregister".equals(aToken.getType())) {
+				if (mClients.contains(aConnector)) {
+					mClients.remove(aConnector);
+				}
+				if (mClients.isEmpty()) {
+					try {
+						mIsServerTimeRunning = false;
+						mServerTimeThread.join();
+						mServerTimeThread.stop();
+					} catch (InterruptedException ex) {
+						Logger.getLogger(JQueryPlugIn.class.getName()).log(Level.SEVERE, null, ex);
+					}
+				}
+			}
+		}
+	}
+
+	class getServerTime implements Runnable {
+
+		@Override
+		public void run() {
+			Token lToken = TokenFactory.createToken(getNamespace(), "datetime");
+			Date d;
+			while (mIsServerTimeRunning) {
+				if (mClients != null && !mClients.isEmpty()) {
+					d = new Date();
+					lToken.setInteger("hours", d.getHours());
+					lToken.setInteger("minutes", d.getMinutes());
+					lToken.setInteger("seconds", d.getSeconds());
+
+					for (WebSocketConnector lConnector : mClients) {
+						getServer().sendToken(lConnector, lToken);
+					}
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException ex) {
+					}
+				}
+			}
+		}
 	}
 }
