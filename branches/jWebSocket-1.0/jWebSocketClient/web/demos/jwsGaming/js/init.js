f7c86a4f5978b6d9f2283b0b8db1bdf8d7063c8c
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-07-12 02:47:40 +0000

    jwsGaming demo almost finished!

--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsGaming/js/init.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsGaming/js/init.js
@@ -7,49 +7,29 @@ function init(){
 	mLog                = {};
 	mLog.isDebugEnabled = true;
     
-	var lMessage = "This demo is being modified, it's not ready to use yet, we are \n\
-		creating a new set of demos for the jWebSocket 1.0 version, we apologize\n\
-		for the problems it can cause to you, please, refer to the old demos area\n\
-		in the navigation menu.\n\
-		<img src='../../res/img/under_construction.png'></img>";
+	$( "#demo_box" ).gaming();
 	
-	// dialog(aMessage, aTitle, aIsModal, aCloseFunction, aButtons, aIconType, aWidth)
-	dialog(lMessage, "Demo under construction", true, null, null, "", 400);
-	
-	//Each demo will configure its own callbacks to be passed to the login widget
-	var lCallbacks = {
-		OnOpen: function(aEvent){
-		},
-		OnClose: function(aEvent){
-		},
-		OnMessage: function(aEvent){
-		},
-		OnWelcome: function(aEvent){
-		},
-		OnGoodBye: function(aEvent){
-		}
-	};
-    
 	// Options
 	// @maxLogLines: maximum number of lines that will be logged
 	// @linesToDelete: quantity of lines that will be deleted from 
 	// the log window each time the log exceeds the maxLogLines
-	$("#log_box").log({
+	$( "#log_box" ).log({
 		maxLogLines: 200, 
 		linesToDelete: 20
 	});
 	
-	$("#demo_box").auth(lCallbacks);
-	
 	//configuring tooltip as we wish
-	$("[title]").tooltip({
+	$( "[title]" ).tooltip({
 		position: "top center", 
 		onShow: function() {
 			var lTip = this.getTip();
-			var lTop = ("<div class='top'></div>");
-			var lMiddle = $("<div class='middle'></div>").text(lTip.text());
-			var lBottom = ("<div class='bottom'></div>");
-			lTip.html("").append(lTop).append(lMiddle).append(lBottom);
+			var lTop = ( "<div class='top'></div>" );
+			var lMiddle = $( "<div class='middle'></div>" ).text( lTip.text() );
+			var lBottom = ( "<div class='bottom'></div>" );
+			lTip.html( "" ).append( lTop ).append( lMiddle ).append( lBottom );
+			lTip.mouseover( function(){
+				$( this ).hide();
+			});
 		}
 	});
 }
