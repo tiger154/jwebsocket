230e3974a27afcbdcb6b36870a302951dc11fd4f
Artemisa <Artemisa@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2012-08-23 16:06:20 +0000

    Fixes in Tomcat demo and version update

--- branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/config/JWebSocketServerConstants.java
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/config/JWebSocketServerConstants.java
@@ -25,7 +25,7 @@ public final class JWebSocketServerConstants {
 	/**
 	 * Current version string of the jWebSocket package.
 	 */
-	public static final String VERSION_STR = "1.0 beta 9 (nightly build 20613)";
+	public static final String VERSION_STR = "1.0 beta 10 (nightly build 20822)";
 	/**
 	 * Name space base for tokens and plug-ins.
 	 */
