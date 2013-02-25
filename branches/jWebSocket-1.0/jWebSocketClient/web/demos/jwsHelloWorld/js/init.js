bad632abbc2f172d701cf80d8eeefe642f69df3e
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-02-25 17:35:56 +0000

    jwsHelloWorld Demo almost ready, just pending a Review from Alexander through the code, some functionalities of file load and file save does not work, we should check this

--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsHelloWorld/js/init.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsHelloWorld/js/init.js
@@ -1,49 +1,37 @@
-/* 
+/*
+ * jWebSocket HelloWorld Initialization script 
  * @author vbarzana
  */
 
-function init(){
-	w                   = {};
-	mLog                = {};
+function init( ) {
+	w                   = { };
+	mLog                = { };
 	mLog.isDebugEnabled = true;
 	
-	//Each demo will configure its own callbacks to be passed to the login widget
-	var lCallbacks = {
-		OnOpen: function(aEvent){
-		},
-		OnClose: function(aEvent){
-		},
-		// If not OnMessage given, will be used the default callback
-//		OnMessage: function(aEvent, aToken){
-//			
-//		},
-		OnWelcome: function(aEvent){
-		},
-		OnGoodBye: function(aEvent){
-		}
-	};
-    
-	// Options
-	// @maxLogLines: maximum number of lines that will be logged
-	// @linesToDelete: quantity of lines that will be deleted from 
-	// the log window each time the log exceeds the maxLogLines
-	$("#log_box").log({
-		maxLogLines: 200, 
-		linesToDelete: 20
+	// Setting the styles to the buttons, avoiding to fill the HTML code 
+	// with unnecessary data
+	$( '.button' ).each( function( ) {
+		var lBtn = $( this );
+		var lRightClass = lBtn.hasClass( 'download' ) ? 'r_download' : 'btn_right';
+		lBtn.attr( "class", "button onmouseup" )
+		.attr( "onmouseover", "this.className='button onmouseover'" )
+		.attr( "onmousedown", "this.className='button onmousedown'"  )
+		.attr( "onmouseup", "this.className='button onmouseup'"  )
+		.attr( "onmouseout", "this.className='button onmouseout'"  )
+		.attr( "onclick", "this.className='button onmouseover'"  );
+		lBtn.html( '<div class="btn_left"/>'+'<div class="btn_center">' +
+			lBtn.html( )+ '</div>' + '<div class="' + lRightClass + '"></div>' );
 	});
 	
-	$("#demo_box").auth(lCallbacks);
-	$("#demo_box").helloWorld();
-	
 	//configuring tooltip as we wish
-	$("[title]").tooltip({
+	$( "[title]" ).tooltip({
 		position: "bottom center", 
-		onShow: function() {
-			var lTip = this.getTip();
-			var lTop = ("<div class='top'></div>");
-			var lMiddle = $("<div class='middle'></div>").text(lTip.text());
-			var lBottom = ("<div class='bottom'></div>");
-			lTip.html("").append(lTop).append(lMiddle).append(lBottom);
+		onShow: function( ) {
+			var lTip = this.getTip( );
+			var lTop = ( "<div class='top'></div>" );
+			var lMiddle = $( "<div class='middle'></div>" ).text( lTip.text( ) );
+			var lBottom = ( "<div class='bottom'></div>" );
+			lTip.html( "" ).append( lTop ).append( lMiddle ).append( lBottom );
 			this.getTrigger( ).mouseout( function( ) {
 				lTip.hide( ).hide( );
 			});
@@ -53,34 +41,20 @@ function init(){
 		}
 	});
 	
-	checkWebSocketSupport();
+	// Initialyzing the widgets
+	
+	// Options
+	// @maxLogLines: maximum number of lines that will be loggeds
+	// @linesToDelete: quantity of lines that will be deleted from 
+	// the log window each time the log exceeds the maxLogLines
+	$("#log_box").log({
+		maxLogLines: 200,
+		linesToDelete: 20
+	});
+	
+	$( "#demo_box" ).helloWorld( );
 }
 
-function checkWebSocketSupport(){
-	if( jws.browserSupportsWebSockets() ) {
-		lWSC = new jws.jWebSocketJSONClient({
-			OnWelcome: ""
-		});
-        
-        
-		lWSC.setSamplesCallbacks({
-			OnSamplesServerTime: getServerTimeCallback
-		});
-		lWSC.setFileSystemCallbacks({
-			OnFileLoaded: onFileLoadedObs,
-			OnFileSaved: onFileSavedObs,
-			OnFileError: onFileErrorObs
-		});
-	} else {
-		//disable all buttons
-		//        $( "#login_button" ).attr( "disabled", "disabled" );
-		var lMsg = jws.MSG_WS_NOT_SUPPORTED;
-		alert( lMsg );
-		if(mLog.isDebugEnabled)
-			log( lMsg );
-	}
-}
-
-$(document).ready(function(){
-	init();
+$( document ).ready( function( ) {
+	init( );
 });
