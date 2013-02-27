c0ffdce9b65cc6f28ab73c348208d4de3875f826
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-02-27 19:35:33 +0000

    jwsStreaming demo ready!

--- jWebSocketClient/web/demos/jwsStreaming/js/init.js
+++ jWebSocketClient/web/demos/jwsStreaming/js/init.js
@@ -1,31 +1,36 @@
 /* 
+ * jWebSocket Streaming Demo Initialization script
  * @author vbarzana
  */
 
-function init(  ) {
+
+function init( ) {
 	w                   = {};
 	mLog                = {};
 	mLog.isDebugEnabled = true;
     
-	// Options
-	// @maxLogLines: maximum number of lines that will be logged
-	// @linesToDelete: quantity of lines that will be deleted from 
-	// the log window each time the log exceeds the maxLogLines
-	$( "#log_box" ).log({
-		maxLogLines: 200, 
-		linesToDelete: 20
+	// Setting the styles to the buttons, avoiding to fill the HTML code 
+	// with unnecessary data
+	$( '.button' ).each( function( ) {
+		var lBtn = $( this );
+		var lRightClass = lBtn.hasClass( 'download' ) ? 'r_download' : 'btn_right';
+		lBtn.attr( "class", "button onmouseup" )
+		.attr( "onmouseover", "this.className='button onmouseover'" )
+		.attr( "onmousedown", "this.className='button onmousedown'"  )
+		.attr( "onmouseup", "this.className='button onmouseup'"  )
+		.attr( "onmouseout", "this.className='button onmouseout'"  )
+		.attr( "onclick", "this.className='button onmouseover'"  );
+		lBtn.html( '<div class="btn_left"/>'+'<div class="btn_center">' +
+			lBtn.html( )+ '</div>' + '<div class="' + lRightClass + '"></div>' );
 	});
 	
-	//starting the widget Streaming
-	$( "#demo_box" ).streaming();
-	
-	//configuring tooltip as we wish
-	$( "[title]" ).tooltip( {
+	//configuring tooltip
+	$( "[title]" ).tooltip({
 		position: "bottom center", 
-		onShow: function(  ) {
-			var lTip = this.getTip(  );
+		onShow: function( ) {
+			var lTip = this.getTip( );
 			var lTop = ( "<div class='top'></div>" );
-			var lMiddle = $( "<div class='middle'></div>" ).text( lTip.text(  ) );
+			var lMiddle = $( "<div class='middle'></div>" ).text( lTip.text( ) );
 			var lBottom = ( "<div class='bottom'></div>" );
 			lTip.html( "" ).append( lTop ).append( lMiddle ).append( lBottom );
 			this.getTrigger( ).mouseout( function( ) {
@@ -35,8 +40,22 @@ function init(  ) {
 				lTip.show( );
 			});
 		}
-	} );
+	});
+	
+	
+	// Initializing the log widget to allow logging in the logs area
+	// @maxLogLines: maximum number of lines that will be logged
+	// @linesToDelete: quantity of lines that will be deleted from 
+	// the log window each time the log exceeds the maxLogLines
+	$( "#log_box" ).log({
+		maxLogLines: 200, 
+		linesToDelete: 20
+	});
+	
+	// Initializing the Streaming widget
+	$( "#demo_box" ).streaming( );
 }
-$( document ).ready( function(  ) {
-	init(  );
-} );
+
+$( document ).ready( function( ) {
+	init( );
+});
