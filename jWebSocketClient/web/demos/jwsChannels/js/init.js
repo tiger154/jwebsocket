818cb613cada2b4411113abbbd6b230e8b75e301
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-02-27 20:33:30 +0000

    Channels demo ready!

--- jWebSocketClient/web/demos/jwsChannels/js/init.js
+++ jWebSocketClient/web/demos/jwsChannels/js/init.js
@@ -1,24 +1,30 @@
 /* 
+ * jWebSocket Fundamentals Initialization script
  * @author vbarzana
  */
 
-function init(){
+function init( ) {
 	w                   = {};
 	mLog                = {};
 	mLog.isDebugEnabled = true;
     
-	$( "#demo_box" ).channels();
-	
-	// Options
-	// @maxLogLines: maximum number of lines that will be logged
-	// @linesToDelete: quantity of lines that will be deleted from 
-	// the log window each time the log exceeds the maxLogLines
-	$( "#log_box" ).log({
-		maxLogLines: 200, 
-		linesToDelete: 20
+	// Setting the styles to the buttons, avoiding to fill the HTML code 
+	// with unnecessary data
+	$( '.button' ).each( function( ) {
+		var lBtn = $( this );
+		var lRightClass = lBtn.hasClass( 'download' ) ? 'r_download' : 'btn_right';
+		lBtn.attr( "class", "button onmouseup" )
+		.attr( "onmouseover", "this.className='button onmouseover'" )
+		.attr( "onmousedown", "this.className='button onmousedown'"  )
+		.attr( "onmouseup", "this.className='button onmouseup'"  )
+		.attr( "onmouseout", "this.className='button onmouseout'"  )
+		.attr( "onclick", "this.className='button onmouseover'"  );
+		lBtn.html( '<div class="btn_left"/>'+'<div class="btn_center">' +
+			lBtn.html( )+ '</div>' + '<div class="' + lRightClass + '"></div>' );
 	});
-	//configuring tooltip as we wish
-	$( "[title]" ).tooltip( {
+	
+	//configuring tooltip
+	$( "[title]" ).tooltip({
 		position: "bottom center", 
 		onShow: function( ) {
 			var lTip = this.getTip( );
@@ -34,8 +40,21 @@ function init(){
 			});
 		}
 	});
+	
+	
+	// Initializing the log widget to allow logging in the logs area
+	// @maxLogLines: maximum number of lines that will be logged
+	// @linesToDelete: quantity of lines that will be deleted from 
+	// the log window each time the log exceeds the maxLogLines
+	$( "#log_box" ).log({
+		maxLogLines: 200, 
+		linesToDelete: 20
+	});
+	
+	$( "#demo_box" ).channels( );
+	
 }
 
-$(document).ready(function(){
-	init();
+$( document ).ready( function( ) {
+	init( );
 });
