7943a993bd7304debe37c5d7d6b9531b53e8f236
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-03-01 13:56:09 +0000

    Updated JavaScript client files to new Apache license

--- jWebSocketClient/web/res/js/jwsWorker.js
+++ jWebSocketClient/web/res/js/jwsWorker.js
@@ -1,23 +1,24 @@
 //	---------------------------------------------------------------------------
-//	jWebSocket Worker (supports multithreading and background processes
-//	on the browser clients, if they support the HTML5 WebWorker standard)
-//	Copyright (c) 2011 Alexander Schulze, Innotrade GmbH, Herzogenrath
+//	jWebSocket WebWorker Support (Community Edition, CE)
+//	(supports multithreading and background processes on browser clients,
+//	 given if they already support the HTML5 WebWorker standard)
 //	---------------------------------------------------------------------------
-//	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU Lesser General Public License as published by the
-//	Free Software Foundation; either version 3 of the License, or (at your
-//	option) any later version.
-//	This program is distributed in the hope that it will be useful, but WITHOUT
-//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-//	more details.
-//	You should have received a copy of the GNU Lesser General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org), Germany (NRW), Herzogenrath
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 //	---------------------------------------------------------------------------
 
 //:i:en:This method is executed if postmessage is invoked by the caller.
-//:i:de:Über aEvent können von der Applikation Daten _
-//:i:de:an den Thread übergeben werden
 onmessage = function( aEvent ) {
 	// console.log( "started!" );
 	// here computationally intensive processes can be run as thread.
@@ -28,5 +29,4 @@ onmessage = function( aEvent ) {
 	// run the method and return the result via postmessage to the application.
 	// in the application the onmessage listener of the worker is invoked
 	postMessage( lMethod( aEvent.data.args ) );
-
 };
