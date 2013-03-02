4ae0e906764d5fb503fa1d32a7fc4add3d424c37
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-03-02 14:53:17 +0000

    Fixed new Versioning and licensing of JDBC plug-in, formatting.

--- branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketJDBCPlugIn/src/main/java/org/jwebsocket/plugins/jdbc/JDBCPlugIn.java
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketJDBCPlugIn/src/main/java/org/jwebsocket/plugins/jdbc/JDBCPlugIn.java
@@ -1,21 +1,20 @@
 //	---------------------------------------------------------------------------
-//	jWebSocket - jWebSocket JDBC Plug-In
-//  Copyright (c) 2010 Innotrade GmbH, jWebSocket.org
+//	jWebSocket - jWebSocket JDBC Plug-In (Community Edition, CE)
 //	---------------------------------------------------------------------------
-//  THIS CODE IS FOR RESEARCH, EVALUATION AND TEST PURPOSES ONLY!
-//  THIS CODE MAY BE SUBJECT TO CHANGES WITHOUT ANY NOTIFICATION!
-//	THIS CODE IS NOT YET SECURE AND MAY NOT BE USED FOR PRODUCTION ENVIRONMENTS!
-//	---------------------------------------------------------------------------
-//	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU Lesser General Public License as published by the
-//	Free Software Foundation; either version 3 of the License, or (at your
-//	option) any later version.
-//	This program is distributed in the hope that it will be useful, but WITHOUT
-//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-//	more details.
-//	You should have received a copy of the GNU Lesser General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 //	---------------------------------------------------------------------------
 package org.jwebsocket.plugins.jdbc;
 
@@ -33,6 +32,7 @@ import org.apache.log4j.Logger;
 import org.jwebsocket.api.IBasicStorage;
 import org.jwebsocket.api.PluginConfiguration;
 import org.jwebsocket.api.WebSocketConnector;
+import org.jwebsocket.config.JWebSocketCommonConstants;
 import org.jwebsocket.config.JWebSocketServerConstants;
 import org.jwebsocket.kit.PlugInResponse;
 import org.jwebsocket.logging.Logging;
@@ -54,6 +54,11 @@ public class JDBCPlugIn extends TokenPlugIn {
 	// if namespace changed updateSQL client plug-in accordingly!
 	private static final String NS_JDBC = JWebSocketServerConstants.NS_BASE + ".plugins.jdbc";
 	private final static String VERSION = "1.0.0";
+	private final static String VENDOR = JWebSocketCommonConstants.VENDOR_CE;
+	private final static String LABEL = "jWebSocket JDBCPlugIn";
+	private final static String COPYRIGHT = JWebSocketCommonConstants.COPYRIGHT_CE;
+	private final static String LICENSE = JWebSocketCommonConstants.LICENSE_CE;
+	private final static String DESCRIPTION = "jWebSocket JDBCPlugIn - Community Edition";
 	private IBasicStorage mCache = null;
 	private int mConnValTimeout = 300;
 	private static ApplicationContext mBeanFactory;
@@ -99,6 +104,31 @@ public class JDBCPlugIn extends TokenPlugIn {
 	@Override
 	public String getVersion() {
 		return VERSION;
+	}
+
+	@Override
+	public String getLabel() {
+		return LABEL;
+	}
+
+	@Override
+	public String getDescription() {
+		return DESCRIPTION;
+	}
+
+	@Override
+	public String getVendor() {
+		return VENDOR;
+	}
+
+	@Override
+	public String getCopyright() {
+		return COPYRIGHT;
+	}
+
+	@Override
+	public String getLicense() {
+		return LICENSE;
 	}
 
 	@Override
@@ -158,10 +188,18 @@ public class JDBCPlugIn extends TokenPlugIn {
 		}
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public DataSource getNativeDataSource() {
 		return mNativeAccess.getDataSource();
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	public Class getJDBCTools() {
 		return JDBCTools.class;
 	}
--- branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketJDBCPlugIn/src/main/java/org/jwebsocket/plugins/jdbc/JDBCTools.java
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketJDBCPlugIn/src/main/java/org/jwebsocket/plugins/jdbc/JDBCTools.java
@@ -1,17 +1,20 @@
 //	---------------------------------------------------------------------------
-//	jWebSocket - JDBCTools
-//	Copyright (c) 2010 Innotrade GmbH (http://jWebSocket.org)
+//	jWebSocket - JDBCTools (Community Edition, CE)
 //	---------------------------------------------------------------------------
-//	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU Lesser General Public License as published by the
-//	Free Software Foundation; either version 3 of the License, or (at your
-//	option) any later version.
-//	This program is distributed in the hope that it will be useful, but WITHOUT
-//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
-//  for more details.
-//	You should have received a copy of the GNU Lesser General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 //	---------------------------------------------------------------------------
 package org.jwebsocket.plugins.jdbc;
 
@@ -36,7 +39,7 @@ import org.springframework.jdbc.support.rowset.SqlRowSetMetaData;
 public class JDBCTools {
 
 	/**
-	 * 
+	 *
 	 * @param aData
 	 * @return
 	 */
@@ -45,7 +48,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aClassName
 	 * @return
 	 */
@@ -64,7 +67,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aJavaType
 	 * @param aMetaData
 	 * @return
@@ -93,7 +96,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aJavaType
 	 * @param aMetaData
 	 * @return
@@ -122,7 +125,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aJavaType
 	 * @param aMetaData
 	 * @return
@@ -167,7 +170,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aRowSet
 	 * @param lColCount
 	 * @return
@@ -222,11 +225,11 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aRowSet
 	 * @return
-	 * @throws InvalidResultSetAccessException 
-	 * @throws SerialException  
+	 * @throws InvalidResultSetAccessException
+	 * @throws SerialException
 	 */
 	public static Token resultSetToToken(SqlRowSet aRowSet)
 			throws InvalidResultSetAccessException, SerialException {
@@ -278,7 +281,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aFieldList
 	 * @return
 	 */
@@ -297,7 +300,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aFieldList
 	 * @return
 	 */
@@ -306,7 +309,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aFieldList
 	 * @return
 	 */
@@ -315,7 +318,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param lField
 	 * @return
 	 */
@@ -331,7 +334,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aValueList
 	 * @return
 	 */
@@ -350,7 +353,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aValueList
 	 * @return
 	 */
@@ -359,7 +362,7 @@ public class JDBCTools {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param aValueList
 	 * @return
 	 */
