808406d855479281fce3b6db725bfe1fc5a6eae8
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-03-03 22:07:17 +0000

    Fixed new versioning and licensing of Shared Canvas plug-in, formatting and fixed name space processing

--- branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketSharedCanvasPlugIn/src/main/java/org/jwebsocket/sharedcanvas/SharedCanvasPlugIn.java
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketSharedCanvasPlugIn/src/main/java/org/jwebsocket/sharedcanvas/SharedCanvasPlugIn.java
@@ -1,5 +1,5 @@
 //	---------------------------------------------------------------------------
-//	jWebSocket - jWebSocket SharedCanvas Plug-in (Community Edition, CE)
+//	jWebSocket SharedCanvas Plug-in (Community Edition, CE)
 //	---------------------------------------------------------------------------
 //	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
 //  Alexander Schulze, Germany (NRW)
