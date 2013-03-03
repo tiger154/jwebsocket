05eae697081e21d3a48fdd3eed9665ec86916180
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-03-03 12:29:18 +0000

    Fixed new licensing of Tomcat engine, formatting

--- jWebSocketEngines/jWebSocketTomcatEngine/src/main/java/org/jwebsocket/tomcat/TomcatServlet.java
+++ jWebSocketEngines/jWebSocketTomcatEngine/src/main/java/org/jwebsocket/tomcat/TomcatServlet.java
@@ -1,17 +1,20 @@
 //	---------------------------------------------------------------------------
-//	jWebSocket - Tomcat WebSocket Servlet, from Tomcat 7.0.35
-//	Copyright (c) 2012 jWebSocket.org, Innotrade GmbH
+//	jWebSocket - Tomcat WebSocket Servlet, from Tomcat 7.0.35 (Community Edition, CE)
 //	---------------------------------------------------------------------------
-//	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU Lesser General Public License as published by the
-//	Free Software Foundation; either version 3 of the License, or (at your
-//	option) any later version.
-//	This program is distributed in the hope that it will be useful, but WITHOUT
-//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-//	more details.
-//	You should have received a copy of the GNU Lesser General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 //	---------------------------------------------------------------------------
 package org.jwebsocket.tomcat;
 
@@ -40,10 +43,18 @@ public class TomcatServlet extends WebSocketServlet {
 	private ThreadLocal<HttpServletRequest> mRequestContainer = new ThreadLocal<HttpServletRequest>();
 	private TomcatEngine mEngine;
 
+	/**
+	 *
+	 * @return
+	 */
 	public boolean isRunningEmbedded() {
 		return ContextListener.isRunningEmbedded();
 	}
 
+	/**
+	 *
+	 * @throws ServletException
+	 */
 	@Override
 	public void init() throws ServletException {
 		if (JWebSocketInstance.STARTED == JWebSocketInstance.getStatus()) {
@@ -64,24 +75,41 @@ public class TomcatServlet extends WebSocketServlet {
 		}
 	}
 
+	/**
+	 *
+	 * @param aRequest
+	 * @param aResponse
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	@Override
 	protected void service(HttpServletRequest aRequest, HttpServletResponse aResponse) throws ServletException, IOException {
 		mRequestContainer.set(aRequest);
-		
+
 		// setting the session timeout
-		if (ContextListener.isRunningEmbedded()){
+		if (ContextListener.isRunningEmbedded()) {
 			aRequest.getSession().setMaxInactiveInterval(mEngine.getConfiguration().getTimeout());
 		}
-		
+
 		super.service(aRequest, aResponse);
 	}
 
+	/**
+	 *
+	 * @param aSubProtocols
+	 * @return
+	 */
 	@Override
 	protected String selectSubProtocol(List<String> aSubProtocols) {
 		// super.selectSubProtocol(aSubProtocols);
 		return TomcatWrapper.selectSubProtocol(aSubProtocols);
 	}
 
+	/**
+	 *
+	 * @param aOrigin
+	 * @return
+	 */
 	@Override
 	protected boolean verifyOrigin(String aOrigin) {
 		return EngineUtils.isOriginValid(aOrigin, mEngine.getConfiguration().getDomains());
@@ -97,6 +125,12 @@ public class TomcatServlet extends WebSocketServlet {
 		return "jWebSocket Tomcat WebSocket Servlet";
 	}
 
+	/**
+	 *
+	 * @param aSubProtocol
+	 * @param aRequest
+	 * @return
+	 */
 	@Override
 	protected StreamInbound createWebSocketInbound(String aSubProtocol, HttpServletRequest aRequest) {
 		// do not use the aRequest paremeter here,
