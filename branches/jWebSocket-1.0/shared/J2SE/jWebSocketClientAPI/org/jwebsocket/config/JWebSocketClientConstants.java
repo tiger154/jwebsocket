efa6714132b63191bf65f4fd0d64e36c6cb394ea
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-03-05 17:37:28 +0000

    Adjusting licenses

--- branches/jWebSocket-1.0/shared/J2SE/jWebSocketClientAPI/org/jwebsocket/config/JWebSocketClientConstants.java
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketClientAPI/org/jwebsocket/config/JWebSocketClientConstants.java
@@ -1,17 +1,20 @@
+//  ---------------------------------------------------------------------------
+//  jWebSocket - JWebSocketClientConstants (Community Edition, CE)
 //	---------------------------------------------------------------------------
-//	jWebSocket - Server Configuration Constants
-//	Copyright (c) 2010 Alexander Schulze, Innotrade GmbH
-//	---------------------------------------------------------------------------
-//	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU Lesser General Public License as published by the
-//	Free Software Foundation; either version 3 of the License, or (at your
-//	option) any later version.
-//	This program is distributed in the hope that it will be useful, but WITHOUT
-//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-//	more details.
-//	You should have received a copy of the GNU Lesser General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 //	---------------------------------------------------------------------------
 package org.jwebsocket.config;
 
@@ -19,16 +22,16 @@ package org.jwebsocket.config;
  * Provides a global shared container for the jWebSocket configuration settings.
  *
  * @author aschulze
- * @version $Id: JWebSocketServerConstants.java 624 2010-07-06 12:28:44Z fivefeetfurther $
+ * @version $Id: JWebSocketServerConstants.java 624 2010-07-06 12:28:44Z
+ * fivefeetfurther $
  */
 public final class JWebSocketClientConstants {
-	
 
 	/**
 	 * Current version string of the jWebSocket package.
 	 */
 	// (RC0 build 30228)
-	public static final String VERSION_STR = "1.0 RC0 (build 30228)";
+	public static final String VERSION_STR = "1.0 RC0 (build 30305)";
 	/**
 	 * Name space base for tokens and plug-ins.
 	 */
