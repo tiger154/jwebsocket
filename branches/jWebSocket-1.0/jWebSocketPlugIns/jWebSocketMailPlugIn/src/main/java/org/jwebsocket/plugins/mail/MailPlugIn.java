9ee60335b655702b3c8f0184d4b59a77f62b7445
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-03-05 17:46:54 +0000

    Fixing license headers for Mail Plug-in to Apache 2.0, versioning for plug-ins, reformatting, fixing headers and class names, fixing imports, global clean-up.

--- branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketMailPlugIn/src/main/java/org/jwebsocket/plugins/mail/MailPlugIn.java
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketMailPlugIn/src/main/java/org/jwebsocket/plugins/mail/MailPlugIn.java
@@ -1,20 +1,20 @@
 //	---------------------------------------------------------------------------
-//	jWebSocket - jWebSocket SMTP Plug-In
-//  Copyright (c) 2010 Innotrade GmbH, jWebSocket.org
+//	jWebSocket - jWebSocket SMTP Plug-in (Community Edition, CE)
 //	---------------------------------------------------------------------------
-//  THIS CODE IS FOR RESEARCH, EVALUATION AND TEST PURPOSES ONLY!
-//  THIS CODE MAY BE SUBJECT TO CHANGES WITHOUT ANY NOTIFICATION!
-//	---------------------------------------------------------------------------
-//	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU Lesser General Public License as published by the
-//	Free Software Foundation; either version 3 of the License, or (at your
-//	option) any later version.
-//	This program is distributed in the hope that it will be useful, but WITHOUT
-//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-//	more details.
-//	You should have received a copy of the GNU Lesser General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 //	---------------------------------------------------------------------------
 package org.jwebsocket.plugins.mail;
 
@@ -41,6 +41,7 @@ import org.apache.log4j.Logger;
 import org.jwebsocket.api.PluginConfiguration;
 import org.jwebsocket.api.WebSocketConnector;
 import org.jwebsocket.cachestorage.ehcache.EhCacheCacheStorage;
+import org.jwebsocket.config.JWebSocketCommonConstants;
 import org.jwebsocket.config.JWebSocketConfig;
 import org.jwebsocket.config.JWebSocketServerConstants;
 import org.jwebsocket.kit.PlugInResponse;
@@ -62,6 +63,12 @@ public class MailPlugIn extends TokenPlugIn {
 	private static Logger mLog = Logging.getLogger();
 	// if namespace changed update client plug-in accordingly!
 	private static final String NS_MAIL = JWebSocketServerConstants.NS_BASE + ".plugins.mail";
+	private final static String VERSION = "1.0.0";
+	private final static String VENDOR = JWebSocketCommonConstants.VENDOR_CE;
+	private final static String LABEL = "jWebSocket MailPlugIn";
+	private final static String COPYRIGHT = JWebSocketCommonConstants.COPYRIGHT_CE;
+	private final static String LICENSE = JWebSocketCommonConstants.LICENSE_CE;
+	private final static String DESCRIPTION = "jWebSocket MailPlugIn - Community Edition";
 	private static MailStore mMailStore = null;
 	private static ApplicationContext mBeanFactory;
 	private static Settings mSettings;
@@ -101,6 +108,36 @@ public class MailPlugIn extends TokenPlugIn {
 		} catch (Exception lEx) {
 			mLog.error(Logging.getSimpleExceptionMessage(lEx, "instantiating mail plug-in"));
 		}
+	}
+
+	@Override
+	public String getVersion() {
+		return VERSION;
+	}
+
+	@Override
+	public String getLabel() {
+		return LABEL;
+	}
+
+	@Override
+	public String getDescription() {
+		return DESCRIPTION;
+	}
+
+	@Override
+	public String getVendor() {
+		return VENDOR;
+	}
+
+	@Override
+	public String getCopyright() {
+		return COPYRIGHT;
+	}
+
+	@Override
+	public String getLicense() {
+		return LICENSE;
 	}
 
 	@Override
