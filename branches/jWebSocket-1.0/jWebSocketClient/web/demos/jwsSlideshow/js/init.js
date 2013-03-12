27e252bf0d6af997e01742be9ad46974ccb909e0
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-03-12 17:22:31 +0000

    Slideshow demo channels based, new version of the slideshow, almost ready, just details to be fixed

--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsSlideshow/js/init.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsSlideshow/js/init.js
@@ -3,8 +3,39 @@
  */
 
 function init(  ) {
+	w                   = {};
+	mLog                = {};
+	mLog.isDebugEnabled = true;
+    
+	// Options
+	// @maxLogLines: maximum number of lines that will be logged
+	// @linesToDelete: quantity of lines that will be deleted from 
+	// the log window each time the log exceeds the maxLogLines
+	$( "#log_box" ).log({
+		maxLogLines: 200, 
+		linesToDelete: 20
+	});
+	
 	//starting the widget fundamentals
 	$("#presenter").focus();
+	
+	//configuring tooltip as we wish
+	$( "[title]" ).tooltip( {
+		position: "bottom center", 
+		onShow: function(  ) {
+			var lTip = this.getTip(  );
+			var lTop = ( "<div class='top'></div>" );
+			var lMiddle = $( "<div class='middle'></div>" ).text( lTip.text(  ) );
+			var lBottom = ( "<div class='bottom'></div>" );
+			lTip.html( "" ).append( lTop ).append( lMiddle ).append( lBottom );
+			this.getTrigger( ).mouseout( function( ) {
+				lTip.hide( ).hide( );
+			});
+			this.getTrigger( ).mousemove( function( ) {
+				lTip.show( );
+			});
+		}
+	} );
 }
 $( document ).ready( function(  ) {
 	init(  );
--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsSlideshow/js/presenter/wPresenter.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsSlideshow/js/presenter/wPresenter.js
@@ -17,199 +17,256 @@
 /*
  * @author vbarzana
  */
-$.widget( "jws.presenter", {
-    
+$.widget("jws.presenter", {
 	_init: function() {
+		this.NS_CHANNELS = jws.NS_BASE + '.plugins.channels';
+		this.NS_SYSTEM = jws.NS_BASE + '.plugins.system';
+		this.TITLE = "Presenter window";
 		// ------ DOM ELEMENTS --------
-		this.eBtnNext = this.element.find( "#btn_next" );
-		this.eBtnPrev = this.element.find( "#btn_prev" );
-		this.eBtnFirst = this.element.find( "#btn_first" );
-		this.eBtnLast = this.element.find( "#btn_last" );
-		this.eClients = this.element.find( "#viewers" );
-		this.eSlide = this.element.find( "#slide" );
-		this.eContainer = $( ".container" );
-		
+		this.eBtnNext = this.element.find("#btn_next");
+		this.eBtnPrev = this.element.find("#btn_prev");
+		this.eBtnFirst = this.element.find("#btn_first");
+		this.eBtnLast = this.element.find("#btn_last");
+		this.ePresenters = this.element.find("#presenters");
+		this.eViewers = this.element.find("#viewers");
+		this.eSlide = this.element.find("#slide");
+		this.eContainer = $(".container");
+
 		// ------ VARIABLES --------
 		this.mCurrSlide = 1;
 		this.mOldSlide = 0;
 		this.mMaxSlides = 22;
-		this.mClients = 0;
-		
-		w.presenter    = this;
+		this.mPresenters = 0;
+		this.mViewers = 0;
+		this.mChannelId = "jWebSocketSlideShowDemo";
+		this.mChannelAccessKey = "5l1d35h0w";
+		this.mChannelSecretKey = "5l1d35h0w53cr3t!";
+		this.TT_USER_REGISTER = "userregistered";
+		this.TT_USER_UNREGISTER = "userunregistered";
+		this.TT_SEND = "send";
+		this.TT_SLIDE = "slide";
+		this.mClientId = "";
+
+		w.presenter = this;
 		w.presenter.registerEvents();
 	},
-	
-	registerEvents: function(){
-		w.presenter.eBtnNext.click( w.presenter.nextSlide );
-		w.presenter.eBtnPrev.click( w.presenter.prevSlide );
-		w.presenter.eBtnLast.click( w.presenter.lastSlide );
-		w.presenter.eBtnFirst.click( w.presenter.firstSlide );
-	
-		$( document ).keydown( w.presenter.keydown );
-		
+	registerEvents: function() {
+		w.presenter.eBtnNext.click(w.presenter.nextSlide);
+		w.presenter.eBtnPrev.click(w.presenter.prevSlide);
+		w.presenter.eBtnLast.click(w.presenter.lastSlide);
+		w.presenter.eBtnFirst.click(w.presenter.firstSlide);
+		$(document).keydown(w.presenter.keydown);
+		// When closing the window notify the other clients about who is 
+		// leaving the conference room
+		$(window).bind('beforeunload', function() {
+			var lData = {
+				value: "presenter_" + mWSC.getUsername() + "@" + w.presenter.mClientId
+			};
+			w.presenter.publish(w.presenter.TT_USER_UNREGISTER, lData);
+		});
+
 		// Registers all callbacks for jWebSocket basic connection
 		// For more information, check the file ../../res/js/widget/wAuth.js
 		var lCallbacks = {
-			OnMessage: function( aEvent, aToken ) {
-				w.presenter.onMessage( aEvent, aToken );
+			OnMessage: function(aEvent, aToken) {
+				if (mLog.isDebugEnabled) {
+					log(" <b>" + w.presenter.TITLE + " new message received: </b>" +
+							JSON.stringify(aToken));
+				}
+				w.presenter.onMessage(aEvent, aToken);
+			},
+			OnWelcome: function(aToken) {
+				// Registering the callbacks for the channels
+				mWSC.setChannelCallbacks({
+					// When any subscription arrives from the server
+					OnChannelSubscription: w.presenter.onChannelSubscription
+				});
+				w.presenter.mClientId = aToken.sourceId;
 			}
 		};
-		w.presenter.eContainer.auth( lCallbacks );
-		
-		// Try to logon in the server using the default guest credentials
-		// for more information check the public widget wAuth.js
-		w.auth.logon( true );
+		w.presenter.eContainer.auth(lCallbacks);
+		AUTO_USER_AND_PASSWORD = true;
+		w.auth.logon();
 	},
-	
+	onMessage: function(aEvent, aToken) {
+		if (aToken.reqType === "login" && aToken.code === 0) {
+			// Subscribe to a certain channel
+			mWSC.channelSubscribe(w.presenter.mChannelId,
+					w.presenter.mChannelAccessKey, {
+				OnSuccess: function( ) {
+					w.presenter.authenticateChannel();
+				}
+			});
+		} else if (aToken.ns === w.presenter.NS_CHANNELS) {
+			// When the channel authorizes the user to publish on it
+			if (aToken.reqType === "authorize") {
+				if (aToken.code === 0) {
+					var lData = {
+						value: "presenter_" + mWSC.getUsername() + "@" +
+								w.presenter.mClientId
+					};
+					// We send a notification trough the channel to inform about
+					// a new presenter online
+					w.presenter.publish(w.presenter.TT_USER_REGISTER, lData);
+				}
+				// When information is published in the channel the data is sent
+				// in a map inside the token and the type of the token comes in 
+				// the key "data"
+			} else if (aToken.type === "data") {
+				switch (aToken.data) {
+					case w.presenter.TT_USER_REGISTER:
+						w.presenter.userRegistered(aToken.map, aToken.publisher);
+						break;
+					case w.presenter.TT_SLIDE:
+						// Pass the current slide
+						w.presenter.goTo(aToken.map.slide);
+						break;
+					case w.presenter.TT_USER_UNREGISTER:
+						w.presenter.userUnregistered(aToken.map);
+						break;
+				}
+			}
+		} else if (aToken.ns === w.presenter.NS_SYSTEM) {
+			if (aToken.type === w.presenter.TT_SEND) {
+				w.presenter.updateUsers(aToken.data);
+				w.presenter.goTo(aToken.data.currslide);
+			}
+		}
+	},
 	nextSlide: function( ) {
-		if( w.presenter.mCurrSlide < w.presenter.mMaxSlides ) {
+		if (w.presenter.mCurrSlide < w.presenter.mMaxSlides) {
 			w.presenter.mCurrSlide++;
 			w.presenter.updateSlide( );
 		}
 	},
-	
 	prevSlide: function( ) {
-		if( w.presenter.mCurrSlide > 1 ) {
+		if (w.presenter.mCurrSlide > 1) {
 			w.presenter.mCurrSlide--;
 			w.presenter.updateSlide( );
 		}
 	},
-	
 	lastSlide: function( ) {
 		w.presenter.mCurrSlide = w.presenter.mMaxSlides;
 		w.presenter.updateSlide( );
 	},
-	
 	firstSlide: function( ) {
 		w.presenter.mCurrSlide = 1;
 		w.presenter.updateSlide( );
 	},
-	
-	goTo: function( aSlide ) {
-		if( w.presenter.mOldSlide != aSlide ) {
-			// Don't include slideshow effects or you won't get real time
-			//			w.presenter.eSlide.fadeTo( 80, 0.2, function(){
-			w.presenter.eSlide.attr( "src" ,"slides/Slide" + 
-				jws.tools.zerofill( aSlide, 4 ) + ".gif");
-			//			});
-			//			w.presenter.eSlide.fadeTo( 40, 1 );
-		
+	goTo: function(aSlide) {
+		if (w.presenter.mOldSlide != aSlide) {
+			w.presenter.eSlide.attr("src", "slides/Slide" +
+					jws.tools.zerofill(aSlide, 4) + ".gif");
 			w.presenter.eBtnLast.isDisabled && w.presenter.eBtnLast.enable( );
 			w.presenter.eBtnNext.isDisabled && w.presenter.eBtnNext.enable( );
 			w.presenter.eBtnFirst.isDisabled && w.presenter.eBtnFirst.enable( );
 			w.presenter.eBtnPrev.isDisabled && w.presenter.eBtnPrev.enable( );
-		
-			if( aSlide == 1 ){
+
+			if (aSlide == 1) {
 				w.presenter.eBtnPrev.disable( );
 				w.presenter.eBtnFirst.disable( );
-			}else if( aSlide == w.presenter.mMaxSlides ) {
+			} else if (aSlide == w.presenter.mMaxSlides) {
 				w.presenter.eBtnNext.disable( );
 				w.presenter.eBtnLast.disable( );
 			}
 			w.presenter.mOldSlide = aSlide;
 		}
 	},
-	
 	updateSlide: function() {
-		if( w.presenter.mOldSlide != w.presenter.mCurrSlide ) {
-			mWSC.broadcastToken({
-				action: "slide",
+		if (w.presenter.mOldSlide !== w.presenter.mCurrSlide) {
+			w.presenter.publish(w.presenter.TT_SLIDE, {
 				slide: w.presenter.mCurrSlide,
-				senderIncluded: true,
-				responseRequested: false,
-				clientCount: w.presenter.mClients
+				presenter: w.presenter.mClientId
 			});
 		}
 	},
-	
-	keydown: function( aEvent ) {
-		if ( mWSC.isConnected() ) {
+	updateUsers: function(aData) {
+		w.presenter.mPresenters = aData.presenters;
+		w.presenter.mViewers = aData.viewers;
+		w.presenter.ePresenters.text(w.presenter.mPresenters);
+		w.presenter.eViewers.text(w.presenter.mViewers);
+	},
+	userRegistered: function(aData, aPublisher) {
+		var lUser = aData.value.trim().split("_");
+		var lData = {
+			currslide: w.presenter.mCurrSlide,
+			presenters: lUser[0] == "presenter" ? ++w.presenter.mPresenters : w.presenter.mPresenters,
+			viewers: lUser[0] == "viewer" ? ++w.presenter.mViewers : w.presenter.mViewers,
+		};
+		if (aPublisher !== w.presenter.mClientId) {
+			mWSC.sendText(aPublisher, lData);
+		}
+		w.presenter.updateUsers(lData);
+	},
+	userUnregistered: function(aData) {
+		var lUser = aData.value.trim().split("_");
+		if (lUser[0] == "presenter") {
+			w.presenter.mPresenters > 0 && w.presenter.mPresenters--;
+		} else {
+			w.presenter.mViewers > 0 && w.presenter.mViewers--;
+		}
+		w.presenter.updateUsers({
+			presenters: w.presenter.mPresenters,
+			viewers: w.presenter.mViewers
+		})
+	},
+	keydown: function(aEvent) {
+		if (mWSC.isConnected()) {
 			var lKeyCode = aEvent.keyCode || aEvent.keyChar;
-			switch( lKeyCode ) {
-				case 37: {
-					// Left Arrow (Ctrl key pressed takes you to the First slide)
-					aEvent.ctrlKey && w.presenter.firstSlide()
-					|| w.presenter.prevSlide();	
-					aEvent.preventDefault();
-					break;
-				}
-				case 39: {
-					// Right Arrow (Ctrl key pressed takes you to the First slide)
-					aEvent.ctrlKey && w.presenter.lastSlide()
-					|| w.presenter.nextSlide();	
-					aEvent.preventDefault();
-					break;	
-				}
+			switch (lKeyCode) {
+				case 37:
+					{
+						// Left Arrow (Ctrl key pressed takes you to the First slide)
+						aEvent.ctrlKey && w.presenter.firstSlide()
+								|| w.presenter.prevSlide();
+						aEvent.preventDefault();
+						break;
+					}
+				case 39:
+					{
+						// Right Arrow (Ctrl key pressed takes you to the First slide)
+						aEvent.ctrlKey && w.presenter.lastSlide()
+								|| w.presenter.nextSlide();
+						aEvent.preventDefault();
+						break;
+					}
 			}
 		}
 	},
-	
-	onMessage: function( aEvent, aToken ) {
-		console.log(aToken);
-		// check if slide has to be updated
-		if( aToken.action == "slide" ) {
-			w.presenter.mClients = aToken.clientCount;
-			w.presenter.eClients.text( w.presenter.mClients );
-			if( w.presenter.mOldSlide != aToken.slide ) {
-				// Pass the current slide
-				w.presenter.goTo( aToken.slide );
-				// Log the incoming message
-				if( mLog.isDebugEnabled ) {
-					log( "<font style='color:#888'>jWebSocket '" + aToken.type 
-						+ "' token received, full message: '" + aEvent.data + "</font>" );
-				}
-			}
-		// check if new client connected and send the current for initialization
-		} else if( "event" == aToken.type
-			&& "connect" == aToken.name ) {
-			w.presenter.mClients = aToken.clientCount;
-			w.presenter.eClients.text( w.presenter.mClients );
-			mWSC.sendToken({
-				ns: jws.NS_SYSTEM,
-				type: "send",
-				targetId: aToken.sourceId,
-				action: "slide",
-				slide: w.presenter.mCurrSlide,
-				clientCount: w.presenter.mClients
-			});
-		} else if( "event" == aToken.type
-			&& "disconnect" == aToken.name ) {
-			w.presenter.mClients = aToken.clientCount - 1;
-			w.presenter.eClients.text( w.presenter.mClients );
-			mWSC.sendToken({
-				ns: jws.NS_SYSTEM,
-				type: "send",
-				targetId: aToken.sourceId,
-				action: "slide",
-				slide: w.presenter.mCurrSlide,
-				clientCount: w.presenter.mClients
-			});
-		}
+	// try to authenticate against the channel to publish data
+	authenticateChannel: function() {
+		// use access key and secret key for this channel to authenticate
+		// required to publish data only
+		var lRes = mWSC.channelAuth(w.presenter.mChannelId,
+				w.presenter.mChannelAccessKey, w.presenter.mChannelSecretKey);
+	},
+	publish: function(aType, aData) {
+		mWSC.channelPublish(w.presenter.mChannelId, aType, aData);
 	}
 });
 
-(function($){
+(function($) {
 	$.fn.buttons = {};
 	$.fn.disable = function( ) {
 		var lButton = this;
 		var lId = lButton.attr("id");
 		lButton.isDisabled = true;
 		$.fn.buttons[lId] = lButton.clone();
-		var lEvents = ["onmouseover","onmousedown","onmouseup","onmouseout","onclick"];
-		$( lEvents ).each(function( aIndex, aElem ){
-			lButton.attr( aElem, null );
+		var lEvents = ["onmouseover", "onmousedown", "onmouseup", "onmouseout", "onclick"];
+		$(lEvents).each(function(aIndex, aElem) {
+			lButton.attr(aElem, null);
 		});
-		lButton.attr( "class", "button onmousedown" );
+		lButton.attr("class", "button onmousedown");
 	};
 	$.fn.enable = function( ) {
 		var lButton = this;
-		var lId = lButton.attr( "id" );
-		var lEvents = ["onmouseover","onmousedown","onmouseup","onmouseout","onclick"];
-		$( lEvents ).each(function( aIndex, aAttribute ){
-			lButton.attr( aAttribute, $.fn.buttons[ lId ].attr( aAttribute ) );
+		var lId = lButton.attr("id");
+		var lEvents = ["onmouseover", "onmousedown", "onmouseup", "onmouseout", "onclick"];
+		$(lEvents).each(function(aIndex, aAttribute) {
+			lButton.attr(aAttribute, $.fn.buttons[ lId ].attr(aAttribute));
 		});
-		lButton.attr( "class", "button onmouseout" );
-		
+		lButton.attr("class", "button onmouseout");
+
 		lButton.isDisabled = false;
 	};
 })(jQuery);
