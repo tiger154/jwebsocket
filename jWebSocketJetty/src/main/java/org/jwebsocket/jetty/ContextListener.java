b410ffd6ad4a4ef2343e4a44c5246876ef57328b
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-03-15 16:54:56 +0000

    Fixing license headers for jWebSocket Jetty Module to Apache 2.0, versioning for plug-in, reformatting, fixing headers and class names, fixing imports, global clean-up for pending RC0 deployment.

--- jWebSocketJetty/src/main/java/org/jwebsocket/jetty/ContextListener.java
+++ jWebSocketJetty/src/main/java/org/jwebsocket/jetty/ContextListener.java
@@ -1,17 +1,20 @@
 //	---------------------------------------------------------------------------
-//	jWebSocket - Context Listener for Jetty Web Applications
-//	Copyright (c) 2010 jWebSocket.org, Alexander Schulze, Innotrade GmbH
+//	jWebSocket - Context Listener for Jetty Web Applications (Community Edition, CE)
 //	---------------------------------------------------------------------------
-//	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU Lesser General Public License as published by the
-//	Free Software Foundation; either version 3 of the License, or (at your
-//	option) any later version.
-//	This program is distributed in the hope that it will be useful, but WITHOUT
-//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-//	more details.
-//	You should have received a copy of the GNU Lesser General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 //	---------------------------------------------------------------------------
 package org.jwebsocket.jetty;
 
@@ -24,21 +27,23 @@ import org.jwebsocket.server.TokenServer;
 
 /**
  * Web application life cycle listener.
+ *
  * @author alexanderschulze
  */
 public class ContextListener implements ServletContextListener {
 
 	/**
 	 * initializes the web application on startup.
+	 *
 	 * @param aSCE
 	 */
 	@Override
 	public void contextInitialized(ServletContextEvent aSCE) {
 		// the following line must not be removed due to GNU LGPL 3.0 license!
 		JWebSocketFactory.printCopyrightToConsole();
-		
+
 		JWebSocketConfig.initForWebApp(aSCE.getServletContext());
-		
+
 		// start the jWebSocket server sub system with default config and bootstrap
 		JWebSocketFactory.start();
 
@@ -53,6 +58,7 @@ public class ContextListener implements ServletContextListener {
 
 	/**
 	 * cleans up the web application on termination.
+	 *
 	 * @param aSCE
 	 */
 	@Override
