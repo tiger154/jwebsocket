2b3d2d537ac7ceffbeae4a6bd924d4877f0a2bda
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-03-15 16:54:56 +0000

    Fixing license headers for jWebSocket Jetty Module to Apache 2.0, versioning for plug-in, reformatting, fixing headers and class names, fixing imports, global clean-up for pending RC0 deployment.

--- branches/jWebSocket-1.0/jWebSocketJetty/src/main/java/org/jwebsocket/jetty/Test.java
+++ branches/jWebSocket-1.0/jWebSocketJetty/src/main/java/org/jwebsocket/jetty/Test.java
@@ -1,17 +1,20 @@
 //	---------------------------------------------------------------------------
-//	jWebSocket - Test Servlet for Jetty Demo App
-//	Copyright (c) 2011 jWebSocket.org, Alexander Schulze, Innotrade GmbH
+//	jWebSocket - Test Servlet for Jetty Demo App (Community Edition, CE)
 //	---------------------------------------------------------------------------
-//	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU Lesser General Public License as published by the
-//	Free Software Foundation; either version 3 of the License, or (at your
-//	option) any later version.
-//	This program is distributed in the hope that it will be useful, but WITHOUT
-//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-//	more details.
-//	You should have received a copy of the GNU Lesser General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 //	---------------------------------------------------------------------------
 package org.jwebsocket.jetty;
 
@@ -27,66 +30,73 @@ import javax.servlet.http.HttpServletResponse;
  * @author aschulze
  */
 public class Test extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param aRequest servlet request
-     * @param aResponse servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest aRequest, HttpServletResponse aResponse)
-    throws ServletException, IOException {
-        aResponse.setContentType("text/html;charset=UTF-8");
-        PrintWriter lOut = aResponse.getWriter();
-        try {
-            lOut.println("<html>");
-            lOut.println("<head>");
-            lOut.println("<title>Jetty Servlet Test</title>");
-            lOut.println("</head>");
-            lOut.println("<body>");
-            lOut.println("<h1>Jetty Servlet Test at " + aRequest.getContextPath () + "</h1>");
-            lOut.println("</body>");
-            lOut.println("</html>");
-        } finally { 
-            lOut.close();
-        }
-    } 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param aRequest servlet request
-     * @param aResponse servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest aRequest, HttpServletResponse aResponse)
-    throws ServletException, IOException {
-        processRequest(aRequest, aResponse);
-    } 
+	/**
+	 * Processes requests for both HTTP
+	 * <code>GET</code> and
+	 * <code>POST</code> methods.
+	 *
+	 * @param aRequest servlet request
+	 * @param aResponse servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	protected void processRequest(HttpServletRequest aRequest, HttpServletResponse aResponse)
+			throws ServletException, IOException {
+		aResponse.setContentType("text/html;charset=UTF-8");
+		PrintWriter lOut = aResponse.getWriter();
+		try {
+			lOut.println("<html>");
+			lOut.println("<head>");
+			lOut.println("<title>Jetty Servlet Test</title>");
+			lOut.println("</head>");
+			lOut.println("<body>");
+			lOut.println("<h1>Jetty Servlet Test at " + aRequest.getContextPath() + "</h1>");
+			lOut.println("</body>");
+			lOut.println("</html>");
+		} finally {
+			lOut.close();
+		}
+	}
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param aRequest servlet request
-     * @param aResponse servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest aRequest, HttpServletResponse aResponse)
-    throws ServletException, IOException {
-        processRequest(aRequest, aResponse);
-    }
+	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+	/**
+	 * Handles the HTTP
+	 * <code>GET</code> method.
+	 *
+	 * @param aRequest servlet request
+	 * @param aResponse servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest aRequest, HttpServletResponse aResponse)
+			throws ServletException, IOException {
+		processRequest(aRequest, aResponse);
+	}
 
-    /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+	/**
+	 * Handles the HTTP
+	 * <code>POST</code> method.
+	 *
+	 * @param aRequest servlet request
+	 * @param aResponse servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest aRequest, HttpServletResponse aResponse)
+			throws ServletException, IOException {
+		processRequest(aRequest, aResponse);
+	}
 
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
 }
