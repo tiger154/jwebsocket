e8c57e2406848bf1c56ba8a8c86078bb3d8ac0ac
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-04-01 14:02:44 +0000

    Fixing the links of downloads in jwebsocket

--- jWebSocketClient/web/demos/jwsSmartCard/js/wAuth.js
+++ jWebSocketClient/web/demos/jwsSmartCard/js/wAuth.js
@@ -52,7 +52,7 @@ $.widget("jws.auth",{
 		var lUsername = w.auth.eUsername.val();
 		var lPassword = w.auth.ePassword.val();
 		if(lUsername == "" || lPassword == ""){
-			dialog( "User or password are not correct, please check", "Incorrect Data" );
+			jwsDialog( "User or password are not correct, please check", "Incorrect Data" );
 			return;
 		}
 	
