b655b405e8971d4e2d605e2bfe84b6eb46a9ad3e
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-04-15 15:03:42 +0000

    uploading webrtc application and last app prototype fixes

--- rte/jWebSocket-1.0/conf/ScriptingPlugIn/scripts/js/demoApp/App.js
+++ rte/jWebSocket-1.0/conf/ScriptingPlugIn/scripts/js/demoApp/App.js
@@ -1,4 +1,5 @@
-app.importScript("lib/myAppLib.js");
+// importing App script
+App.importScript("${APP_HOME}/lib/myAppLib.js");
 
 importClass(org.apache.commons.io.FileUtils);
 importClass(java.io.File);
@@ -7,13 +8,33 @@ var Object = {
 	sayHello: sayHello
 };
 
-var lFn = function() {
-	app.getLogger().debug("Processing lFn...");
+App.on("filterIn", function(aToken) {
+	App.getLogger().debug("Calling filter in: " + aToken.toString());
+});
 
-	return {
-		name: "Rolando SM",
-		age: 27
-	};
+App.on("token", function(aToken, aConnector) {
+	var lResponse = App.createResponse(aToken);
+	lResponse.put("required", true);
+	lResponse.put("name", aToken.get("name"));
+
+	App.sendToken(aConnector, lResponse);
+});
+
+App.on("filterOut", function(aToken) {
+	App.getLogger().debug("Calling filter out: " + aToken.toString());
+});
+
+App.on("connectorStarted", function(aConnector) {
+	App.getLogger().debug("New client started: " + aConnector.getId());
+});
+
+App.on("connectorStopped", function(aConnector) {
+	App.getLogger().debug("Client stopped: " + aConnector.getId());
+});
+
+var lFn = function(aConnector) {
+	App.getLogger().debug("Processing lFn...");
+	App.requireAuthority(aConnector, "admin");
 };
 
 var read = function(aFile) {
@@ -24,7 +45,8 @@ var getVersion = function() {
 	return java.lang.System.getProperty("java.version");
 };
 
-app.publish("Main", {
+// publish App object to be accessed from the client
+App.publish("Main", {
 	read: read,
 	version: getVersion,
 	sayHello: Object.sayHello,
