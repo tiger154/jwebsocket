2342d333098f071660a587a725b676447cbee0a1
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-04-22 16:26:16 +0000

    supporting server-side scripting plug-in in Java client

---
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketJavaSEClient/org/jwebsocket/client/plugins/scripting/ScriptingPlugIn.java
@@ -0,0 +1,91 @@
+//	---------------------------------------------------------------------------
+//	jWebSocket - ScriptingPlugIn
+//	Copyright (c) 2012 jWebSocket.org, Innotrade GmbH
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.jwebsocket.client.plugins.scripting;
+
+import java.util.List;
+import org.jwebsocket.api.WebSocketTokenClient;
+import org.jwebsocket.client.plugins.BaseClientTokenPlugIn;
+import org.jwebsocket.config.JWebSocketClientConstants;
+import org.jwebsocket.kit.WebSocketException;
+import org.jwebsocket.token.Token;
+import org.jwebsocket.token.TokenFactory;
+import org.jwebsocket.token.WebSocketResponseTokenListener;
+
+/**
+ *
+ * @author kyberneees
+ */
+public class ScriptingPlugIn extends BaseClientTokenPlugIn {
+
+	public ScriptingPlugIn(WebSocketTokenClient aClient) {
+		super(aClient, JWebSocketClientConstants.NS_SCRIPTING);
+	}
+
+	public ScriptingPlugIn(WebSocketTokenClient aClient, String aNS) {
+		super(aClient, aNS);
+	}
+
+	/**
+	 * Calls an application published object method.
+	 *
+	 * @param aApp The application name
+	 * @param aObjectId The application published object identifier
+	 * @param aMethod The method name
+	 * @param aArgs The method arguments
+	 * @param aListener The response listener.
+	 * @throws WebSocketException
+	 */
+	public void callMethod(String aApp, String aObjectId, String aMethod, List<Object> aArgs,
+			WebSocketResponseTokenListener aListener) throws WebSocketException {
+		Token lRequest = TokenFactory.createToken(getNS(), "callMethod");
+		lRequest.setString("app", aApp);
+		lRequest.setString("method", aMethod);
+		lRequest.setString("objectId", aObjectId);
+		lRequest.setList("args", aArgs);
+
+		getTokenClient().sendToken(lRequest, aListener);
+	}
+
+	/**
+	 * Reloads an script application in runtime.
+	 *
+	 * @param aApp The application name
+	 * @param aListener The response listener
+	 * @throws WebSocketException
+	 */
+	public void reloadApp(String aApp, WebSocketResponseTokenListener aListener) throws WebSocketException {
+		Token lRequest = TokenFactory.createToken(getNS(), "reloadApp");
+		lRequest.setString("app", aApp);
+
+		getTokenClient().sendToken(lRequest, aListener);
+	}
+
+	/**
+	 * Sends a token to script application.
+	 *
+	 * @param aApp The application name
+	 * @param aToken The token to be sent
+	 * @param aListener The response listener
+	 * @throws WebSocketException
+	 */
+	public void sendToken(String aApp, Token aToken, WebSocketResponseTokenListener aListener) throws WebSocketException {
+		aToken.setNS(getNS());
+		aToken.setType("token");
+		aToken.setString("app", aApp);
+
+		getTokenClient().sendToken(aToken, aListener);
+	}
+}
