d2c1d0d236a9838488f4ed7fd34f55e17633aff5
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-05-02 20:44:35 +0000

    adding modify channel feature to the channel plugin.

--- jWebSocketClient/web/res/js/jwsEventsPlugIn.js
+++ jWebSocketClient/web/res/js/jwsEventsPlugIn.js
@@ -5,14 +5,14 @@
 //	Alexander Schulze, Germany (NRW)
 //  Author: Rolando Santamaria Maso
 //
-//	Licensed under the Apache License, Version 2.0 (the "License");
+//	Licensed under the Apache License, Version 2.0 (the 'License');
 //	you may not use this file except in compliance with the License.
 //	You may obtain a copy of the License at
 //
 //	http://www.apache.org/licenses/LICENSE-2.0
 //
 //	Unless required by applicable law or agreed to in writing, software
-//	distributed under the License is distributed on an "AS IS" BASIS,
+//	distributed under the License is distributed on an 'AS IS' BASIS,
 //	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //	See the License for the specific language governing permissions and
 //	limitations under the License.
@@ -23,9 +23,9 @@
 //:ancestor:*:-
 //:d:en:Implementation of the [tt]jws.EventsCallbacksHandler[/tt] class. _
 //:d:en:This class handle request callbacks on the events plug-in
-jws.oop.declareClass( "jws", "EventsCallbacksHandler", null, {
+jws.oop.declareClass( 'jws', 'EventsCallbacksHandler', null, {
 	OnTimeout: function(aRawRequest, aArgs){
-		if (undefined != aArgs.meta["OnTimeout"] && "function" == typeof(aArgs.meta["OnTimeout"])){
+		if (undefined != aArgs.meta['OnTimeout'] && 'function' == typeof(aArgs.meta['OnTimeout'])){
 			aArgs.meta.OnTimeout(aRawRequest);
 		}
 	}
@@ -46,7 +46,7 @@ jws.oop.declareClass( "jws", "EventsCallbacksHandler", null, {
 				{
 					switch (err)
 					{
-						case "stop_filter_chain":
+						case 'stop_filter_chain':
 							return;
 							break;
 						default:
@@ -81,12 +81,12 @@ jws.oop.declareClass( "jws", "EventsCallbacksHandler", null, {
 //:ancestor:*:-
 //:d:en:Implementation of the [tt]jws.EventsNotifier[/tt] class. _
 //:d:en:This class handle raw events notifications to/from the server side.
-jws.oop.declareClass( "jws", "EventsNotifier", null, {
-	ID: ""
+jws.oop.declareClass( 'jws', 'EventsNotifier', null, {
+	ID: ''
 	,
 	jwsClient: {}
 	,
-	NS: ""
+	NS: ''
 	,
 	filterChain: []
 	,
@@ -102,7 +102,7 @@ jws.oop.declareClass( "jws", "EventsNotifier", null, {
 		
 		//Initializing each filters
 		for (var lIndex = 0, lEnd = this.filterChain.length; lIndex < lEnd; lIndex++){
-			if (this.filterChain[lIndex]["initialize"]){
+			if (this.filterChain[lIndex]['initialize']){
 				this.filterChain[lIndex].initialize(this);
 			}
 		}
@@ -147,7 +147,7 @@ jws.oop.declareClass( "jws", "EventsNotifier", null, {
 					}
 					catch(err) {
 						switch (err) {
-							case "stop_filter_chain":
+							case 'stop_filter_chain':
 								return;
 								break;
 							default:
@@ -161,25 +161,25 @@ jws.oop.declareClass( "jws", "EventsNotifier", null, {
 			this.jwsClient.sendToken(lToken, lRequest);
 		}
 		else
-			jws.console.error( "client:not_connected" );
+			jws.console.error( 'client:not_connected' );
 	}
 	,
 	//:m:*:processToken
 	//:d:en:Processes an incoming token. Used to support S2C events notifications. _
-	//:d:en:Use the "event_name" and "plugin_id" information to execute _
+	//:d:en:Use the 'event_name' and 'plugin_id' information to execute _
 	//:d:en:a targered method in a plug-in.
 	//:a:en::aToken:Object:Token to be processed
 	//:r:*:::void:none
 	processToken: function (aToken) {
-		if ((this.NS == aToken.ns && "auth.logon" == aToken.reqType && 0 == aToken.code)){
+		if ((this.NS == aToken.ns && 'auth.logon' == aToken.reqType && 0 == aToken.code)){
 			this.user.principal = aToken.username;
 			this.user.uuid = aToken.uuid;
 			this.user.roles = aToken.roles;
 		}
-		else if ((this.NS == aToken.ns && "auth.logoff" == aToken.reqType && 0 == aToken.code)){
+		else if ((this.NS == aToken.ns && 'auth.logoff' == aToken.reqType && 0 == aToken.code)){
 			this.user.clear();
 		} 
-		else if (this.NS == aToken.ns && "s2c.en" == aToken.type){
+		else if (this.NS == aToken.ns && 's2c.en' == aToken.type){
 			var lMethod = aToken._e;
 			var lPlugIn = aToken._p;
 
@@ -191,7 +191,7 @@ jws.oop.declareClass( "jws", "EventsNotifier", null, {
 				//Sending response back to the server if the event notification
 				//has a callback
 				if (aToken.hc){
-					this.notify("s2c.r", {
+					this.notify('s2c.r', {
 						args: {
 							_rid: aToken.uid,
 							_r: lRes,
@@ -201,13 +201,13 @@ jws.oop.declareClass( "jws", "EventsNotifier", null, {
 				}
 			}
 			else {
-				//Sending the "not supported" event notification
-				this.notify("s2c.ens", {
+				//Sending the 'not supported' event notification
+				this.notify('s2c.ens', {
 					args: {
 						_rid: aToken.uid
 					}
 				});
-				jws.console.error( "s2c_event_support_not_found for: " + lMethod );
+				jws.console.error( 's2c_event_support_not_found for: ' + lMethod );
 			}
 		}
 	}
@@ -219,11 +219,11 @@ jws.oop.declareClass( "jws", "EventsNotifier", null, {
 //:d:en:Implementation of the [tt]jws.EventsPlugInGenerator[/tt] class. _
 //:d:en:This class handle the generation of server plug-ins as _
 //:d:en:Javascript objects.
-jws.oop.declareClass( "jws", "EventsPlugInGenerator", null, {
+jws.oop.declareClass( 'jws', 'EventsPlugInGenerator', null, {
 
 	//:m:*:generate
 	//:d:en:Processes an incoming token. Used to support S2C events notifications. _
-	//:a:en::aPlugInId:String:Remote plug-in "id" to generate in the client.
+	//:a:en::aPlugInId:String:Remote plug-in 'id' to generate in the client.
 	//:a:en::aNotifier:jws.EventsNotifier:The event notifier used to connect with the server.
 	//:a:en::aCallbacks:Function:This callback is called when the plug-in has been generated successfully.
 	//:a:en::aCallbacks:Object:Contains the OnSuccess and OnFailure callbacks
@@ -232,7 +232,7 @@ jws.oop.declareClass( "jws", "EventsPlugInGenerator", null, {
 		var lPlugIn = new jws.EventsPlugIn();
 		lPlugIn.notifier = aNotifier;
 
-		aNotifier.notify("plugin.getapi", {
+		aNotifier.notify('plugin.getapi', {
 			args: {
 				plugin_id: aPlugInId
 			}
@@ -247,25 +247,25 @@ jws.oop.declareClass( "jws", "EventsPlugInGenerator", null, {
 
 				//Generating the plugin methods
 				for (method in aResponseEvent.api){
-					eval("this.plugIn." + method + "=function(aOptions){if (undefined == aOptions){aOptions = {};};var eventName=this.plugInAPI."+method+".type; aOptions.eventDefinition=this.plugInAPI."+ method + "; aOptions.timeout = this.plugInAPI."+method+".timeout; this.notifier.notify(eventName, aOptions);}")
+					eval('this.plugIn.' + method + '=function(aOptions){if (undefined == aOptions){aOptions = {};};var eventName=this.plugInAPI.'+method+'.type; aOptions.eventDefinition=this.plugInAPI.'+ method + '; aOptions.timeout = this.plugInAPI.'+method+'.timeout; this.notifier.notify(eventName, aOptions);}')
 				}
 
 				//Registering the plugin in the notifier
 				this.plugIn.notifier.plugIns[this.plugIn.id] = this.plugIn;
 
 				//Plugin is generated successfully
-				if ("function" == typeof(this.callbacks)){
+				if ('function' == typeof(this.callbacks)){
 					this.callbacks(this.plugIn);
-				} else if ("function" == typeof( this.callbacks["OnSuccess"] )){
+				} else if ('function' == typeof( this.callbacks['OnSuccess'] )){
 					this.callbacks.OnSuccess(this.plugIn);
 				}
 			}
 			,
 			OnFailure: function(aResponseEvent){
-				if ("function" == typeof( this.callbacks["OnFailure"] )){
+				if ('function' == typeof( this.callbacks['OnFailure'] )){
 					this.callbacks.OnFailure(this.plugIn);
 				} else {
-					jws.console.error("Failure generating plug-in: " + aResponseEvent.msg );
+					jws.console.error('Failure generating plug-in: ' + aResponseEvent.msg );
 				}
 			}	
 		});
@@ -280,8 +280,8 @@ jws.oop.declareClass( "jws", "EventsPlugInGenerator", null, {
 //:d:en:Implementation of the [tt]jws.EventsPlugIn[/tt] class. _
 //:d:en:This class represents an abstract client plug-in. The methods are _
 //:d:en:generated in runtime.
-jws.oop.declareClass( "jws", "EventsPlugIn", null, {
-	id: ""
+jws.oop.declareClass( 'jws', 'EventsPlugIn', null, {
+	id: ''
 	,
 	notifier: {}
 	,
@@ -295,10 +295,10 @@ jws.oop.declareClass( "jws", "EventsPlugIn", null, {
 //:class:*:jws.AppUser
 //:ancestor:*:-
 //:d:en:Application user instance.
-jws.oop.declareClass( "jws", "AppUser", null, {
-	principal: ""
+jws.oop.declareClass( 'jws', 'AppUser', null, {
+	principal: ''
 	,
-	uuid: ""
+	uuid: ''
 	,
 	roles: []
 	,
@@ -306,9 +306,9 @@ jws.oop.declareClass( "jws", "AppUser", null, {
 	//:d:en:Clear the user instance
 	//:r:*:::void:none
 	clear: function (){
-		this.principal = "";
+		this.principal = '';
 		this.roles = [];
-		this.uuid = "";
+		this.uuid = '';
 	}
 	,
 	//:m:*:isAuthenticated
@@ -340,8 +340,8 @@ jws.oop.declareClass( "jws", "AppUser", null, {
 //:ancestor:*:-
 //:d:en:Implementation of the [tt]jws.EventsBaseFilter[/tt] class. _
 //:d:en:This class represents an abstract client filter.
-jws.oop.declareClass( "jws", "EventsBaseFilter", null, {
-	id: ""
+jws.oop.declareClass( 'jws', 'EventsBaseFilter', null, {
+	id: ''
 	,
 	
 	//:m:*:initialize
@@ -372,8 +372,8 @@ jws.oop.declareClass( "jws", "EventsBaseFilter", null, {
 //:d:en:Implementation of the [tt]jws.SecurityFilter[/tt] class. _
 //:d:en:This class handle the security for every C2S event notification _
 //:d:en:in the client, using the server side security configuration.
-jws.oop.declareClass( "jws", "SecurityFilter", jws.EventsBaseFilter, {
-	id: "security"
+jws.oop.declareClass( 'jws', 'SecurityFilter', jws.EventsBaseFilter, {
+	id: 'security'
 	,
 	user: null
 	,
@@ -411,11 +411,11 @@ jws.oop.declareClass( "jws", "SecurityFilter", jws.EventsBaseFilter, {
 				if (aRequest.OnResponse){
 					aRequest.OnResponse({
 						code: -2,
-						msg: "User is not authenticated yet. Login first!"
+						msg: 'User is not authenticated yet. Login first!'
 					}, aRequest.args);
 				}
 				this.OnNotAuthorized(aToken);
-				throw "stop_filter_chain";
+				throw 'stop_filter_chain';
 			}
 
 			//Checking if the user have the allowed roles
@@ -424,8 +424,8 @@ jws.oop.declareClass( "jws", "SecurityFilter", jws.EventsBaseFilter, {
 				for (var k = 0; k < lUsers.length; k++){
 					lU = lUsers[k];
 					
-					if ("all" != lU){
-						lExclusion = (lU.substring(0,1) == "!") ? true : false;
+					if ('all' != lU){
+						lExclusion = (lU.substring(0,1) == '!') ? true : false;
 						lU = (lExclusion) ? lU.substring(1) : lU;
 
 						if (lU == this.user.principal){
@@ -446,11 +446,11 @@ jws.oop.declareClass( "jws", "SecurityFilter", jws.EventsBaseFilter, {
 				if (!lUserAuthorized && lUserMatch || 0 == lRoles.length){
 					aRequest.OnResponse({
 						code: -2,
-						msg: "Not autorized to notify this event. USER restrictions: " + lUsers.toString()
+						msg: 'Not autorized to notify this event. USER restrictions: ' + lUsers.toString()
 					}, aRequest.args);
 					
 					this.OnNotAuthorized(aToken);
-					throw "stop_filter_chain";
+					throw 'stop_filter_chain';
 				}
 			}
 
@@ -460,8 +460,8 @@ jws.oop.declareClass( "jws", "SecurityFilter", jws.EventsBaseFilter, {
 					for (var j = 0; j < this.user.roles.length; j++){
 						lR = lRoles[i];
 					
-						if ("all" != lR){
-							lExclusion = (lR.substring(0,1) == "!") ? true : false;
+						if ('all' != lR){
+							lExclusion = (lR.substring(0,1) == '!') ? true : false;
 							lR = (lExclusion) ? lR.substring(1) : lR;
 
 							if (lR == this.user.roles[j]){
@@ -487,24 +487,24 @@ jws.oop.declareClass( "jws", "SecurityFilter", jws.EventsBaseFilter, {
 					if (aRequest.OnResponse){
 						aRequest.OnResponse({
 							code: -2,
-							msg: "Not autorized to notify this event. ROLE restrictions: " + lRoles.toString()
+							msg: 'Not autorized to notify this event. ROLE restrictions: ' + lRoles.toString()
 						}, aRequest.args);
 					}
 					this.OnNotAuthorized(aToken);
-					throw "stop_filter_chain";
+					throw 'stop_filter_chain';
 				}
 			}
 		}
 	}
 	,
 	//:m:*:OnNotAuthorized
-	//:d:en:This method is called when a "not authorized" event notification _
+	//:d:en:This method is called when a 'not authorized' event notification _
 	//:d:en:is detected. Allows to define a global behiavor for this kind _
 	//:d:en:of exception.
-	//:a:en::aToken:Object:The "not authorized" token to be processed.
+	//:a:en::aToken:Object:The 'not authorized' token to be processed.
 	//:r:*:::void:none
 	OnNotAuthorized: function(aToken){
-		jws.console.error( "not_authorized" );
+		jws.console.error( 'not_authorized' );
 	}
 });
 
@@ -514,8 +514,8 @@ jws.oop.declareClass( "jws", "SecurityFilter", jws.EventsBaseFilter, {
 //:d:en:Implementation of the [tt]jws.CacheFilter[/tt] class. _
 //:d:en:This class handle the cache for every C2S event notification _
 //:d:en:in the client, using the server side cache configuration.
-jws.oop.declareClass( "jws", "CacheFilter", jws.EventsBaseFilter, {
-	id: "cache"
+jws.oop.declareClass( 'jws', 'CacheFilter', jws.EventsBaseFilter, {
+	id: 'cache'
 	,
 	cache:{}
 	,
@@ -526,7 +526,7 @@ jws.oop.declareClass( "jws", "CacheFilter", jws.EventsBaseFilter, {
 		this.user = aNotifier.user;
 		
 		// notifying to the server that cache is enabled in the client
-		aNotifier.notify("clientcacheaspect.setstatus", {
+		aNotifier.notify('clientcacheaspect.setstatus', {
 			args: {
 				enabled: true
 			}
@@ -573,7 +573,7 @@ jws.oop.declareClass( "jws", "CacheFilter", jws.EventsBaseFilter, {
 					aRequest.OnResponse(lCachedResponseEvent, aRequest.args);
 				}
 				
-				throw "stop_filter_chain";
+				throw 'stop_filter_chain';
 			}
 		}
 	}
@@ -609,8 +609,8 @@ jws.oop.declareClass( "jws", "CacheFilter", jws.EventsBaseFilter, {
 //:ancestor:*:jws.EventsBaseFilter
 //:d:en:Implementation of the [tt]jws.ValidatorFilter[/tt] class. _
 //:d:en:This class handle the validation for every argument in the request.
-jws.oop.declareClass( "jws", "ValidatorFilter", jws.EventsBaseFilter, {
-	id: "validator"
+jws.oop.declareClass( 'jws', 'ValidatorFilter', jws.EventsBaseFilter, {
+	id: 'validator'
 	,
 	
 	//:m:*:beforeCall
@@ -627,29 +627,29 @@ jws.oop.declareClass( "jws", "ValidatorFilter", jws.EventsBaseFilter, {
 				if (aRequest.OnResponse){
 					aRequest.OnResponse({
 						code: -4,
-						msg: "Argument '"+lArguments[i].name+"' is required!"
+						msg: 'Argument \''+lArguments[i].name+'\' is required!'
 					}, aRequest.args);
 				}
-				throw "stop_filter_chain";
+				throw 'stop_filter_chain';
 			}else if (aToken.hasOwnProperty(lArguments[i].name)){
 				var lRequiredType = lArguments[i].type;
 				var lArgumentType = jws.tools.getType(aToken[lArguments[i].name]);
 				
 				//Supporting the numberic types domain
-				if ("number" == lRequiredType && ("integer" == lArgumentType || "double" == lArgumentType)){
+				if ('number' == lRequiredType && ('integer' == lArgumentType || 'double' == lArgumentType)){
 					return;
 				}
-				if ("double" == lRequiredType && ("integer" == lArgumentType)){
+				if ('double' == lRequiredType && ('integer' == lArgumentType)){
 					return;
 				}
 				if (lRequiredType != lArgumentType){
 					if (aRequest.OnResponse){
 						aRequest.OnResponse({
 							code: -4,
-							msg: "Argument '"+lArguments[i].name+"' has invalid type. Required type is: '"+lRequiredType+"'!"
+							msg: 'Argument \''+lArguments[i].name+'\' has invalid type. Required type is: \''+lRequiredType+'\'!'
 						}, aRequest.args);
 					}
-					throw "stop_filter_chain";
+					throw 'stop_filter_chain';
 				}
 			}
 		}
--- jWebSocketClient/web/res/js/jwsScriptingPlugIn.js
+++ jWebSocketClient/web/res/js/jwsScriptingPlugIn.js
@@ -4,14 +4,14 @@
 //	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
 //  Alexander Schulze, Germany (NRW)
 //
-//	Licensed under the Apache License, Version 2.0 (the "License");
+//	Licensed under the Apache License, Version 2.0 (the 'License');
 //	you may not use this file except in compliance with the License.
 //	You may obtain a copy of the License at
 //
 //	http://www.apache.org/licenses/LICENSE-2.0
 //
 //	Unless required by applicable law or agreed to in writing, software
-//	distributed under the License is distributed on an "AS IS" BASIS,
+//	distributed under the License is distributed on an 'AS IS' BASIS,
 //	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //	See the License for the specific language governing permissions and
 //	limitations under the License.
@@ -24,14 +24,14 @@
 //:d:en:This client-side plug-in provides the API to access the features of the _
 //:d:en:Scripting plug-in on the jWebSocket server.
 jws.ScriptingPlugIn = {
-	//:const:*:NS:String:org.jwebsocket.plugins.scripting (jws.NS_BASE + ".plugins.scripting")
+	//:const:*:NS:String:org.jwebsocket.plugins.scripting (jws.NS_BASE + '.plugins.scripting')
 	//:d:en:Namespace for the [tt]ScriptingPlugIn[/tt] class.
 	// if namespace is changed update server plug-in accordingly!
-	NS: jws.NS_BASE + ".plugins.scripting",
+	NS: jws.NS_BASE + '.plugins.scripting',
 	//:const:*:JWS_NS:String:scripting
 	//:d:en:Namespace within the jWebSocketClient instance.
 	// if namespace changed update the applications accordingly!
-	JWS_NS: "scripting",
+	JWS_NS: 'scripting',
 			
 	//:m:*:callScriptMethod
 	//:d:en:Calls an script application published object method. 
@@ -46,7 +46,7 @@ jws.ScriptingPlugIn = {
 		if (0 === lRes.code) {
 			var lToken = {
 				ns: jws.ScriptingPlugIn.NS,
-				type: "callMethod",
+				type: 'callMethod',
 				method: aMethod,
 				objectId: aObjectId,
 				app: aApp,
@@ -67,7 +67,7 @@ jws.ScriptingPlugIn = {
 		if (0 === lRes.code) {
 			var lToken = {
 				ns: jws.ScriptingPlugIn.NS,
-				type: "reloadApp",
+				type: 'reloadApp',
 				app: aApp
 			};
 			this.sendToken(lToken, aOptions);
@@ -86,7 +86,7 @@ jws.ScriptingPlugIn = {
 		if (0 === lRes.code && aToken) {
 			aToken.app = aApp;
 			aToken.ns = jws.ScriptingPlugIn.NS;
-			aToken.type = "token";
+			aToken.type = 'token';
 
 			this.sendToken(aToken, aOptions);
 		}
