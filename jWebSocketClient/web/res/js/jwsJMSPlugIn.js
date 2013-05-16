27c75dd95f51a9aa34ab37b99e0ff4cb5e131bfa
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-05-16 12:01:37 +0000

    JMS, Apache Active MQ update, including JMS Stockticker demo.

--- jWebSocketClient/web/res/js/jwsJMSPlugIn.js
+++ jWebSocketClient/web/res/js/jwsJMSPlugIn.js
@@ -26,152 +26,141 @@
 //:d:en:plug-in provides the methods to subscribe and unsubscribe at certain _
 //:d:en:channel on the server.
 jws.JMSPlugIn = {
-
 	// :const:*:NS:String:org.jwebsocket.plugins.channels (jws.NS_BASE +
 	// ".plugins.jms")
 	// :d:en:Namespace for the [tt]ChannelPlugIn[/tt] class.
 	// if namespace changes update server plug-in accordingly!
-	NS : jws.NS_BASE + ".plugins.jms",
-
-	SEND_TEXT : "sendJmsText",
-	SEND_TEXT_MESSAGE : "sendJmsTextMessage",
-	SEND_MAP : "sendJmsMap",
-	SEND_MAP_MESSAGE : "sendJmsMapMessage",
-	LISTEN : "listenJms",
-	LISTEN_MESSAGE : "listenJmsMessage",
-	UNLISTEN : "unlistenJms",
-
-	listenJms : function(aConnectionFactoryName, aDestinationName, 
-		aPubSubDomain, aOptions) {
+	NS: jws.NS_BASE + ".plugins.jms",
+	SEND_TEXT: "sendJmsText",
+	SEND_TEXT_MESSAGE: "sendJmsTextMessage",
+	SEND_MAP: "sendJmsMap",
+	SEND_MAP_MESSAGE: "sendJmsMapMessage",
+	LISTEN: "listenJms",
+	LISTEN_MESSAGE: "listenJmsMessage",
+	UNLISTEN: "unlistenJms",
+	listenJms: function(aConnectionFactoryName, aDestinationName,
+			aPubSubDomain, aOptions) {
 		var lRes = this.checkConnected();
-		if (0 == lRes.code) {
+		if (0 === lRes.code) {
 			this.sendToken({
-				ns : jws.JMSPlugIn.NS,
-				type : jws.JMSPlugIn.LISTEN,
-				connectionFactoryName : aConnectionFactoryName,
-				destinationName : aDestinationName,
-				pubSubDomain : aPubSubDomain
-			}, aOptions );
+				ns: jws.JMSPlugIn.NS,
+				type: jws.JMSPlugIn.LISTEN,
+				connectionFactoryName: aConnectionFactoryName,
+				destinationName: aDestinationName,
+				pubSubDomain: aPubSubDomain
+			}, aOptions);
 		}
 		return lRes;
 	},
-
-	listenJmsMessage : function(aConnectionFactoryName, aDestinationName,
-		aPubSubDomain, aOptions) {
+	listenJmsMessage: function(aConnectionFactoryName, aDestinationName,
+			aPubSubDomain, aOptions) {
 		var lRes = this.checkConnected();
-		if (0 == lRes.code) {
+		if (0 === lRes.code) {
 			this.sendToken({
-				ns : jws.JMSPlugIn.NS,
-				type : jws.JMSPlugIn.LISTEN_MESSAGE,
-				connectionFactoryName : aConnectionFactoryName,
-				destinationName : aDestinationName,
-				pubSubDomain : aPubSubDomain
-			}, aOptions );
+				ns: jws.JMSPlugIn.NS,
+				type: jws.JMSPlugIn.LISTEN_MESSAGE,
+				connectionFactoryName: aConnectionFactoryName,
+				destinationName: aDestinationName,
+				pubSubDomain: aPubSubDomain
+			}, aOptions);
 		}
 		return lRes;
 	},
-
-	unlistenJms : function(aConnectionFactoryName, aDestinationName,
-		aPubSubDomain, aOptions) {
+	unlistenJms: function(aConnectionFactoryName, aDestinationName,
+			aPubSubDomain, aOptions) {
 		var lRes = this.checkConnected();
-		if (0 == lRes.code) {
+		if (0 === lRes.code) {
 			this.sendToken({
-				ns : jws.JMSPlugIn.NS,
-				type : jws.JMSPlugIn.UNLISTEN,
-				connectionFactoryName : aConnectionFactoryName,
-				destinationName : aDestinationName,
-				pubSubDomain : aPubSubDomain
-			}, aOptions );
+				ns: jws.JMSPlugIn.NS,
+				type: jws.JMSPlugIn.UNLISTEN,
+				connectionFactoryName: aConnectionFactoryName,
+				destinationName: aDestinationName,
+				pubSubDomain: aPubSubDomain
+			}, aOptions);
 		}
 		return lRes;
 	},
-	
-
-	sendJmsText : function(aConnectionFactoryName, aDestinationName,
-		aPubSubDomain, aText, aOptions ) {
+	sendJmsText: function(aConnectionFactoryName, aDestinationName,
+			aPubSubDomain, aText, aOptions) {
 		var lRes = this.checkConnected();
-		if (0 == lRes.code) {
+		if (0 === lRes.code) {
 			this.sendToken({
-				ns : jws.JMSPlugIn.NS,
-				type : jws.JMSPlugIn.SEND_TEXT,
-				connectionFactoryName : aConnectionFactoryName,
-				destinationName : aDestinationName,
-				pubSubDomain : aPubSubDomain,
-				msgPayLoad : aText
-			}, aOptions );
+				ns: jws.JMSPlugIn.NS,
+				type: jws.JMSPlugIn.SEND_TEXT,
+				connectionFactoryName: aConnectionFactoryName,
+				destinationName: aDestinationName,
+				pubSubDomain: aPubSubDomain,
+				msgPayLoad: aText
+			}, aOptions);
 		}
 		return lRes;
 	},
-
-	sendJmsTextMessage : function(aConnectionFactoryName, aDestinationName,
-		aPubSubDomain, aText, aJmsHeaderProperties, aOptions ) {
+	sendJmsTextMessage: function(aConnectionFactoryName, aDestinationName,
+			aPubSubDomain, aText, aJmsHeaderProperties, aOptions) {
 		var lRes = this.checkConnected();
-		if (0 == lRes.code) {
+		if (0 === lRes.code) {
 			this.sendToken({
-				ns : jws.JMSPlugIn.NS,
-				type : jws.JMSPlugIn.SEND_TEXT_MESSAGE,
-				connectionFactoryName : aConnectionFactoryName,
-				destinationName : aDestinationName,
-				pubSubDomain : aPubSubDomain,
-				msgPayLoad : aText,
-				jmsHeaderProperties : aJmsHeaderProperties
-			}, aOptions );
+				ns: jws.JMSPlugIn.NS,
+				type: jws.JMSPlugIn.SEND_TEXT_MESSAGE,
+				connectionFactoryName: aConnectionFactoryName,
+				destinationName: aDestinationName,
+				pubSubDomain: aPubSubDomain,
+				msgPayLoad: aText,
+				jmsHeaderProperties: aJmsHeaderProperties
+			}, aOptions);
 		}
 		return lRes;
 	},
-
-	sendJmsMap : function(aConnectionFactoryName, aDestinationName,
-		aPubSubDomain, aMap, aOptions ) {
+	sendJmsMap: function(aConnectionFactoryName, aDestinationName,
+			aPubSubDomain, aMap, aOptions) {
 		var lRes = this.checkConnected();
-		if (0 == lRes.code) {
+		if (0 === lRes.code) {
 			this.sendToken({
-				ns : jws.JMSPlugIn.NS,
-				type : jws.JMSPlugIn.SEND_MAP,
-				connectionFactoryName : aConnectionFactoryName,
-				destinationName : aDestinationName,
-				pubSubDomain : aPubSubDomain,
-				msgPayLoad : aMap
-			}, aOptions );
+				ns: jws.JMSPlugIn.NS,
+				type: jws.JMSPlugIn.SEND_MAP,
+				connectionFactoryName: aConnectionFactoryName,
+				destinationName: aDestinationName,
+				pubSubDomain: aPubSubDomain,
+				msgPayLoad: aMap
+			}, aOptions);
 		}
 		return lRes;
 	},
-	
-	sendJmsMapMessage : function(aConnectionFactoryName, aDestinationName,
-		aPubSubDomain, aMap, aJmsHeaderProperties, aOptions ) {
+	sendJmsMapMessage: function(aConnectionFactoryName, aDestinationName,
+			aPubSubDomain, aMap, aJmsHeaderProperties, aOptions) {
 		var lRes = this.checkConnected();
-		if (0 == lRes.code) {
+		if (0 === lRes.code) {
 			this.sendToken({
-				ns : jws.JMSPlugIn.NS,
-				type : jws.JMSPlugIn.SEND_MAP_MESSAGE,
-				connectionFactoryName : aConnectionFactoryName,
-				destinationName : aDestinationName,
-				pubSubDomain : aPubSubDomain,
-				msgPayLoad : aMap,
-				jmsHeaderProperties : aJmsHeaderProperties
-			}, aOptions );
+				ns: jws.JMSPlugIn.NS,
+				type: jws.JMSPlugIn.SEND_MAP_MESSAGE,
+				connectionFactoryName: aConnectionFactoryName,
+				destinationName: aDestinationName,
+				pubSubDomain: aPubSubDomain,
+				msgPayLoad: aMap,
+				jmsHeaderProperties: aJmsHeaderProperties
+			}, aOptions);
 		}
 		return lRes;
 	},
-
-	processToken : function(aToken) {
+	processToken: function(aToken) {
 		// check if namespace matches
-		if (aToken.ns == jws.JMSPlugIn.NS) {
+		if (aToken.ns === jws.JMSPlugIn.NS) {
 			// here you can handle incoming tokens from the server
 			// directy in the plug-in if desired.
-			if ("event" == aToken.type) {
-				if ("handleJmsText" == aToken.name) {
+			if ("event" === aToken.type) {
+				if ("handleJmsText" === aToken.name) {
 					if (this.OnHandleJmsText) {
 						this.OnHandleJmsText(aToken);
 					}
-				} else if ("handleJmsTextMessage" == aToken.name) {
+				} else if ("handleJmsTextMessage" === aToken.name) {
 					if (this.OnHandleJmsTextMessage) {
 						this.OnHandleJmsTextMessage(aToken);
 					}
-				} else if ("handleJmsMap" == aToken.name) {
+				} else if ("handleJmsMap" === aToken.name) {
 					if (this.OnHandleJmsMap) {
 						this.OnHandleJmsMap(aToken);
 					}
-				} else if ("handleJmsMapMessage" == aToken.name) {
+				} else if ("handleJmsMapMessage" === aToken.name) {
 					if (this.OnHandleJmsMapMessage) {
 						this.OnHandleJmsMapMessage(aToken);
 					}
@@ -179,8 +168,7 @@ jws.JMSPlugIn = {
 			}
 		}
 	},
-
-	setJMSCallbacks : function(aListeners) {
+	setJMSCallbacks: function(aListeners) {
 		if (!aListeners) {
 			aListeners = {};
 		}
