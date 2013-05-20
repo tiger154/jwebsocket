4827fa85c068d09dfe5b9bd3195039d7a8c25dd6
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-05-20 12:41:23 +0000

    starting jms tests, but server does not work as expected. Suspended until alex participation

--- branches/jWebSocket-1.0/jWebSocketClient/web/test/performance/js/Clients.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/test/performance/js/Clients.js
@@ -1,45 +1,61 @@
 var lWSC = new jws.jWebSocketJSONClient();
 $(document).ready(function(){
-	lWSC.open("ws://localhost:8787/jWebSocket/jWebSocket", {
-		OnWelcome: function(){
-			lWSC.login('root', 'root');
-		},
-		OnLogon: function(){
-			
-			$('#start').click(function(){
-				log('Openning client connections...', true);
+	$('#start').click(function(){
+		log('Openning client connections...', true);
 				
-				$('#start').fadeOut(500);
-				openConnections(parseInt($('#clients').val()));
-			});
-		}
+		$('#start').fadeOut(500);
+		openConnections(parseInt($('#clients').val()));
 	});
 });
 
 var lClients = [];
 var lActive = 0;
 openConnections = function(aClients){
-	for (var i = 0; i < aClients; i++){
-		var lC =  new jws.jWebSocketJSONClient();
-		lC.open("ws://localhost:8787/jWebSocket/jWebSocket", {
-			OnWelcome: function(){
-				log('Established. Active: ' + (++lActive), true);
-				
-				if ($('#transport').val() == 'channelBroadcast'){
-					lC.channelSubscribe("publicA", "access");
-					lC.setChannelCallbacks({
-						OnChannelBroadcast: function(aToken){
-						}
-					});
-				}
-			},
-			OnClose: function(){
-				log('Closed. Active: ' + (--lActive), true);
-			},
-			OnMessage: function(aMessage){
-			}
-		});
+	if (aClients == 0)
+		return;
 		
-		lClients.push(lC);
-	}
+	var lC =  new jws.jWebSocketJSONClient();
+	lC.open("ws://localhost:8787/jWebSocket/jWebSocket", {
+		OnWelcome: function(){
+			lC.login('root', 'root');
+		},
+		OnLogon: function(){
+			if ($('#transport').val() == 'channelBroadcast'){
+				lC.channelSubscribe("publicA", "access", {
+					OnSuccess: function(){
+						log('Established. Active: ' + (++lActive), true);
+					}
+				});
+				lC.setChannelCallbacks({
+					OnChannelBroadcast: function(aToken){
+					}
+				});
+			} else if ($('#transport').val() == 'broadcast'){
+				log('Established. Active: ' + (++lActive), true);
+			} else if ($('#transport').val() == 'jmsBroadcast'){
+				lC.listenJms( 
+					"connectionFactory",	// aConnectionFactoryName, 
+					"testQueue",			// aDestinationName, 
+					false, {
+						OnSuccess: function(aToken){
+							log('Established. Active: ' + (++lActive), true);
+						}
+					}
+					);
+				lC.setJMSCallbacks({
+					OnHandleJmsText: function(aToken){
+						console.log(aToken);
+					}
+				});
+			}
+			
+			lClients.push(lC);
+			openConnections(aClients - 1);
+		},
+		OnClose: function(){
+			log('Closed. Active: ' + (--lActive), true);
+		},
+		OnMessage: function(aMessage){
+		}
+	});
 }
