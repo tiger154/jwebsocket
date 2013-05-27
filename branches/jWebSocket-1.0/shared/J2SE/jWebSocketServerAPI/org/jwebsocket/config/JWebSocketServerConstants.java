01cf9a9e45ce691d0bcf53d70fa6a88085dcd4dc
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-05-27 14:52:11 +0000

    Deleted obsolete AppServer project, added new NetBeans WebAppDemo project, adjusted read_me's, updated deployment batches.

--- branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/config/JWebSocketServerConstants.java
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/config/JWebSocketServerConstants.java
@@ -28,7 +28,7 @@ public final class JWebSocketServerConstants {
 	/**
 	 * Current version string of the jWebSocket package.
 	 */
-	public static final String VERSION_STR = "1.0 RC2 (build 30522)";
+	public static final String VERSION_STR = "1.0 RC2 (build 30526)";
 	/**
 	 * Name space base for tokens and plug-ins.
 	 */
