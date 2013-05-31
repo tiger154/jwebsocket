acf4321705d0c268918692794f3a6a1a99353695
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-05-31 10:37:55 +0000

    Fix jWebSocketSenchaPlugIn, load action generated getNS and getTokenType to access to private attributes from the action class.

--- branches/jWebSocket-1.0/jWebSocketClient/web/lib/ExtJS/jWebSocketSenchaPlugIn/form/action/Load.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/lib/ExtJS/jWebSocketSenchaPlugIn/form/action/Load.js
@@ -36,9 +36,9 @@
 //:d:en:Implementation of the default load action of the form but using _
 //:d:en:jWebSocketClient to get the data from the jWebSocket Server. _
 //:d:en:This class representsis a proxy for the underlying BasicForm's load call
-Ext.define( 'Ext.jws.form.action.Load', {
+Ext.define('Ext.jws.form.action.Load', {
 	extend: 'Ext.form.action.Load',
-	requires: [ 'Ext.direct.Manager', 'Ext.jws.Client' ],
+	requires: ['Ext.direct.Manager', 'Ext.jws.Client'],
 	alternateClassName: 'Ext.jws.form.action.Load',
 	alias: 'formaction.jwsload',
 	type: 'jwsload',
@@ -52,15 +52,29 @@ Ext.define( 'Ext.jws.form.action.Load', {
 	//:r:*::void:none
 	constructor: function( ) {
 		var lSelf = this;
-		lSelf.callParent( arguments );
+		lSelf.callParent(arguments);
 
-		if ( typeof lSelf.ns === "undefined" ) {
-			Ext.Error.raise( "You must specify a namespace (ns) value!" );
+		if (typeof lSelf.ns === "undefined") {
+			Ext.Error.raise("You must specify a namespace (ns) value!");
 		}
 
-		if ( typeof lSelf.tokentype === "undefined" ) {
-			Ext.Error.raise( "You must specify a token type (tokentype) value!" );
+		if (typeof lSelf.tokentype === "undefined") {
+			Ext.Error.raise("You must specify a token type (tokentype) value!");
 		}
+	},
+	//:m:*:getNS
+	//:d:en:Returns the namespace from the current form action
+	//:a:en::::none
+	//:r:*:lNs:String:The namespace
+	getNS: function() {
+		return this.ns || this.form.ns;
+	},
+	//:m:*:getTokenType
+	//:d:en:Returns the tokentype from the current form action
+	//:a:en::::none
+	//:r:*:lTokenType:String:The type of the token
+	getTokenType: function() {
+		return this.tokentype || this.form.tokentype;
 	},
 	//:m:*:run
 	//:d:en:This method is executed by the internal doAction method to _
@@ -71,19 +85,19 @@ Ext.define( 'Ext.jws.form.action.Load', {
 	//:r:*::void:none
 	run: function( ) {
 		var lCallbacks = this.createCallback();
-		Ext.jws.Client.send( this.ns, this.tokentype, this.getParams(), lCallbacks, this );
+		Ext.jws.Client.send(this.getNS(), this.getTokenType(), this.getParams(), lCallbacks, this);
 	},
 	//:m:*:processResponse
 	//:d:en:Processes the response returned by the server and fires the success _
 	//:d:en:or failure callback depending on the results
 	//:a:en::aResponse:Object:The response returned by the server
 	//:r:*:fResult:Object:The response ready for the user
-	processResponse: function( aResponse ) {
+	processResponse: function(aResponse) {
 		this.fResponse = aResponse;
-		if ( !aResponse.responseText && !aResponse.responseXML && !aResponse.type ) {
+		if (!aResponse.responseText && !aResponse.responseXML && !aResponse.type) {
 			return true;
 		}
-		return (this.fResult = this.handleResponse( aResponse ));
+		return (this.fResult = this.handleResponse(aResponse));
 	},
 	//:m:*:handleResponse
 	//:d:en:Internal method to change the incoming jWebSocket server resopnse _
@@ -92,14 +106,14 @@ Ext.define( 'Ext.jws.form.action.Load', {
 	//:d:en:or false, in other case the response will be success
 	//:a:en::aResponse:Object:The response returned by the server
 	//:r:*:fResult:Object:The response modified and ready for the client
-	handleResponse: function( aResponse ) {
-		if ( aResponse ) {
+	handleResponse: function(aResponse) {
+		if (aResponse) {
 			var lData = aResponse.data ? aResponse.data[0] : null;
 			return {
 				success: aResponse.success || aResponse.code === 0,
 				data: lData
 			};
 		}
-		return Ext.decode( aResponse.data );
+		return Ext.decode(aResponse.data);
 	}
-} );
+});
