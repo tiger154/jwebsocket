fb89070e442e02970503bf49e1ea4623ff6830ac
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-07-05 15:45:53 +0000

    Added getOptions to jWebSocket.js, refactored JMS bridge to be JMS Gateway, added new JMS config files, updated Perl files according to new conventions.

--- jWebSocketPlugIns/jWebSocketJMSPlugIn/src/main/java/org/jwebsocket/plugins/jms/gateway/JMSSender.java
+++ jWebSocketPlugIns/jWebSocketJMSPlugIn/src/main/java/org/jwebsocket/plugins/jms/gateway/JMSSender.java
@@ -16,7 +16,7 @@
 //	See the License for the specific language governing permissions and
 //	limitations under the License.
 //	---------------------------------------------------------------------------
-package org.jwebsocket.plugins.jms.bridge;
+package org.jwebsocket.plugins.jms.gateway;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
