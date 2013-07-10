c0ef7b959278660013eacb7abdaed577ef74f3d4
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-07-10 13:21:39 +0000

    Updated license headers for Shared Objectsl plug-in

--- branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketSharedObjectsPlugIn/src/main/java/org/jwebsocket/plugins/sharedobjects/SharedCanvas.java
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketSharedObjectsPlugIn/src/main/java/org/jwebsocket/plugins/sharedobjects/SharedCanvas.java
@@ -1,7 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// ---------------------------------------------------------------------------
+// jWebSocket - SharedCanvas (Community Edition, CE)
+//	---------------------------------------------------------------------------
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org), Germany (NRW), Herzogenrath
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+//	---------------------------------------------------------------------------
 package org.jwebsocket.plugins.sharedobjects;
 
 import java.util.List;
@@ -20,7 +33,7 @@ public class SharedCanvas extends BaseSharedObject {
 	 * @param aCmd
 	 */
 	public void addCmd(String aCmd) {
-		 mCmds.add(aCmd);
+		mCmds.add(aCmd);
 	}
 
 	/**
@@ -28,6 +41,6 @@ public class SharedCanvas extends BaseSharedObject {
 	 * @param aIndex
 	 */
 	public void removeCmd(int aIndex) {
-		 mCmds.remove(aIndex);
+		mCmds.remove(aIndex);
 	}
 }
