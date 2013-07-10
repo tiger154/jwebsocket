3940a7d14e563141bf46ae3e2022a6ace1bb59ca
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-07-10 13:21:39 +0000

    Updated license headers for Shared Objectsl plug-in

--- jWebSocketPlugIns/jWebSocketSharedObjectsPlugIn/src/main/java/org/jwebsocket/plugins/sharedobjects/SharedObject.java
+++ jWebSocketPlugIns/jWebSocketSharedObjectsPlugIn/src/main/java/org/jwebsocket/plugins/sharedobjects/SharedObject.java
@@ -1,19 +1,20 @@
+// ---------------------------------------------------------------------------
+// jWebSocket - SharedObject (Community Edition, CE)
 //	---------------------------------------------------------------------------
-//	jWebSocket - Shared Object
-//	(C) 2010 jWebSocket.org, Alexander Schulze, Innotrade GmbH
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org), Germany (NRW), Herzogenrath
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 //	---------------------------------------------------------------------------
-//	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU Lesser General Public License as published by the
-//	Free Software Foundation; either version 3 of the License, or (at your
-//	option) any later version.
-//	This program is distributed in the hope that it will be useful, but WITHOUT
-//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
-//	more details.
-//	You should have received a copy of the GNU Lesser General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
-//	---------------------------------------------------------------------------
-
 package org.jwebsocket.plugins.sharedobjects;
 
 import java.util.Date;
@@ -32,7 +33,6 @@ public class SharedObject {
 	 *
 	 */
 	public static int MODE_READ_ONLY = 1;
-
 	/**
 	 *
 	 */
@@ -41,7 +41,6 @@ public class SharedObject {
 	 *
 	 */
 	public static int LOCK_STATE_LOCKED = 1;
-
 	private Object object = null;
 	private int mode = 0; // see above
 	private int lockstate = 0; // see above
@@ -102,5 +101,4 @@ public class SharedObject {
 	public void setLock_timestamp(Date lock_timestamp) {
 		this.lock_timestamp = lock_timestamp;
 	}
-
 }
