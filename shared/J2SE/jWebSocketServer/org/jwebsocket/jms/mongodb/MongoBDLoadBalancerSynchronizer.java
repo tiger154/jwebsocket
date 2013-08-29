da2ea7cd80fd7fe209aac0b87360c87341fbd3e3
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-08-29 16:14:30 +0000

    uploading MongoDB load balancer synchronizer

---
+++ shared/J2SE/jWebSocketServer/org/jwebsocket/jms/mongodb/MongoBDLoadBalancerSynchronizer.java
@@ -0,0 +1,68 @@
+//	---------------------------------------------------------------------------
+//	jWebSocket - MongoBDLoadBalancerSynchronizer (Community Edition, CE)
+//	---------------------------------------------------------------------------
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+//	---------------------------------------------------------------------------
+package org.jwebsocket.jms.mongodb;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import org.jwebsocket.jms.Attributes;
+import org.jwebsocket.jms.api.ILoadBalancerSynchronizer;
+
+/**
+ * Implementation based on MongoDB primary key duplication restriction exception
+ *
+ * @author kyberneees
+ */
+public class MongoBDLoadBalancerSynchronizer implements ILoadBalancerSynchronizer {
+
+	private DBCollection mCollection;
+
+	public void setCollection(DBCollection aCollection) {
+		this.mCollection = aCollection;
+	}
+
+	public DBCollection getCollection() {
+		return mCollection;
+	}
+
+	@Override
+	public boolean getLoadBalancerTurn(String aMessageId) {
+		try {
+			mCollection.insert(new BasicDBObject().append(Attributes.MESSAGE_ID, aMessageId));
+			return true;
+		} catch (Exception lEx) {
+			return false;
+		}
+	}
+
+	@Override
+	public void initialize() throws Exception {
+		mCollection.ensureIndex(new BasicDBObject().append(Attributes.MESSAGE_ID, 1),
+				new BasicDBObject().append("unique", true));
+		if (!mCollection.isCapped()) {
+			// converting collection to capped (limiting collection size)
+			mCollection.getDB().command(new BasicDBObject()
+					.append("convertToCapped", mCollection.getName())
+					.append("max", 1000000));
+		}
+	}
+
+	@Override
+	public void shutdown() throws Exception {
+	}
+}
