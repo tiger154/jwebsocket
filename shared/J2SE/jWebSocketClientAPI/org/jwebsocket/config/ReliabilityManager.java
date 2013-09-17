3ba23f255e58a92ccd46013fd5d2d78dd4f9a4ac
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-09-17 18:44:25 +0000

    adding generic client classes and adding methods to the WebSocketClient interface

--- shared/J2SE/jWebSocketClientAPI/org/jwebsocket/config/ReliabilityManager.java
+++ shared/J2SE/jWebSocketClientAPI/org/jwebsocket/config/ReliabilityManager.java
@@ -12,7 +12,7 @@
 //	You should have received a copy of the GNU Lesser General Public License along
 //	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
 //	---------------------------------------------------------------------------
-package org.jwebsocket.client.java;
+package org.jwebsocket.config;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
