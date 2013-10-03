3618a7b8f788816a373bb1b0f40c7f0fcb74860a
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-10-03 17:47:37 +0000

    adding Attributes

--- shared/J2SE/jWebSocketCommon/org/jwebsocket/jms/Attributes.java
+++ shared/J2SE/jWebSocketCommon/org/jwebsocket/jms/Attributes.java
@@ -7,11 +7,13 @@ package org.jwebsocket.jms;
 public class Attributes {
 
 	public static final String SESSION_ID = "sessionId";
+	public static final String CORRELATION_ID = "correlationId";
 	public static final String IP_ADDRESS = "ipAddress";
 	public static final String IP_BROADCAST = "isBroadcast";
 	public static final String MESSAGE_TYPE = "msgType";
 	public static final String NODE_ID = "nodeId";
 	public static final String NAMESPACE = "ns";
+	public static final String DESTINATION = "destination";
 	public static final String CONNECTION_ID = "connectionId";
 	public static final String CONSUMER_ID = "consumerId";
 	public static final String STATUS = "status";
@@ -21,5 +23,6 @@ public class Attributes {
 	public static final String DATA = "data";
 	public static final String MESSAGE_ID = "msgId";
 	public static final String REPLY_SELECTOR = "replySelector";
+	public static final String SELECTOR = "selector";
 	public static final String SENDER_INCLUDED = "senderIncluded";
 }
