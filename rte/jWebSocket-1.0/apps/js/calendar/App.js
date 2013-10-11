2e97aebfe1001f76b56680536cbb8a76ba7ac47c
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-10-11 14:42:38 +0000

    adding license headers

--- rte/jWebSocket-1.0/apps/js/calendar/App.js
+++ rte/jWebSocket-1.0/apps/js/calendar/App.js
@@ -1,3 +1,22 @@
+//	---------------------------------------------------------------------------
+//	jWebSocket Calendar script app demo (Community Edition, CE)
+//	---------------------------------------------------------------------------
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+//	---------------------------------------------------------------------------
+
 App.on('appLoaded', function(aHotLoad){
 	// setting the app description
 	App.setDescription('Basic Calendar application to get the server-side date/time');
@@ -30,8 +49,8 @@ App.on('appLoaded', function(aHotLoad){
 	// publishing the 'Calendar' object to the client-side
 	App.publish('Calendar', {
 		getDate: function(aDateFormat){
-			var lFormatter = new Class('java.text.SimpleDateFormat')(aDateFormat);
-			var lNow = new Class('java.util.Date')();
+			var lFormatter = new Packages.java.text.SimpleDateFormat(aDateFormat);
+			var lNow = new Packages.java.util.Date();
 			
 			return lFormatter.format(lNow);
 		}
