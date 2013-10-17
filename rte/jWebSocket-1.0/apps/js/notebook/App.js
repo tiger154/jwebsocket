ebce5ff683b0b6ea6b772e6a61ef13a9efc02239
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-10-17 14:14:37 +0000

    removing unneeded ns

--- rte/jWebSocket-1.0/apps/js/notebook/App.js
+++ rte/jWebSocket-1.0/apps/js/notebook/App.js
@@ -16,8 +16,6 @@
 //	See the License for the specific language governing permissions and
 //	limitations under the License.
 //	---------------------------------------------------------------------------
-var NS = "org.jwebsocket.plugins.scripting.notebook";
-
 App.importScript('${EXT}/MongoDBUtils');
 App.importScript('${APP_HOME}/service/NotesService');
 
