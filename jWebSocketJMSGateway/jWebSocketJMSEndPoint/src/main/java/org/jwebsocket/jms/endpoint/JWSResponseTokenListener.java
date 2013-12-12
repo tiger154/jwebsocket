ea6efcd45c3b4cc90f6202fcbeedd7994376e0dc
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2013-12-12 13:22:21 +0000

    adding missing class

---
+++ jWebSocketJMSGateway/jWebSocketJMSEndPoint/src/main/java/org/jwebsocket/jms/endpoint/JWSResponseTokenListener.java
@@ -0,0 +1,57 @@
+//  ---------------------------------------------------------------------------
+//  jWebSocket - JWSResponseTokenListener (Community Edition, CE)
+//	---------------------------------------------------------------------------
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
+//  Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+//	---------------------------------------------------------------------------
+package org.jwebsocket.jms.endpoint;
+
+import javax.jms.Message;
+import org.jwebsocket.packetProcessors.JSONProcessor;
+import org.jwebsocket.token.Token;
+
+/**
+ *
+ * @author kyberneees
+ */
+public class JWSResponseTokenListener implements IJMSResponseListener {
+
+	@Override
+	public void onReponse(String aReponse, Message aMessage) {
+		Token lResponseToken = JSONProcessor.JSONStringToToken(aReponse);
+
+		// calling token callbacks
+		onReponse(lResponseToken);
+
+		if (0 == lResponseToken.getCode()) {
+			onSuccess(lResponseToken);
+		} else {
+			onFailure(lResponseToken);
+		}
+	}
+
+	public void onReponse(Token aReponse) {
+	}
+
+	public void onSuccess(Token aReponse) {
+	}
+
+	public void onFailure(Token aReponse) {
+	}
+
+	@Override
+	public void onTimeout() {
+	}
+}
