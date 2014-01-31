d8c89254a2de2bd5e7f5fc2f3c05eb83e4144f2a
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-01-31 11:59:28 +0000

    Debug logs for flash bridge now configurable, flash usage can be forced for development and test purpose, updated to build 140131.

--- branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/config/JWebSocketServerConstants.java
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/config/JWebSocketServerConstants.java
@@ -28,7 +28,7 @@ public final class JWebSocketServerConstants {
 	/**
 	 * Current version string of the jWebSocket package.
 	 */
-	public static final String VERSION_STR = "1.0.0 RC2 (build 40123)";
+	public static final String VERSION_STR = "1.0.0 RC2 (build 40131)";
 	/**
 	 * Name space base for tokens and plug-ins.
 	 */
