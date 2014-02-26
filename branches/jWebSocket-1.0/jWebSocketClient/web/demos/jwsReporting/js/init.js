510490664d5597a4324eed88eada2bd5a5b93ec0
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-02-26 18:15:40 +0000

    fixing reporting plug-in demo

--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsReporting/js/init.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsReporting/js/init.js
@@ -1,43 +1,55 @@
 /* 
  * @author vbarzana
+ * @author jpuentes
  */
 
 function init(){
-	w                   = {};
-	mLog                = {};
+	mWSC = {};
+	w = {};
+	mLog = {};
 	mLog.isDebugEnabled = true;
 	
-	$( "#demo_box" ).reporting();
+	// Setting the styles to the buttons, avoiding to fill the HTML code 
+	// with unnecessary data
+	$('.button').each(function( ) {
+		var lBtn = $(this);
+		var lRightClass = lBtn.hasClass('download') ? 'r_download' : 'btn_right';
+		lBtn.attr("class", "button onmouseup")
+				.attr("onmouseover", "this.className='button onmouseover'")
+				.attr("onmousedown", "this.className='button onmousedown'")
+				.attr("onmouseup", "this.className='button onmouseup'")
+				.attr("onmouseout", "this.className='button onmouseout'")
+				.attr("onclick", "this.className='button onmouseover'");
+		lBtn.html('<div class="btn_left"/>' + '<div class="btn_center">' +
+				lBtn.html( ) + '</div>' + '<div class="' + lRightClass + '"></div>');
+	});
+
+	//Configuring tooltip
+	$("[title]").tooltip({
+		position: "bottom center",
+		onShow: function( ) {
+			var lTip = this.getTip( );
+			var lTop = ("<div class='top'></div>");
+			var lMiddle = $("<div class='middle'></div>").text(lTip.text( ));
+			var lBottom = ("<div class='bottom'></div>");
+			lTip.html("").append(lTop).append(lMiddle).append(lBottom);
+			this.getTrigger( ).mouseout(function( ) {
+				lTip.hide( ).hide( );
+			});
+			this.getTrigger( ).mousemove(function( ) {
+				lTip.show( );
+			});
+		}
+	});
 
 	// Options
 	// @maxLogLines: maximum number of lines that will be logged
 	// @linesToDelete: quantity of lines that will be deleted from 
 	// the log window each time the log exceeds the maxLogLines
-	$("#log_box").log({
-		maxLogLines: 200, 
-		linesToDelete: 20
-	});
-	
-	//Configuring tooltip as we wish
-	$("[title]").tooltip({
-		position: "bottom center", 
-		onShow: function() {
-			var lTip = this.getTip();
-			var lTop = ("<div class='top'></div>");
-			var lMiddle = $("<div class='middle'></div>").text(lTip.text());
-			var lBottom = ("<div class='bottom'></div>");
-			lTip.html("").append(lTop).append(lMiddle).append(lBottom);
-			this.getTrigger( ).mouseout( function( ) {
-				lTip.hide( ).hide( );
-			});
-			this.getTrigger( ).mousemove( function( ) {
-				lTip.show( );
-			});
-		}
-	});
+	$("#log_box").log();
+	$("#container").reporting( );
 }
 
-
-$(document).ready(function(){
+$(document).ready(function( ) {
 	init();
 });
--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsReporting/js/wReporting.js
+++ branches/jWebSocket-1.0/jWebSocketClient/web/demos/jwsReporting/js/wReporting.js
@@ -35,8 +35,11 @@ $.widget("jws.reporting", {
 		this.eBtnCreateReport = this.element.find("#create_report_btn");
 		this.eTxaReportFields = jws.$("jws_reporting_fields_txa");
 		this.eTxaReportParams = jws.$("jws_reporting_params_txa");
-		this.eBtnTestGetReports = this.element.find("#test_get_reports_btn");
-		this.eBtnTestCreateReport = this.element.find("#test_create_report_btn");
+		this.eGenContentBox = jws.$("gen_content_box");
+		$("#gen_content_box").hide();
+		$("#help_footer").hide();
+		this.eFormIsShow = false;
+		this.eHelpIsShow = false;
 
 //		this.eCbReportType = this.element.find("#report_type_cb");
 //		this.ePdfObject = this.element.find("#pdf");
@@ -46,7 +49,7 @@ $.widget("jws.reporting", {
 		// Keeping a reference of the widget, when a websocket message
 		// comes from the server the scope "this" doesnt exist anymore
 		w.reporting = this;
-		w.reporting.registerEvents();
+		w.reporting.registerEvents( );
 	},
 	/**
 	 * Registers all callbacks, and assigns all buttons and dom elements actions
@@ -64,6 +67,7 @@ $.widget("jws.reporting", {
 				});
 			},
 			OnWelcome: function(aEvent) {
+				
 			},
 			OnClose: function() {
 			},
@@ -88,13 +92,43 @@ $.widget("jws.reporting", {
 				}
 			}
 		};
-		$("#demo_box").auth(lCallbacks);
 		// Registering click events of DOM elements
 		w.reporting.eBtnGetReports.click(w.reporting.getReports);
 		w.reporting.eBtnUpTemplate.click(w.reporting.uploadTemplate);
 		w.reporting.eBtnCreateReport.click(w.reporting.createReport);
-		w.reporting.eBtnTestGetReports.click(w.reporting.testgetReports);
-		w.reporting.eBtnTestCreateReport.click(w.reporting.testcreateReport);
+
+		$("#show_icon").click(function() {
+			if (w.reporting.eFormIsShow) {
+				$("#gen_content_box").hide("slow");
+				w.reporting.eFormIsShow = false;
+			}
+			else {
+				$("#gen_content_box").show("slow");
+				w.reporting.eFormIsShow = true;
+			}
+		});
+		
+		$("#show_icon_help").click(function() {
+			if (w.reporting.eHelpIsShow ) {
+				$("#help_footer").hide("slow");
+				w.reporting.eHelpIsShow = false;
+			}
+			else {
+				$("#help_footer").show("slow");
+				w.reporting.eHelpIsShow  = true;
+			}
+		});
+		
+		w.reporting.eTxaReportFields.value = "[{name: 'Alexander', lastName: 'Schulze', age: 40, email: 'a.schulze@jwebsocket.org'},\n\
+			{name: 'Rolando', lastName: 'Santamaria Maso', age: 27, email: 'r.santamaria@jwebsocket.org'},	\n\
+			{name: 'Lisdey', lastName: 'Perez', age: 27, email: 'l.perez@jwebsocket.org'},	\n\
+			{name: 'Marcos', lastName: 'Gonzalez', age: 27, email: 'm.gonzalez@jwebsocket.org'},\n\
+			{name: 'Osvaldo', lastName: 'Aguilar', age: 27, email: 'o.aguilar@jwebsocket.org'},\n\
+			{name: 'Victor', lastName: 'Barzana', age: 27, email: 'v.barzana@jwebsocket.org'},\n\
+			{name: 'Javier Alejandro', lastName: 'Puentes Serrano', age: 27, email: 'j.puentes@jwebsocket.org'}]";
+		w.reporting.eTxaReportParams.value = "{reportTitle: 'JWebSocket Contact Report'}";
+		
+		$("#demo_box").auth(lCallbacks);
 	},
 	/**
 	 * Executed every time the server sends a message to the client
@@ -107,10 +141,10 @@ $.widget("jws.reporting", {
 //			if (aToken.type == "response") {
 //				// If the login data is ok
 //				if (aToken.reqType == "login" && aToken.code == 0) {
-//					var lSuccess = new PDFObject({
-//						url: "jwsReportSample.pdf"
-//					}).
-//							embed("pdf");
+////					var lSuccess = new PDFObject({
+////						url: "jwsReportSample.pdf"
+////					}).
+////							embed("pdf");
 //				}
 //				// If anything went wrong in the server show information error
 //				if (aToken.code == -1) {
@@ -166,67 +200,73 @@ $.widget("jws.reporting", {
 					true, "alert");
 		}
 	},
-	createReport: function() {
-		if (mWSC.isConnected()) {
-			var lReportId = w.reporting.eCbReportList.value;
-			var lFormat = w.reporting.eCbReportFormats.value;
-
-			if (!w.reporting.eTxaReportFields.value) {
-				jwsDialog("Enter the report fields [{id:'value'},{id:'value'},{id:'value'}]", "jWebSocket info",
-						true, "alert");
-			}
-			else if (!w.reporting.eTxaReportParams.value) {
-				jwsDialog("Enter the report params {id:'value'}", "jWebSocket info",
-						true, "alert");
+	createReport: function() {		
+		if (!w.reporting.eFormIsShow) {
+			if ((mWSC.isConnected())) {
+				var lReportName = "person";
+				var lParams = {reportTitle: 'JWebSocket Contact Report'};
+				var lFields = [
+					{name: 'Alexander', lastName: 'Schulze', age: 40, email: 'a.schulze@jwebsocket.org'},
+					{name: 'Rolando', lastName: 'Santamaria Maso', age: 27, email: 'r.santamaria@jwebsocket.org'},
+					{name: 'Lisdey', lastName: 'Perez', age: 27, email: 'l.perez@jwebsocket.org'},
+					{name: 'Marcos', lastName: 'Gonzalez', age: 27, email: 'm.gonzalez@jwebsocket.org,'},
+					{name: 'Osvaldo', lastName: 'Aguilar', age: 27, email: 'o.aguilar@jwebsocket.org,'},
+					{name: 'Victor', lastName: 'Barzana', age: 27, email: 'v.barzana@jwebsocket.org,'},
+					{name: 'Javier Alejandro', lastName: 'Puentes Serrano', age: 27, email: 'j.puentes@jwebsocket.org'}];
+				log("Creating Report...");
+				mWSC.reportingGenerateReport(
+						lReportName,
+						lParams,
+						lFields,
+						{
+							useConection: false,
+							outputType: "pdf"
+						}
+				);
 			}
 			else {
-				eval("var lParams =" + w.reporting.eTxaReportParams.value);
-				eval("var lFields =" + w.reporting.eTxaReportFields.value);
-				log("Creating Report...");
-				if (mWSC.isConnected()) {
-					mWSC.reportingGenerateReport(
-							lReportId,
-							lParams,
-							lFields,
-							{
-								useConection: w.reporting.eChbxUseConnection.checked,
-								outputType: lFormat
-							}
-					);
-				} else {
-					jwsDialog(w.reporting.MSG_NOTCONNECTED, "jWebSocket error",
+				jwsDialog(w.reporting.MSG_NOTCONNECTED, "jWebSocket error",
+						true, "alert");
+			}
+		}
+		else
+		{
+			log("Creating Report...");
+			if (mWSC.isConnected()) {
+				var lReportName = w.reporting.eCbReportList.value;
+				var lFormat = w.reporting.eCbReportFormats.value;
+
+				if (!w.reporting.eTxaReportFields.value) {
+					jwsDialog("Enter the report fields [{id:'value'},{id:'value'},{id:'value'}]", "jWebSocket info",
 							true, "alert");
 				}
-			}
-		} else {
-			jwsDialog(w.reporting.MSG_NOTCONNECTED, "jWebSocket error",
-					true, "alert");
-		}
-	},
-	testgetReports: function(aToken) {
-		if (mWSC.isConnected()) {
-			log("Retreiving list of reports via jWebSocket...");
-			mWSC.reportingGetReports({
-				OnResponse: function(aToken) {
-					log("Reports " + aToken.data);
+				else if (!w.reporting.eTxaReportParams.value) {
+					jwsDialog("Enter the report params {id:'value'}", "jWebSocket info",
+							true, "alert");
 				}
-			});
-		} else {
-			jwsDialog(w.reporting.MSG_NOTCONNECTED, "jWebSocket error",
-					true, "alert");
+				else if (!w.reporting.eCbReportList.value) {
+					jwsDialog("Please select an available report from the list", "jWebSocket info",
+							true, "alert");
+				}
+				eval("var lParams =" + w.reporting.eTxaReportParams.value);
+				eval("var lFields =" + w.reporting.eTxaReportFields.value);
+
+				mWSC.reportingGenerateReport(
+						lReportName,
+						lParams,
+						lFields,
+						{
+							useConection: w.reporting.eChbxUseConnection.checked,
+							outputType: lFormat
+						}
+				);
+			} else {
+				jwsDialog(w.reporting.MSG_NOTCONNECTED, "jWebSocket error",
+						true, "alert");
+			}
 		}
-	},
-	testcreateReport: function(aToken) {
-		if (mWSC.isConnected()) {
-			log("Creating a report...");
-			mWSC.sendToken({
-				ns: "org.jwebsocket.plugins.testreporting",
-				type: "testGenerateReport"
-			});
-		} else {
-			jwsDialog(w.reporting.MSG_NOTCONNECTED, "jWebSocket error",
-					true, "alert");
-		}
+
+
 	},
 	handleReport: function(aToken) {
 		mWSC.fileLoad(aToken.path, jws.FileSystemPlugIn.ALIAS_PRIVATE, {
