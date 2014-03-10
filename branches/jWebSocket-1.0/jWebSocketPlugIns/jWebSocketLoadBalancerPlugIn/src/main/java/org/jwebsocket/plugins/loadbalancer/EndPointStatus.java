0d77d730a91a1e80eb7c9744a22bfbb1b526ebd2
rbetancourt <rbetancourt@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-03-10 07:28:08 +0000

    New implementation for the next methods in the jWebSocketloadBalancerPlugIn :
    public void shutdownServiceEndPointAction(final WebSocketConnector aConnector, final Token aToken)
    
    public ClusterEndPoint registerEndPoint(WebSocketConnector aConnector)
    
    public ClusterEndPoint removeEndPoint(ClusterEndPoint aClusterEndPoint)
    
    
    
    Fixed some errors in next methods:
    
    public ClusterEndPoint getRoundRobinEndPoint()
    
    public ClusterEndPoint getOptimumEndPoint()
    
    public ClusterEndPoint getOptimumRREndPoint()
    
    private ClusterEndPoint getOptimumServiceEndPoint(String aNamespace)
    
    
    Fixed some errors in general(descriptions, comments, names).
    Fixed some conceptual errors.
    
    I have added new settings and new entries in the loadbalancer.xml.

--- branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketLoadBalancerPlugIn/src/main/java/org/jwebsocket/plugins/loadbalancer/EndPointStatus.java
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketLoadBalancerPlugIn/src/main/java/org/jwebsocket/plugins/loadbalancer/EndPointStatus.java
@@ -25,18 +25,13 @@ package org.jwebsocket.plugins.loadbalancer;
  */
 public enum EndPointStatus {
 
-	/**
-	 *
-	 */
 	OFFLINE, // disconnected and unavailable.
-
-	/**
-	 *
-	 */
+        /**
+         * 
+         */
 	ONLINE, // connected and available.
-
-	/**
-	 *
-	 */
-	SHUTTING_DOWN
+        /**
+         * 
+         */
+	SHUTTING_DOWN // end point to be shutdown, do not accept new clients.
 }
