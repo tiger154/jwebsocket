e6c6033e8c8bfdb42c9ea6471f3b9767f26ddfcb
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-03-11 14:12:02 +0000

    fixing header comments

--- branches/jWebSocket-1.0/shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/system/SystemPlugIn.java
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/system/SystemPlugIn.java
@@ -67,12 +67,11 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 
 /**
- * implements the jWebSocket system tokens like login, logout, send, broadcast
+ * Implements the jWebSocket system core features like login, logout, send, broadcast
  * etc...
  *
  * @author Alexander Schulze
- * @author kybernees {Support for client-side session management and Spring
- * authentication}
+ * @author kybernees
  */
 public class SystemPlugIn extends TokenPlugIn {
 
