5a5b55edf9035649032245ee78bb5815336f75e3
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-04-01 14:11:11 +0000

    Upgrading ReportingPlugIn demo for JDBC reports generation. Jasmine tests upgraded also.

--- jWebSocketPlugIns/jWebSocketReportingPlugIn/src/main/java/org/jwebsocket/plugins/reporting/service/JasperReportService.java
+++ jWebSocketPlugIns/jWebSocketReportingPlugIn/src/main/java/org/jwebsocket/plugins/reporting/service/JasperReportService.java
@@ -1,7 +1,7 @@
 //	---------------------------------------------------------------------------
 //	jWebSocket - Report Service for Reporting Plug-In (Community Edition, CE)
 //	---------------------------------------------------------------------------
-//	Copyright 2010-2014 Innotrade GmbH (jWebSocket.org)
+//	Copyright 2010-2013 Innotrade GmbH (jWebSocket.org)
 //	Alexander Schulze, Germany (NRW)
 //
 //	Licensed under the Apache License, Version 2.0 (the "License");
@@ -39,7 +39,7 @@ import org.springframework.util.Assert;
 
 /**
  *
- * @author Javier Alejandro Puentes Serrano
+ * @author Javier Alejandro Puentes
  */
 public class JasperReportService implements IJasperReportService {
 
@@ -200,6 +200,12 @@ public class JasperReportService implements IJasperReportService {
 		return lFinalPath.replace(aUserHome, "");
 	}
 
+	@Override
+	public String getConnectionAlias(){
+		return
+				mSettings.getJdbcAlias();
+	}
+	
 	@Override
 	public void initialize() throws Exception {
 	}
