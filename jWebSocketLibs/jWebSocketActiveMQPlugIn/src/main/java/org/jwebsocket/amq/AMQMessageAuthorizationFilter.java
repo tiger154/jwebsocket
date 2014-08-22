a57aa3f52c9e608672e53f33f7b1055705cf5beb
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-08-22 14:10:45 +0000

    Adding support to secure target jWebSocket AMQ clusters nodes and default message hub topics. Optionally can be disabled and controlled by AMQ security mechanism. Default mechanism checks that consumer username matches the cluster name.

--- jWebSocketLibs/jWebSocketActiveMQPlugIn/src/main/java/org/jwebsocket/amq/AMQMessageAuthorizationFilter.java
+++ jWebSocketLibs/jWebSocketActiveMQPlugIn/src/main/java/org/jwebsocket/amq/AMQMessageAuthorizationFilter.java
@@ -24,17 +24,28 @@ import java.util.List;
 import org.apache.activemq.broker.ConnectionContext;
 import org.apache.activemq.command.Message;
 import org.apache.activemq.security.MessageAuthorizationPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Message authorization policy implementation to provide security on replies
- * delivery. Only request owners can receive replies.
+ * delivery. The filter ensures that only request owners can receive replies.
  *
  * @author Rolando Santamaria Maso
  */
 public class AMQMessageAuthorizationFilter implements MessageAuthorizationPolicy {
 
 	private final List<String> mTargetDestinations = new LinkedList<String>();
+	private final String NODES_TOPIC_POSTFIX = "_nodes";
+	private final String MESSAGEHUB_TOPIC_POSTFIX = "_messagehub";
+	private boolean mSecureNodesTopic = true;
+	private boolean mSecureMessageHubTopic = true;
+	private static final Logger mLog = LoggerFactory.getLogger(AMQBasicSecurityPlugIn.class);
 
+	public AMQMessageAuthorizationFilter() {
+		mLog.info("Instantiating jWebSocket AMQMessageAuthorizationFilter...");
+	}
+	
 	/**
 	 *
 	 * @return
@@ -52,6 +63,39 @@ public class AMQMessageAuthorizationFilter implements MessageAuthorizationPolicy
 	}
 
 	/**
+	 * 
+	 * @param aSecureMessageHubTopic 
+	 */
+	public void setSecureMessageHubTopic(boolean aSecureMessageHubTopic) {
+		this.mSecureMessageHubTopic = aSecureMessageHubTopic;
+	}
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public boolean isSecureMessageHubTopic() {
+		return mSecureMessageHubTopic;
+	}
+
+	/**
+	 * 
+	 * @param aSecureNodesTopic 
+	 */
+	public void setSecureNodesTopic(boolean aSecureNodesTopic) {
+		this.mSecureNodesTopic = aSecureNodesTopic;
+	}
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public boolean isSecureNodesTopic() {
+		return mSecureNodesTopic;
+	}
+	
+
+	/**
 	 *
 	 * @param aContext
 	 * @param aMessage
@@ -62,10 +106,14 @@ public class AMQMessageAuthorizationFilter implements MessageAuthorizationPolicy
 		try {
 			String lMessageDest = aMessage.getDestination().getQualifiedName();
 			for (String lSecureDest : mTargetDestinations) {
+				// authorizing AMQ cluster nodes
+				if (aContext.isNetworkConnection()) {
+					return true;
+				}
+
+				// restricting message consumption in the cluster main topic
 				if (lSecureDest.matches(lMessageDest)) {
-					if (aContext.isNetworkConnection()) {
-						return true;
-					} else if (aContext.getConnectionId().getValue().equals((String) aMessage.getProperty("connectionId"))) {
+					if (aContext.getConnectionId().getValue().equals((String) aMessage.getProperty("connectionId"))) {
 						aMessage.removeProperty("connectionId");
 						aMessage.removeProperty("replySelector");
 						// allow if the consumer connection id matches the message target connection id
@@ -75,11 +123,23 @@ public class AMQMessageAuthorizationFilter implements MessageAuthorizationPolicy
 						return true;
 					} else if (aMessage.getDestination().getPhysicalName().equals(aContext.getUserName())) {
 						// allow if the username matches the message destination 
-						// (required for server side processing nodes)
+						// guarantees message consumption across server nodes
 						return true;
 					}
 
 					return false;
+				}
+
+				// restricting message consumption in the cluster server nodes topic  
+				if (mSecureNodesTopic && (lSecureDest + NODES_TOPIC_POSTFIX).matches(lMessageDest)) {
+					return aMessage.getDestination().getPhysicalName()
+							.equals(aContext.getUserName() + NODES_TOPIC_POSTFIX);
+				}
+
+				// restricting message consumption in the cluster message hub topic
+				if (mSecureMessageHubTopic && (lSecureDest + MESSAGEHUB_TOPIC_POSTFIX).matches(lMessageDest)) {
+					return aMessage.getDestination().getPhysicalName()
+							.equals(aContext.getUserName() + MESSAGEHUB_TOPIC_POSTFIX);
 				}
 			}
 		} catch (IOException lEx) {
