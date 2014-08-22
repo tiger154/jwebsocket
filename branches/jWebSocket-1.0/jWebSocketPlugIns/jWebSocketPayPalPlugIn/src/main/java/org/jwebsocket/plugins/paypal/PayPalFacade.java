cdbe5c51f65443e029ab834c7f1268086b91cde1
odiaz <odiaz@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-08-22 13:53:17 +0000

    Fixing code

--- branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketPayPalPlugIn/src/main/java/org/jwebsocket/plugins/paypal/PayPalFacade.java
+++ branches/jWebSocket-1.0/jWebSocketPlugIns/jWebSocketPayPalPlugIn/src/main/java/org/jwebsocket/plugins/paypal/PayPalFacade.java
@@ -1,19 +1,21 @@
-/*
- * Copyright 2014 zcool.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+//	---------------------------------------------------------------------------
+//	jWebSocket - jWebSocket JDBC Plug-In (Community Edition, CE)
+//	---------------------------------------------------------------------------
+//	Copyright 2010-2014 Innotrade GmbH (jWebSocket.org)
+//      Alexander Schulze, Germany (NRW)
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+//	---------------------------------------------------------------------------
 package org.jwebsocket.plugins.paypal;
 
 import com.paypal.api.payments.Amount;
@@ -27,7 +29,6 @@ import com.paypal.api.payments.Transaction;
 import com.paypal.core.rest.OAuthTokenCredential;
 import com.paypal.core.rest.PayPalRESTException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -37,24 +38,25 @@ import org.jwebsocket.token.Token;
 
 /**
  *
- * @author zcool
+ * @author Omar Antonio Díaz Peña
  */
-public class PaypalFacade {
+public class PayPalFacade {
+
     private String clientID;
     private String clientSecret;
 
-    public PaypalFacade(String aClientID, String aClientSecret) {
+    public PayPalFacade(String aClientID, String aClientSecret) {
         this.clientID = aClientID;
         this.clientSecret = aClientSecret;
     }
-    
-    public String getAccessToken() throws Exception{
-        
+
+    public String getAccessToken() throws Exception {
+
         String accessToken = null;
         try {
             accessToken = new OAuthTokenCredential(this.clientID, this.clientSecret).getAccessToken();
         } catch (PayPalRESTException ex) {
-            Logger.getLogger(PaypalFacade.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(PayPalFacade.class.getName()).log(Level.SEVERE, null, ex);
         }
         return accessToken;
     }
@@ -62,10 +64,10 @@ public class PaypalFacade {
     public Payment createPayment(Token aToken) throws Exception {
         Payment lPayment = new Payment();
         lPayment.setIntent(aToken.getString("intent"));
-        
+
         Payer lPayer = new Payer();
-        Map lPayerData = (Map)aToken.getMap("payer").get("payer_info");
-        
+        Map lPayerData = (Map) aToken.getMap("payer").get("payer_info");
+
         PayerInfo lPayerInfo = new PayerInfo();
         lPayer.setPayerInfo(lPayerInfo);
         lPayer.setPaymentMethod("paypal");
@@ -73,58 +75,55 @@ public class PaypalFacade {
         lPayerInfo.setLastName((String) lPayerData.get("last_name"));
         lPayerInfo.setPayerId((String) lPayerData.get("payer_id"));
         lPayer.setPayerInfo(lPayerInfo);
-        Map lRedirectData = (Map)aToken.getMap("redirect_urls");
-        
+        Map lRedirectData = (Map) aToken.getMap("redirect_urls");
+
         RedirectUrls lRedirectUrls = new RedirectUrls();
-        lRedirectUrls.setReturnUrl((String)lRedirectData.get("return_url"));
-        lRedirectUrls.setCancelUrl((String)lRedirectData.get("cancel_url"));
+        lRedirectUrls.setReturnUrl((String) lRedirectData.get("return_url"));
+        lRedirectUrls.setCancelUrl((String) lRedirectData.get("cancel_url"));
         lPayment.setRedirectUrls(lRedirectUrls);
-        
+
         lPayment.setPayer(lPayer);
         List lTransactionsData = aToken.getList("transactions");
 
         LinkedList<Transaction> lTransactions = new LinkedList<Transaction>();
-        
+
         for (Object object : lTransactionsData) {
-            Map lTransactionData = (Map)object;
-            Map lTmpListData = (Map)lTransactionData.get("item_list");
-            List lItemListData = (List)lTmpListData.get("items");
-            Map lAmountData = (Map)lTransactionData.get("amount");
+            Map lTransactionData = (Map) object;
+            Map lTmpListData = (Map) lTransactionData.get("item_list");
+            List lItemListData = (List) lTmpListData.get("items");
+            Map lAmountData = (Map) lTransactionData.get("amount");
             Transaction lTransaction = new Transaction();
-            
+
             ItemList lItemList = new ItemList();
             List<Item> lTmpItemList = new ArrayList<Item>();
-            
+
             for (Object lItemData : lItemListData) {
-                Map lTmpItemData = (Map)lItemData;
+                Map lTmpItemData = (Map) lItemData;
                 Item lItem = new Item();
-                lItem.setCurrency((String)lTmpItemData.get("currency"));
-                lItem.setPrice((String)lTmpItemData.get("price"));
-                lItem.setName((String)lTmpItemData.get("name"));
+                lItem.setCurrency((String) lTmpItemData.get("currency"));
+                lItem.setPrice((String) lTmpItemData.get("price"));
+                lItem.setName((String) lTmpItemData.get("name"));
                 lItem.setQuantity(lTmpItemData.get("quantity").toString());
-                lItem.setSku((String)lTmpItemData.get("sku"));
-                
+                lItem.setSku((String) lTmpItemData.get("sku"));
+
                 lTmpItemList.add(lItem);
             }
-            
+
             lItemList.setItems(lTmpItemList);
             lTransaction.setItemList(lItemList);
-            
+
             Amount lAmount = new Amount();
-            lAmount.setCurrency((String)lAmountData.get("currency"));
-            lAmount.setTotal((String)lAmountData.get("total"));
+            lAmount.setCurrency((String) lAmountData.get("currency"));
+            lAmount.setTotal((String) lAmountData.get("total"));
             lTransaction.setAmount(lAmount);
             lTransaction.setDescription(
-                    (String)lTransactionData.get("description")
+                    (String) lTransactionData.get("description")
             );
             lTransactions.add(lTransaction);
         }
-        
+
         lPayment.setTransactions(lTransactions);
-        
+
         return lPayment.create(this.getAccessToken());
     }
 }
-
-
-
