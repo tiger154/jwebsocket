9c88462f719263b5c8741f68192118279ef81639
aschulze <aschulze@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-11-21 10:55:14 +0000

    Updated Filesystem Plug-in to not scan folders always in intervals of 1 second, now turned off per default, introduced new config settings: monitoringActive and monitoringInterval.

--- jWebSocketPlugIns/jWebSocketFileSystemPlugIn/src/main/java/org/jwebsocket/plugins/filesystem/Settings.java
+++ jWebSocketPlugIns/jWebSocketFileSystemPlugIn/src/main/java/org/jwebsocket/plugins/filesystem/Settings.java
@@ -31,69 +31,99 @@ import org.jwebsocket.config.JWebSocketConfig;
  */
 public class Settings {
 
-    private Map<String, String> mAliases = new FastMap<String, String>();
+	private Map<String, String> mAliases = new FastMap<String, String>();
+	private boolean mMonitoringActive = false;
+	private long mMonitoringInterval = 5000;
 
-    /**
-     * Gets the FSP aliases information (Map). Includes alias name and alias
-     * directory path.
-     *
-     * @return the aliases
-     */
-    public Map<String, String> getAliases() {
-        return mAliases;
-    }
+	/**
+	 * Gets the FSP aliases information (Map). Includes alias name and alias
+	 * directory path.
+	 *
+	 * @return the aliases
+	 */
+	public Map<String, String> getAliases() {
+		return mAliases;
+	}
 
-    /**
-     * Sets the FSP aliases information.
-     *
-     * @param aAliases the aliases to set
-     */
-    public void setAliases(Map<String, String> aAliases) {
-        mAliases = aAliases;
-    }
+	/**
+	 * Sets the FSP aliases information.
+	 *
+	 * @param aAliases the aliases to set
+	 */
+	public void setAliases(Map<String, String> aAliases) {
+		mAliases = aAliases;
+	}
 
-    /**
-     * Gets the directory path of a given alias name.
-     *
-     * @param aAliasName
-     * @return
-     */
-    public String getAliasPath(String aAliasName) {
-        return JWebSocketConfig.expandEnvVarsAndProps(mAliases.get(aAliasName));
-    }
+	/**
+	 * Gets the directory path of a given alias name.
+	 *
+	 * @param aAliasName
+	 * @return
+	 */
+	public String getAliasPath(String aAliasName) {
+		return JWebSocketConfig.expandEnvVarsAndProps(mAliases.get(aAliasName));
+	}
 
-    /**
-     * Gets the alias name of a given directory path.
-     *
-     * @param aPath
-     * @return
-     */
-    public String getAliasName(String aPath) {
-        for (Map.Entry<String, String> lEntry : mAliases.entrySet()) {
-            String lAlias = lEntry.getValue().replace("/", File.separator);
-            String lExpandedAlias = JWebSocketConfig.expandEnvVarsAndProps(lAlias).replace("/", File.separator);
-            if (aPath.startsWith(lAlias)
-                    || aPath.startsWith(lExpandedAlias)) {
-                return lEntry.getKey();
-            }
-        }
+	/**
+	 * Gets the alias name of a given directory path.
+	 *
+	 * @param aPath
+	 * @return
+	 */
+	public String getAliasName(String aPath) {
+		for (Map.Entry<String, String> lEntry : mAliases.entrySet()) {
+			String lAlias = lEntry.getValue().replace("/", File.separator);
+			String lExpandedAlias = JWebSocketConfig.expandEnvVarsAndProps(lAlias).replace("/", File.separator);
+			if (aPath.startsWith(lAlias)
+					|| aPath.startsWith(lExpandedAlias)) {
+				return lEntry.getKey();
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    /**
-     * Gets the directory path of a given alias allowing to pass a default value
-     * if data is not present.
-     *
-     * @param aAliasName
-     * @param aDefaultValue
-     * @return
-     */
-    public String getAliasPath(String aAliasName, String aDefaultValue) {
-        String lValue = mAliases.get(aAliasName);
-        if (null == lValue) {
-            return aDefaultValue;
-        }
-        return lValue;
-    }
+	/**
+	 * Gets the directory path of a given alias allowing to pass a default value
+	 * if data is not present.
+	 *
+	 * @param aAliasName
+	 * @param aDefaultValue
+	 * @return
+	 */
+	public String getAliasPath(String aAliasName, String aDefaultValue) {
+		String lValue = mAliases.get(aAliasName);
+		if (null == lValue) {
+			return aDefaultValue;
+		}
+		return lValue;
+	}
+
+	/**
+	 * @return the MonitoringActive
+	 */
+	public boolean isMonitoringActive() {
+		return mMonitoringActive;
+	}
+
+	/**
+	 * @param aMonitoringActive the MonitoringActive to set
+	 */
+	public void setMonitoringActive(boolean aMonitoringActive) {
+		mMonitoringActive = aMonitoringActive;
+	}
+
+	/**
+	 * @return the MonitoringInterval
+	 */
+	public long getMonitoringInterval() {
+		return mMonitoringInterval;
+	}
+
+	/**
+	 * @param aMonitoringInterval
+	 */
+	public void setMonitoringInterval(long aMonitoringInterval) {
+		mMonitoringInterval = aMonitoringInterval;
+	}
 }
