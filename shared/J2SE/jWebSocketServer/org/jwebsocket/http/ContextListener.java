65a6192aae454ee5fd47e7c09662e85498609344
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-12-06 13:13:09 +0000

    Introducing HTTP/REST support base components.

--- shared/J2SE/jWebSocketServer/org/jwebsocket/http/ContextListener.java
+++ shared/J2SE/jWebSocketServer/org/jwebsocket/http/ContextListener.java
@@ -16,7 +16,7 @@
 //	See the License for the specific language governing permissions and
 //	limitations under the License.
 //	---------------------------------------------------------------------------
-package org.jwebsocket.tomcat;
+package org.jwebsocket.http;
 
 import java.net.URLClassLoader;
 import javax.servlet.ServletContext;
