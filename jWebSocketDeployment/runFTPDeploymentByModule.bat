99b7dae058fba391e31c886147813b6aefce5c23
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-12-11 17:29:11 +0000

    The deployment script was having errors and wasn't able to finish the compilation and the deployment, this is now fixed, I am also removing the parent configuration tag from the generated POM projects, so when the user downloads a certain library, eg. jWebSocketJMSEndPoint he doesn't need the parent of it jWebSocketJMSGateway, so that makes the libraries not dependent from their parents. Thanks, Victor

--- jWebSocketDeployment/runFTPDeploymentByModule.bat
+++ jWebSocketDeployment/runFTPDeploymentByModule.bat
@@ -75,7 +75,7 @@ set MODULES[7]=jWebSocketClientAPI
 set ARTIFACT_ID[7]=jWebSocketClientAPI
 set JWS_DEPLOY_VER[7]=%DEPLOYMENT_VERSION%
 
-set MODULES[8]=jWebSocketJMSGateway/jWebSocketJMSEndPoint
+set MODULES[8]=jWebSocketJMSGateway\jWebSocketJMSEndPoint
 set ARTIFACT_ID[8]=jWebSocketJMSEndPoint
 set JWS_DEPLOY_VER[8]=%DEPLOYMENT_VERSION%
 
@@ -87,31 +87,31 @@ set MODULES[10]=jWebSocketJavaSEClient
 set ARTIFACT_ID[10]=jWebSocketJavaSEClient
 set JWS_DEPLOY_VER[10]=%DEPLOYMENT_VERSION%
 
-set MODULES[11]=jWebSocketJMSGateway/jWebSocketJMSClient
+set MODULES[11]=jWebSocketJMSGateway\jWebSocketJMSClient
 set ARTIFACT_ID[11]=jWebSocketJMSClient
 set JWS_DEPLOY_VER[11]=%DEPLOYMENT_VERSION%
 
-set MODULES[12]=jWebSocketJMSGateway/jWebSocketJMSServer
+set MODULES[12]=jWebSocketJMSGateway\jWebSocketJMSServer
 set ARTIFACT_ID[12]=jWebSocketJMSServer
 set JWS_DEPLOY_VER[12]=%DEPLOYMENT_VERSION%
 
-set MODULES[13]=jWebSocketPlugIns/jWebSocketJMSPlugIn
+set MODULES[13]=jWebSocketPlugIns\jWebSocketJMSPlugIn
 set ARTIFACT_ID[13]=jWebSocketJMSPlugIn
 set JWS_DEPLOY_VER[13]=%DEPLOYMENT_VERSION%
 
-set MODULES[14]=jWebSocketJMSGateway/jWebSocketJMSDemoPlugIn
+set MODULES[14]=jWebSocketJMSGateway\jWebSocketJMSDemoPlugIn
 set ARTIFACT_ID[14]=jWebSocketJMSDemoPlugIn
 set JWS_DEPLOY_VER[14]=%DEPLOYMENT_VERSION%
 
-set MODULES[15]=jWebSocketEngines/jWebSocketTomcatEngine
+set MODULES[15]=jWebSocketEngines\jWebSocketTomcatEngine
 set ARTIFACT_ID[15]=jWebSocketTomcatEngine
 set JWS_DEPLOY_VER[15]=%DEPLOYMENT_VERSION%
 
-set MODULES[16]=jWebSocketEngines/jWebSocketJettyEngine
+set MODULES[16]=jWebSocketEngines\jWebSocketJettyEngine
 set ARTIFACT_ID[16]=jWebSocketJettyEngine
 set JWS_DEPLOY_VER[16]=%DEPLOYMENT_VERSION%
 
-set MODULES[17]=jWebSocketEngines/jWebSocketGrizzlyEngine
+set MODULES[17]=jWebSocketEngines\jWebSocketGrizzlyEngine
 set ARTIFACT_ID[17]=jWebSocketGrizzlyEngine
 set JWS_DEPLOY_VER[17]=%DEPLOYMENT_VERSION%
 
@@ -131,209 +131,209 @@ set MODULES[20]=jWebSocketSamples
 set ARTIFACT_ID[20]=jWebSocketSamples
 set JWS_DEPLOY_VER[20]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketChatPlugIn
-set MODULES[21]=jWebSocketChatPlugIn
-set ARTIFACT_ID[21]=jWebSocketPlugIns/jWebSocketChatPlugIn
+rem jWebSocketPlugIns\jWebSocketChatPlugIn
+set MODULES[21]=jWebSocketPlugIns\jWebSocketChatPlugIn
+set ARTIFACT_ID[21]=jWebSocketChatPlugIn
 set JWS_DEPLOY_VER[21]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketSenchaPlugIn
-set MODULES[22]=jWebSocketSenchaPlugIn
-set ARTIFACT_ID[22]=jWebSocketPlugIns/jWebSocketSenchaPlugIn
+rem jWebSocketPlugIns\jWebSocketSenchaPlugIn
+set MODULES[22]=jWebSocketPlugIns\jWebSocketSenchaPlugIn
+set ARTIFACT_ID[22]=jWebSocketSenchaPlugIn
 set JWS_DEPLOY_VER[22]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketJQueryPlugIn
-set MODULES[23]=jWebSocketJQueryPlugIn
-set ARTIFACT_ID[23]=jWebSocketPlugIns/jWebSocketJQueryPlugIn
+rem jWebSocketPlugIns\jWebSocketJQueryPlugIn
+set MODULES[23]=jWebSocketPlugIns\jWebSocketJQueryPlugIn
+set ARTIFACT_ID[23]=jWebSocketJQueryPlugIn
 set JWS_DEPLOY_VER[23]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketJCaptchaPlugIn
-set MODULES[24]=jWebSocketJCaptchaPlugIn
-set ARTIFACT_ID[24]=jWebSocketPlugIns/jWebSocketJCaptchaPlugIn
+rem jWebSocketPlugIns\jWebSocketJCaptchaPlugIn
+set MODULES[24]=jWebSocketPlugIns\jWebSocketJCaptchaPlugIn
+set ARTIFACT_ID[24]=jWebSocketJCaptchaPlugIn
 set JWS_DEPLOY_VER[24]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketJDBCPlugIn
-set MODULES[25]=jWebSocketJDBCPlugIn
-set ARTIFACT_ID[25]=jWebSocketPlugIns/jWebSocketJDBCPlugIn
+rem jWebSocketPlugIns\jWebSocketJDBCPlugIn
+set MODULES[25]=jWebSocketPlugIns\jWebSocketJDBCPlugIn
+set ARTIFACT_ID[25]=jWebSocketJDBCPlugIn
 set JWS_DEPLOY_VER[25]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketRPCPlugIn
-set MODULES[26]=jWebSocketRPCPlugIn
-set ARTIFACT_ID[26]=jWebSocketPlugIns/jWebSocketRPCPlugIn
+rem jWebSocketPlugIns\jWebSocketRPCPlugIn
+set MODULES[26]=jWebSocketPlugIns\jWebSocketRPCPlugIn
+set ARTIFACT_ID[26]=jWebSocketRPCPlugIn
 set JWS_DEPLOY_VER[26]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketRTCPlugIn
-set MODULES[27]=jWebSocketRTCPlugIn
-set ARTIFACT_ID[27]=jWebSocketPlugIns/jWebSocketRTCPlugIn
+rem jWebSocketPlugIns\jWebSocketRTCPlugIn
+set MODULES[27]=jWebSocketPlugIns\jWebSocketRTCPlugIn
+set ARTIFACT_ID[27]=jWebSocketRTCPlugIn
 set JWS_DEPLOY_VER[27]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketJMXPlugIn
-set MODULES[28]=jWebSocketJMXPlugIn
-set ARTIFACT_ID[28]=jWebSocketPlugIns/jWebSocketJMXPlugIn
+rem jWebSocketPlugIns\jWebSocketJMXPlugIn
+set MODULES[28]=jWebSocketPlugIns\jWebSocketJMXPlugIn
+set ARTIFACT_ID[28]=jWebSocketJMXPlugIn
 set JWS_DEPLOY_VER[28]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketSMSPlugIn
-set MODULES[29]=jWebSocketSMSPlugIn
-set ARTIFACT_ID[29]=jWebSocketPlugIns/jWebSocketSMSPlugIn
+rem jWebSocketPlugIns\jWebSocketSMSPlugIn
+set MODULES[29]=jWebSocketPlugIns\jWebSocketSMSPlugIn
+set ARTIFACT_ID[29]=jWebSocketSMSPlugIn
 set JWS_DEPLOY_VER[29]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketMailPlugIn
-set MODULES[30]=jWebSocketMailPlugIn
-set ARTIFACT_ID[30]=jWebSocketPlugIns/jWebSocketMailPlugIn
+rem jWebSocketPlugIns\jWebSocketMailPlugIn
+set MODULES[30]=jWebSocketPlugIns\jWebSocketMailPlugIn
+set ARTIFACT_ID[30]=jWebSocketMailPlugIn
 set JWS_DEPLOY_VER[30]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketReportingPlugIn
-set MODULES[31]=jWebSocketReportingPlugIn
-set ARTIFACT_ID[31]=jWebSocketPlugIns/jWebSocketReportingPlugIn
+rem jWebSocketPlugIns\jWebSocketReportingPlugIn
+set MODULES[31]=jWebSocketPlugIns\jWebSocketReportingPlugIn
+set ARTIFACT_ID[31]=jWebSocketReportingPlugIn
 set JWS_DEPLOY_VER[31]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketFileSystemPlugIn
-set MODULES[32]=jWebSocketFileSystemPlugIn
-set ARTIFACT_ID[32]=jWebSocketPlugIns/jWebSocketFileSystemPlugIn
+rem jWebSocketPlugIns\jWebSocketFileSystemPlugIn
+set MODULES[32]=jWebSocketPlugIns\jWebSocketFileSystemPlugIn
+set ARTIFACT_ID[32]=jWebSocketFileSystemPlugIn
 set JWS_DEPLOY_VER[32]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketExtProcessPlugIn
-set MODULES[33]=jWebSocketExtProcessPlugIn
-set ARTIFACT_ID[33]=jWebSocketPlugIns/jWebSocketExtProcessPlugIn
+rem jWebSocketPlugIns\jWebSocketExtProcessPlugIn
+set MODULES[33]=jWebSocketPlugIns\jWebSocketExtProcessPlugIn
+set ARTIFACT_ID[33]=jWebSocketExtProcessPlugIn
 set JWS_DEPLOY_VER[33]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketChannelPlugIn
-set MODULES[34]=jWebSocketChannelPlugIn
-set ARTIFACT_ID[34]=jWebSocketPlugIns/jWebSocketChannelPlugIn
+rem jWebSocketPlugIns\jWebSocketChannelPlugIn
+set MODULES[34]=jWebSocketPlugIns\jWebSocketChannelPlugIn
+set ARTIFACT_ID[34]=jWebSocketChannelPlugIn
 set JWS_DEPLOY_VER[34]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketEventsPlugIn
-set MODULES[35]=jWebSocketEventsPlugIn
-set ARTIFACT_ID[35]=jWebSocketPlugIns/jWebSocketEventsPlugIn
+rem jWebSocketPlugIns\jWebSocketEventsPlugIn
+set MODULES[35]=jWebSocketPlugIns\jWebSocketEventsPlugIn
+set ARTIFACT_ID[35]=jWebSocketEventsPlugIn
 set JWS_DEPLOY_VER[35]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketLoggingPlugIn
-set MODULES[36]=jWebSocketLoggingPlugIn
-set ARTIFACT_ID[36]=jWebSocketPlugIns/jWebSocketLoggingPlugIn
+rem jWebSocketPlugIns\jWebSocketLoggingPlugIn
+set MODULES[36]=jWebSocketPlugIns\jWebSocketLoggingPlugIn
+set ARTIFACT_ID[36]=jWebSocketLoggingPlugIn
 set JWS_DEPLOY_VER[36]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketStreamingPlugIn
-set MODULES[37]=jWebSocketStreamingPlugIn
-set ARTIFACT_ID[37]=jWebSocketPlugIns/jWebSocketStreamingPlugIn
+rem jWebSocketPlugIns\jWebSocketStreamingPlugIn
+set MODULES[37]=jWebSocketPlugIns\jWebSocketStreamingPlugIn
+set ARTIFACT_ID[37]=jWebSocketStreamingPlugIn
 set JWS_DEPLOY_VER[37]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketTwitterPlugIn
-set MODULES[38]=jWebSocketTwitterPlugIn
-set ARTIFACT_ID[38]=jWebSocketPlugIns/jWebSocketTwitterPlugIn
+rem jWebSocketPlugIns\jWebSocketTwitterPlugIn
+set MODULES[38]=jWebSocketPlugIns\jWebSocketTwitterPlugIn
+set ARTIFACT_ID[38]=jWebSocketTwitterPlugIn
 set JWS_DEPLOY_VER[38]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketXMPPPlugIn
-set MODULES[39]=jWebSocketXMPPPlugIn
-set ARTIFACT_ID[39]=jWebSocketPlugIns/jWebSocketXMPPPlugIn
+rem jWebSocketPlugIns\jWebSocketXMPPPlugIn
+set MODULES[39]=jWebSocketPlugIns\jWebSocketXMPPPlugIn
+set ARTIFACT_ID[39]=jWebSocketXMPPPlugIn
 set JWS_DEPLOY_VER[39]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketStatisticsPlugIn
-set MODULES[40]=jWebSocketStatisticsPlugIn
-set ARTIFACT_ID[40]=jWebSocketPlugIns/jWebSocketStatisticsPlugIn
+rem jWebSocketPlugIns\jWebSocketStatisticsPlugIn
+set MODULES[40]=jWebSocketPlugIns\jWebSocketStatisticsPlugIn
+set ARTIFACT_ID[40]=jWebSocketStatisticsPlugIn
 set JWS_DEPLOY_VER[40]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketStockTickerPlugIn
-set MODULES[41]=jWebSocketStockTickerPlugIn
-set ARTIFACT_ID[41]=jWebSocketPlugIns/jWebSocketStockTickerPlugIn
+rem jWebSocketPlugIns\jWebSocketStockTickerPlugIn
+set MODULES[41]=jWebSocketPlugIns\jWebSocketStockTickerPlugIn
+set ARTIFACT_ID[41]=jWebSocketStockTickerPlugIn
 set JWS_DEPLOY_VER[41]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketSharedObjectsPlugIn
-set MODULES[42]=jWebSocketSharedObjectsPlugIn
-set ARTIFACT_ID[42]=jWebSocketPlugIns/jWebSocketSharedObjectsPlugIn
+rem jWebSocketPlugIns\jWebSocketSharedObjectsPlugIn
+set MODULES[42]=jWebSocketPlugIns\jWebSocketSharedObjectsPlugIn
+set ARTIFACT_ID[42]=jWebSocketSharedObjectsPlugIn
 set JWS_DEPLOY_VER[42]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketSharedCanvasPlugIn
-set MODULES[43]=jWebSocketSharedCanvasPlugIn
-set ARTIFACT_ID[43]=jWebSocketPlugIns/jWebSocketSharedCanvasPlugIn
+rem jWebSocketPlugIns\jWebSocketSharedCanvasPlugIn
+set MODULES[43]=jWebSocketPlugIns\jWebSocketSharedCanvasPlugIn
+set ARTIFACT_ID[43]=jWebSocketSharedCanvasPlugIn
 set JWS_DEPLOY_VER[43]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketBenchmarkPlugIn
-set MODULES[44]=jWebSocketBenchmarkPlugIn
-set ARTIFACT_ID[44]=jWebSocketPlugIns/jWebSocketBenchmarkPlugIn
+rem jWebSocketPlugIns\jWebSocketBenchmarkPlugIn
+set MODULES[44]=jWebSocketPlugIns\jWebSocketBenchmarkPlugIn
+set ARTIFACT_ID[44]=jWebSocketBenchmarkPlugIn
 set JWS_DEPLOY_VER[44]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketAPIPlugIn
-set MODULES[45]=jWebSocketAPIPlugIn
-set ARTIFACT_ID[45]=jWebSocketPlugIns/jWebSocketAPIPlugIn
+rem jWebSocketPlugIns\jWebSocketAPIPlugIn
+set MODULES[45]=jWebSocketPlugIns\jWebSocketAPIPlugIn
+set ARTIFACT_ID[45]=jWebSocketAPIPlugIn
 set JWS_DEPLOY_VER[45]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketProxyPlugIn
-set MODULES[46]=jWebSocketProxyPlugIn
-set ARTIFACT_ID[46]=jWebSocketPlugIns/jWebSocketProxyPlugIn
+rem jWebSocketPlugIns\jWebSocketProxyPlugIn
+set MODULES[46]=jWebSocketPlugIns\jWebSocketProxyPlugIn
+set ARTIFACT_ID[46]=jWebSocketProxyPlugIn
 set JWS_DEPLOY_VER[46]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketAdminPlugIn
-set MODULES[47]=jWebSocketAdminPlugIn
-set ARTIFACT_ID[47]=jWebSocketPlugIns/jWebSocketAdminPlugIn
+rem jWebSocketPlugIns\jWebSocketAdminPlugIn
+set MODULES[47]=jWebSocketPlugIns\jWebSocketAdminPlugIn
+set ARTIFACT_ID[47]=jWebSocketAdminPlugIn
 set JWS_DEPLOY_VER[47]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketArduinoPlugIn
-set MODULES[48]=jWebSocketArduinoPlugIn
-set ARTIFACT_ID[48]=jWebSocketPlugIns/jWebSocketArduinoPlugIn
+rem jWebSocketPlugIns\jWebSocketArduinoPlugIn
+set MODULES[48]=jWebSocketPlugIns\jWebSocketArduinoPlugIn
+set ARTIFACT_ID[48]=jWebSocketArduinoPlugIn
 set JWS_DEPLOY_VER[48]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketMonitoringPlugIn
-set MODULES[49]=jWebSocketMonitoringPlugIn
-set ARTIFACT_ID[49]=jWebSocketPlugIns/jWebSocketMonitoringPlugIn
+rem jWebSocketPlugIns\jWebSocketMonitoringPlugIn
+set MODULES[49]=jWebSocketPlugIns\jWebSocketMonitoringPlugIn
+set ARTIFACT_ID[49]=jWebSocketMonitoringPlugIn
 set JWS_DEPLOY_VER[49]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketTestPlugIn
-set MODULES[50]=jWebSocketTestPlugIn
-set ARTIFACT_ID[50]=jWebSocketPlugIns/jWebSocketTestPlugIn
+rem jWebSocketPlugIns\jWebSocketTestPlugIn
+set MODULES[50]=jWebSocketPlugIns\jWebSocketTestPlugIn
+set ARTIFACT_ID[50]=jWebSocketTestPlugIn
 set JWS_DEPLOY_VER[50]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketCloudPlugIn
-set MODULES[51]=jWebSocketCloudPlugIn
-set ARTIFACT_ID[51]=jWebSocketPlugIns/jWebSocketCloudPlugIn
+rem jWebSocketPlugIns\jWebSocketCloudPlugIn
+set MODULES[51]=jWebSocketPlugIns\jWebSocketCloudPlugIn
+set ARTIFACT_ID[51]=jWebSocketCloudPlugIn
 set JWS_DEPLOY_VER[51]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketFTPPlugIn
-set MODULES[52]=jWebSocketFTPPlugIn
-set ARTIFACT_ID[52]=jWebSocketPlugIns/jWebSocketFTPPlugIn
+rem jWebSocketPlugIns\jWebSocketFTPPlugIn
+set MODULES[52]=jWebSocketPlugIns\jWebSocketFTPPlugIn
+set ARTIFACT_ID[52]=jWebSocketFTPPlugIn
 set JWS_DEPLOY_VER[52]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketLoadBalancerPlugIn
-set MODULES[53]=jWebSocketLoadBalancerPlugIn
-set ARTIFACT_ID[53]=jWebSocketPlugIns/jWebSocketLoadBalancerPlugIn
+rem jWebSocketPlugIns\jWebSocketLoadBalancerPlugIn
+set MODULES[53]=jWebSocketPlugIns\jWebSocketLoadBalancerPlugIn
+set ARTIFACT_ID[53]=jWebSocketLoadBalancerPlugIn
 set JWS_DEPLOY_VER[53]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketValidatorPlugIn
-set MODULES[54]=jWebSocketValidatorPlugIn
-set ARTIFACT_ID[54]=jWebSocketPlugIns/jWebSocketValidatorPlugIn
+rem jWebSocketPlugIns\jWebSocketValidatorPlugIn
+set MODULES[54]=jWebSocketPlugIns\jWebSocketValidatorPlugIn
+set ARTIFACT_ID[54]=jWebSocketValidatorPlugIn
 set JWS_DEPLOY_VER[54]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/JWebSocketPingPongGame
-set MODULES[55]=JWebSocketPingPongGame
-set ARTIFACT_ID[55]=jWebSocketPlugIns/JWebSocketPingPongGame
+rem jWebSocketPlugIns\JWebSocketPingPongGame
+set MODULES[55]=jWebSocketPlugIns\JWebSocketPingPongGame
+set ARTIFACT_ID[55]=JWebSocketPingPongGame
 set JWS_DEPLOY_VER[55]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketItemStorage
-set MODULES[56]=jWebSocketItemStorage
-set ARTIFACT_ID[56]=jWebSocketPlugIns/jWebSocketItemStorage
+rem jWebSocketPlugIns\jWebSocketItemStorage
+set MODULES[56]=jWebSocketPlugIns\jWebSocketItemStorage
+set ARTIFACT_ID[56]=jWebSocketItemStorage
 set JWS_DEPLOY_VER[56]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketScriptingPlugIn
-set MODULES[57]=jWebSocketScriptingPlugIn
-set ARTIFACT_ID[57]=jWebSocketPlugIns/jWebSocketScriptingPlugIn
+rem jWebSocketPlugIns\jWebSocketScriptingPlugIn
+set MODULES[57]=jWebSocketPlugIns\jWebSocketScriptingPlugIn
+set ARTIFACT_ID[57]=jWebSocketScriptingPlugIn
 set JWS_DEPLOY_VER[57]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketQuotaPlugIn
-set MODULES[58]=jWebSocketQuotaPlugIn
-set ARTIFACT_ID[58]=jWebSocketPlugIns/jWebSocketQuotaPlugIn
+rem jWebSocketPlugIns\jWebSocketQuotaPlugIn
+set MODULES[58]=jWebSocketPlugIns\jWebSocketQuotaPlugIn
+set ARTIFACT_ID[58]=jWebSocketQuotaPlugIn
 set JWS_DEPLOY_VER[58]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketPayPalPlugIn
-set MODULES[59]=jWebSocketPayPalPlugIn
-set ARTIFACT_ID[59]=jWebSocketPlugIns/jWebSocketPayPalPlugIn
+rem jWebSocketPlugIns\jWebSocketPayPalPlugIn
+set MODULES[59]=jWebSocketPlugIns\jWebSocketPayPalPlugIn
+set ARTIFACT_ID[59]=jWebSocketPayPalPlugIn
 set JWS_DEPLOY_VER[59]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketJCRPlugIn
-set MODULES[60]=jWebSocketJCRPlugIn
-set ARTIFACT_ID[600]=jWebSocketPlugIns/jWebSocketJCRPlugIn
+rem jWebSocketPlugIns\jWebSocketJCRPlugIn
+set MODULES[60]=jWebSocketPlugIns\jWebSocketJCRPlugIn
+set ARTIFACT_ID[600]=jWebSocketJCRPlugIn
 set JWS_DEPLOY_VER[60]=%DEPLOYMENT_VERSION%
 
-rem jWebSocketPlugIns/jWebSocketTTSPlugIn
-set MODULES[61]=jWebSocketTTSPlugIn
-set ARTIFACT_ID[61]=jWebSocketPlugIns/jWebSocketTTSPlugIn
+rem jWebSocketPlugIns\jWebSocketTTSPlugIn
+set MODULES[61]=jWebSocketPlugIns\jWebSocketTTSPlugIn
+set ARTIFACT_ID[61]=jWebSocketTTSPlugIn
 set JWS_DEPLOY_VER[61]=%DEPLOYMENT_VERSION%
 
 set LENGTH=61
