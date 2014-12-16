3039743389e165ce9b1eeb428f16ec5aa798de6d
vbarzana <vbarzana@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2014-12-16 15:28:05 +0000

    WIP BPMN Controller Now we can access to the file uploader component and take the files from it, the next step will be to read the content of these files to be sent to jWebSocket in base64. That will require a change in the uploader component to adapt it to the current solution, also include it in our project. Thanks, Victor

--- jWebSocketClient/web/demos/SenchaArchitectProjects/BPMNUI/app/controller/BPMNController.js
+++ jWebSocketClient/web/demos/SenchaArchitectProjects/BPMNUI/app/controller/BPMNController.js
@@ -14,29 +14,44 @@
  */
 
 Ext.define('BPMNEditor.controller.BPMNController', {
-    extend: 'Ext.app.Controller',
-    alias: 'controller.bpmnController',
-
-    refs: {
-        canvasTabPanel: 'tpcanvas',
-        btnNew: 'pCanvas btnNew'
-    },
-
-    control: {
-        "btnnew": {
-            click: 'onMybuttonClick'
-        },
-        "btnopen": {
-            click: 'onButtonClick'
-        }
-    },
-
-    onMybuttonClick: function(button, e, eOpts) {
-        this.getCanvasTabPanel().add(Ext.create('BPMNEditor.view.pCanvas'));
-    },
-
-    onButtonClick: function(button, e, eOpts) {
-        Ext.create('BPMNEditor.view.winLoadDiagram').show();
-    }
-
+	extend: 'Ext.app.Controller',
+	alias: 'controller.bpmnController',
+	refs: {
+		canvasTabPanel: 'tpcanvas',
+		btnNew: 'pCanvas btnNew',
+		uploadField: 'winloaddiagram filefield'
+	},
+	control: {
+		"btnnew": {
+			click: 'onMybuttonClick'
+		},
+		"btnopen": {
+			click: 'onButtonClick'
+		},
+		"button[action=uploadDiagram]": {
+			click: 'startUpload'
+		}
+	},
+	onMybuttonClick: function(button, e, eOpts) {
+		this.getCanvasTabPanel().add(Ext.create('BPMNEditor.view.pCanvas'));
+	},
+	onButtonClick: function(button, e, eOpts) {
+		Ext.create('BPMNEditor.view.winLoadDiagram').show();
+	},
+	
+	startUpload: function(aButton, aEvent, aOptions) {
+		var lFileInputEl = this.getUploadField().fileInputEl, lIdx,
+				lFiles = lFileInputEl && lFileInputEl.dom ? lFileInputEl.dom.files : [];
+		if (lFiles.length > 0) {
+			aButton.up('window').close();
+			// Iterate through the file list
+			for (lIdx = 0; lIdx < lFiles.length; lIdx++) {
+				console.log(lFiles[lIdx]);
+				// Now we need to bring up the uploader to upload file by file using the FileAPI from HTML5
+			}
+			Ext.Msg.alert('Upload in progress', JSON.stringify(lFiles));
+		} else {
+			this.getUploadField().markInvalid("Please you must select something!");
+		}
+	}
 });
