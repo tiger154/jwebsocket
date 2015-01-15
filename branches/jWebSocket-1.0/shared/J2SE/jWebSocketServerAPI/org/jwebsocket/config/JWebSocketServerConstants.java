d3af18be8f4a0198a0f31f09b2c65a592921b05e
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-01-15 13:27:17 +0000

    Adding JMS_GATEWAY_DEFAULT_CONNECTION_PREFIX constant.

--- branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/config/JWebSocketServerConstants.java
+++ branches/jWebSocket-1.0/shared/J2SE/jWebSocketServerAPI/org/jwebsocket/config/JWebSocketServerConstants.java
@@ -66,8 +66,8 @@ public final class JWebSocketServerConstants {
 	 */
 	public static String DEFAULT_ENGINE = "tcp";
 	/**
-	 * Default node id if non given in jWebSocket.xml. Empty means do not use
-	 * node-id for single stand-alone systems
+	 * Default node id if non given in jWebSocket.xml. Empty means do not use node-id for single
+	 * stand-alone systems
 	 */
 	public static String DEFAULT_NODE_ID = "";
 	/**
@@ -75,8 +75,7 @@ public final class JWebSocketServerConstants {
 	 */
 	public static final int DEFAULT_MAX_CONNECTIONS = 10000;
 	/**
-	 * The default "on max connections reached" strategy {wait, close, reject,
-	 * redirect}
+	 * The default "on max connections reached" strategy {wait, close, reject, redirect}
 	 */
 	public static final String DEFAULT_ON_MAX_CONNECTIONS_STRATEGY = "reject";
 	/**
@@ -89,11 +88,16 @@ public final class JWebSocketServerConstants {
 	 * @see conf/bootstrap.xml
 	 */
 	public static final String CONNECTION_MANAGER_BEAN_ID = "org.jwebsocket.server.connection_manager";
-	
+
 	public static final boolean KEEP_ALIVE_CONNECTORS = false;
-	
+
 	public static final Integer KEEP_ALIVE_CONNECTORS_INTERVAL = 20000;
-	
+
 	public static final Integer KEEP_ALIVE_CONNECTORS_TIMEOUT = 10000;
+
+	/**
+	 * The default JMSGateway connection prefix value.
+	 */
+	public static final String JMS_GATEWAY_DEFAULT_CONNECTION_PREFIX = "JWS_JMS_GW";
 
 }
