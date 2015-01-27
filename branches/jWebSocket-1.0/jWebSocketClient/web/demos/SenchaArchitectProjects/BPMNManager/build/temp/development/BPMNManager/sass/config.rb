75489c2a299148b1c80c37e93fc07228596102d9
odiaz <odiaz@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-01-27 17:58:46 +0000

    Removing all unnecesary files in the Sencha Project. Thanks Alex

--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/SenchaArchitectProjects/BPMNManager/build/temp/development/BPMNManager/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
-require '../../../../../sass/config.rb'
-cache_path = '/home/zcool/src/jWebSocket/branches/jWebSocket-1.0/jWebSocketClient/web/demos/SenchaArchitectProjects/BPMNManager/build/.sass-cache'
--- branches/jWebSocket-1.0/jWebSocketClient/web/demos/SenchaArchitectProjects/BPMNManager/build/testing/BPMNManager/resources/Readme.md
+++
@@ -1,3 +0,0 @@
-# ext-theme-crisp-55a4f430-ceea-4e25-b215-60beac66a629/resources
-
-This folder contains static resources (typically an `"images"` folder as well).
