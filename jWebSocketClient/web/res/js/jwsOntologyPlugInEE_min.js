d5c73c8c87c78165d537c5156946b3a3bfcdb9e3
rsantamaria <rsantamaria@ae9c1b69-f12a-9e44-4531-b4985e8a65cc>
2015-02-04 17:36:42 +0000

    Fixing isArrayOf method on jws.tools library. OntologyPlugIn tests advances.

--- jWebSocketClient/web/res/js/jwsOntologyPlugInEE_min.js
+++ jWebSocketClient/web/res/js/jwsOntologyPlugInEE_min.js
@@ -1,28 +1,27 @@
-
 //	---------------------------------------------------------------------------
 //	jWebSocket Enterprise Ontology Client Plug-In
 //	(C) Copyright 2012-2014 Innotrade GmbH, Herzogenrath Germany
 //	Author: Alexander Schulze
+//	Author: Rolando Santamaria Maso
 //	---------------------------------------------------------------------------
-jws.OntologyPlugIn={NS:jws.NS_BASE+".plugins.ontology",bf:"getConcepts",aH:"getIndividuals",bO:"getProperties",bH:"getProperty",bK:
-"addConcept",bI:"addIndividual",aG:"addDataTypeProperty",bq:"addObjectProperty",ae:"addStatement",al:"removeConcept",bE:
-"removeIndividual",bF:"removeProperty",av:"removeStatement",ontCall:function(au,J){var C=this.checkConnected();if(0===C.code){var G=
-{ns:jws.OntologyPlugIn.NS,type:"...",alias:au};this.sendToken(G,J);}return C;},bx:function(au,J){var C=this.checkConnected();if(0===
-C.code){var G={ns:jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.bf,alias:au};this.sendToken(G,J);}return C;},bd:function(au,J){
-var C=this.checkConnected();if(0===C.code){var G={ns:jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.aH,alias:au};this.sendToken(G,J);
-}return C;},ay:function(au,bn,J){var C=this.checkConnected();if(0===C.code){var G={ns:jws.OntologyPlugIn.NS,type:
-jws.OntologyPlugIn.bO,alias:au,bQ:bn};this.sendToken(G,J);}return C;},bv:function(au,ba,J){var C=this.checkConnected();if(0===
-C.code){var G={ns:jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.bH,alias:au,aV:ba};this.sendToken(G,J);}return C;},bu:function(au,
-aO,bz,J){var C=this.checkConnected();if(0===C.code){var G={ns:jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.bK,alias:au,bL:aO,aD:bz}
-;this.sendToken(G,J);}return C;},by:function(au,bn,aO,J){var C=this.checkConnected();if(0===C.code){var G={ns:jws.OntologyPlugIn.NS,
-type:jws.OntologyPlugIn.bI,alias:au,bQ:bn,bL:aO};this.sendToken(G,J);}return C;},bB:function(au,aS,bz,J){var C=this.checkConnected()
-;if(0===C.code){var G={ns:jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.aG,alias:au,aV:aS,aD:bz};this.sendToken(G,J);}return C;},aX:
-function(au,aS,bz,J){var C=this.checkConnected();if(0===C.code){var G={ns:jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.bq,alias:au,
-aV:aS,aD:bz};this.sendToken(G,J);}return C;},ao:function(au,aS,J){var C=this.checkConnected();if(0===C.code){var G={ns:
-jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.ae,alias:au,aV:aS};this.sendToken(G,J);}return C;},be:function(au,aO,J){var C=
-this.checkConnected();if(0===C.code){var G={ns:jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.al,alias:au,bL:aO};this.sendToken(G,J);
-}return C;},aI:function(au,bn,J){var C=this.checkConnected();if(0===C.code){var G={ns:jws.OntologyPlugIn.NS,type:
-jws.OntologyPlugIn.bE,alias:au,bQ:bn};this.sendToken(G,J);}return C;},bA:function(au,aS,J){var C=this.checkConnected();if(0===
-C.code){var G={ns:jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.bF,alias:au,aV:aS};this.sendToken(G,J);}return C;},bS:function(au,
-bP,J){var C=this.checkConnected();if(0===C.code){var G={ns:jws.OntologyPlugIn.NS,type:jws.OntologyPlugIn.av,alias:au,aL:bP};
-this.sendToken(G,J);}return C;}};jws.oop.addPlugIn(jws.jWebSocketTokenClient,jws.OntologyPlugIn); 
+
+jws.OntologyPlugIn = {
+	// namespace for Ontology plugin
+	// if namespace is changed update server plug-in accordingly!
+	NS: jws.NS_BASE + ".plugins.ontology",
+	
+	//:m:*:getOntologyPlugIn
+	//:d:en:Get the JavaScript object representation of the OntologyPlugIn
+	//:a:en::aOptions.OnSuccess:function:Function to be called when the plugin instance has been generated.
+	//:r:*:::void:none
+	getOntologyPlugIn: function (aOptions) {
+		aOptions.filter = function (aMethodName, aParams) {
+			aParams.splice(0, 0, "ontologyAlias");
+		};
+
+		return this.getActionPlugIn(jws.OntologyPlugIn.NS, aOptions);
+	}
+};
+
+// add the jWebSocket OntologyPlugIn into the TokenClient class
+jws.oop.addPlugIn(jws.jWebSocketTokenClient, jws.OntologyPlugIn);
